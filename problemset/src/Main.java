543d8ce3aa911d69c4bb4e05ff7b9ad1b01774da
Nguyen Dac Khue <khue0011@gmail.com>
2015-07-14 18:13:04 +0800

    * done 5 problem * remember > divisible for any lucky number, not only for 4 and 7 * revise selection algo + substr + insertion sort

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -1,28 +1,38 @@
-import java.util.Arrays;
-import java.util.Scanner;
 
-public class Main  {
-    public static void main (String [] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] arr = new int[n];
-        for ( int i = 0 ; i < n ; i ++){
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
-        int rel = 0 ;
-        int i = 0, j = n-1 ;
-        while(i < j ){
-            if (arr[i] + arr[j] <= 4){
-                arr[j] += arr[i];
-                i++;
-            }else{
-                rel++;
-                j--;
+import javax.xml.stream.events.Characters;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static final int N = 1000001;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        int [] arr = new int[N];
+        arr[2] = 1 ;
+        for ( int i = 2 ; i < N; i++){
+            if( arr[i] == 0){
+                arr[i] = 1;
+            }
+            for( int j = i+i; j < N ; j += i ){
+                arr[j] = -1;
             }
         }
-        System.out.println(rel + 1);
+
+        int n = sc.nextInt();
+        for (int i = 2 ; i < N; i++){
+            if( arr[i] == -1 && arr[n- i] == -1){
+                System.out.println(i + " " + (n-i) );
+                break;
+            }
+        }
 
 
     }
+
+
+
+
 }
