ec235d5d3d1760888a548b45f8d2fdd18e75beae
Nguyen Dac Khue <khue0011@gmail.com>
2015-07-22 18:13:03 +0800

    *update new problems

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -1,34 +1,21 @@
 
 import javax.xml.stream.events.Characters;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static final int N = 1000001;
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
-        int [] arr = new int[N];
-        arr[2] = 1 ;
-        for ( int i = 2 ; i < N; i++){
-            if( arr[i] == 0){
-                arr[i] = 1;
-            }
-            for( int j = i+i; j < N ; j += i ){
-                arr[j] = -1;
-            }
-        }
 
-        int n = sc.nextInt();
-        for (int i = 2 ; i < N; i++){
-            if( arr[i] == -1 && arr[n- i] == -1){
-                System.out.println(i + " " + (n-i) );
-                break;
-            }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int rel = a;
+        while( a >= b){
+            a = a/b + a%b;
+            rel += a;
         }
-
+        System.out.print(rel);
 
     }
 
