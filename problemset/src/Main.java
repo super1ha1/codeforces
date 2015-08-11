77db000539d5b0b23ab8b90ec055e6b7427105d9
khue001 <khue001@e.ntu.edu.sg>
2015-08-12 01:04:47 +0800

    updated on Summer break

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -7,16 +7,27 @@ public class Main {
     public static final int N = 1000001;
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int rel = a;
-        while( a >= b){
-            a = a/b + a%b;
-            rel += a;
+        int row = 0 , col = 0 , n;
+        for ( int i = 1; i <= 5; i ++){
+            for ( int j = 1; j <= 5; j++){
+                n = sc.nextInt();
+                if( n == 1){
+                    row = i;
+                    col = j ;
+                }
+            }
         }
-        System.out.print(rel);
 
+        System.out.print(Math.abs(row -3 ) + Math.abs(col -3));
+
+    }
+
+    public static int getHour(int a , int b){
+        int mod, rel;
+        mod = a % b;
+        rel = a /b ;
+        if ( rel == 0) return  mod;
+        else return a   + getHour(rel , b);
     }
 
 
