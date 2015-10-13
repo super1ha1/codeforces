4fa263b4e024c133ae12a839c13378f0f27f2012
super1ha1 <khue0011@gmail.com>
2015-10-13 23:46:34 +0800

    add file from US IOI

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -1,55 +1,44 @@
 
-import javax.xml.stream.events.Characters;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static final int N = -1000001;
-    public static void main(String[] args) {
-        int [] arr = {6, 9, 7 , 12, 15, 19, 14 , 16};
-        int [] A = new int[arr.length-1];
-        for (int i = 0 ; i < arr.length -1 ; i++){
-            A[i] = arr[i+1] - arr[i];
-        }
-        int max = divide(A, 0, A.length-1 );
-        System.out.println("sum: " + max );
-    }
-    public static int divide(int[] A, int low, int high){
-        if( low == high){
-            return A[low];
-        }else {
-            int mid = (int) (low + high)/2;
-            int leftSum = divide(A, low, mid);
-            int rightSum = divide(A, mid +1, high);
-            int crossSum = crossSum(A, low, high);
-            if( leftSum >= rightSum && leftSum >= crossSum)
-                return leftSum;
-            else if ( rightSum >= leftSum && rightSum >= crossSum)
-                return rightSum;
-            else return crossSum;
-        }
-    }
-
-    private static int crossSum(int[] A, int low, int high) {
-        int leftMax = N, rightMax = N;
-        int sum = 0;
-        int i, j;
-        int mid = (int) (low + high)/2;
-        for ( i = mid; i >= low; i-- ){
-            sum += A[i];
-            if( sum > leftMax){
-                leftMax = sum;
+    public static void main(String[] args) throws Exception{
+        // Use BufferedReader rather than RandomAccessFile; it's much faster
+        BufferedReader f = new BufferedReader(new FileReader("test.in"));
+        // input file name goes above
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
+        // Use StringTokenizer vs. readLine/split -- lots faster
+        int i1 =0, i2 = 0;
+        String line;
+        try {
+            while (( line = f.readLine()) != null){
+                StringTokenizer st = new StringTokenizer(line);
+                // Get line, break into tokens
+                if( st.hasMoreTokens()){
+                    i1 = Integer.parseInt(st.nextToken());
+                    continue;
+                }
+                if( st.hasMoreTokens()){
+                    i2 = Integer.parseInt(st.nextToken());
+                    break;
+                }
             }
-        }
-        sum = 0;
-        for (j = mid+1; j <= high; j++ ){
-            sum+= A[j];
-            if(sum > rightMax){
-                rightMax = sum;
-            }
-        }
-        System.out.println("Cross max: " + (leftMax + rightMax));
-        return leftMax + rightMax;
-    }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } finally {
+            f.close();
 
+        }
+        System.out.println("i1: "  + i1 + " i2: " + i2);
+
+
+        out.println(i1+i2);                           // output result
+        out.println(10);
+        out.close();
+    }
 }
