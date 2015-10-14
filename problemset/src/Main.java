cfe294069435526acff4f06b1b6e1b1f9c5aba5e
super1ha1 <khue0011@gmail.com>
2015-10-14 18:43:16 +0800

    Add tut dynamic programming

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -3,42 +3,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static final int N = 100;
 
-    public static final int N = -1000001;
     public static void main(String[] args) throws Exception{
-        // Use BufferedReader rather than RandomAccessFile; it's much faster
-        BufferedReader f = new BufferedReader(new FileReader("test.in"));
-        // input file name goes above
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
-        // Use StringTokenizer vs. readLine/split -- lots faster
-        int i1 =0, i2 = 0;
-        String line;
-        try {
-            while (( line = f.readLine()) != null){
-                StringTokenizer st = new StringTokenizer(line);
-                // Get line, break into tokens
-                if( st.hasMoreTokens()){
-                    i1 = Integer.parseInt(st.nextToken());
-                    continue;
-                }
-                if( st.hasMoreTokens()){
-                    i2 = Integer.parseInt(st.nextToken());
-                    break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        } finally {
-            f.close();
-
+        String s = "ATTGAGA";
+//        Set symbol = new HashSet<Character>();
+//        symbol.add('A');
+//        symbol.add('T');
+//        symbol.add('G');
+//        Set A =new HashSet<Integer>();
+//        Set T =new HashSet<Integer>();
+//        Set G =new HashSet<Integer>();
+//        SIP(s, symbol, A, T, G);
+//        printSet(A);
+//        printSet(T);
+//        printSet(G);
+        SIP(s);
+    }
+    public static void SIP(String s, Set symbol, Set A, Set T, Set G){
+        char [] chars = s.toCharArray();
+        for( int i = 0 ; i < chars.length; i ++){
+           switch (chars[i]){
+               case 'A':
+                   A.add(i+1);
+                   break;
+               case 'T':
+                   T.add(i+1);
+                   break;
+               case 'G':
+                   G.add(i+1);
+                   break;
+               default:
+                   break;
+           }}}
+    public static void printSet(Set<Integer> set){
+        for(Integer i: set){
+            System.out.print(i + " ");
         }
-        System.out.println("i1: "  + i1 + " i2: " + i2);
-
-
-        out.println(i1+i2);                           // output result
-        out.println(10);
-        out.close();
+        System.out.print("\n");
+    }
+    public static void SIP(String s){
+        char [] chars = s.toCharArray();
+        Set symbol= new HashSet<Character>();
+        HashMap<Character, Set> map = new HashMap<Character, Set>();
+        for( int i = 0 ; i < chars.length; i ++){
+            if(!symbol.contains(chars[i])){
+                symbol.add(chars[i]);
+                Set newSet = new HashSet<Integer>();
+                newSet.add(i+1);
+                map.put(chars[i], newSet);
+            }else {
+                Set existSet = map.get(chars[i]);
+                existSet.add(i+1);
+            }
+        }
+        for(Character c : map.keySet()){
+            printSet(map.get(c));
+        }
     }
 }
