cb4a963fcfd93ff69b73457fb4c068112e2c502a
super1ha1 <khue0011@gmail.com>
2015-12-02 23:03:05 +0800

    UVA 272 TEX quote

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -6,6 +6,25 @@ class Main
     public static void main (String args[])  // entry point from OS
     {
         Scanner sc = new Scanner(System.in);
+        String line, temp;
+        char [] chars;
+        StringBuilder sb;
 
+        boolean first = false;
+        while (sc.hasNext()){
+            line = sc.nextLine();
+            chars = line.toCharArray();
+            sb = new StringBuilder();
+            for (int i = 0 ; i < chars.length; i++){
+                if( chars[i] == '"'){
+                    first = !first;
+                    temp = first ? "``" : "''";
+                    sb.append(temp);
+                }else {
+                    sb.append(chars[i]);
+                }
+            }
+            System.out.println(sb.toString());
+        }
     }
 }
