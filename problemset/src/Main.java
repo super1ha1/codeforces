4ce248feadcf84de442d5f5ec331939dcf92683c
super1ha1 <khue0011@gmail.com>
2016-01-03 21:33:59 +0800

    UVA 573 - The Snail

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -8,20 +8,42 @@ public  class Main
 {
     public static void main (String args[])  throws Exception// entry point from OS
     {
-        int n ;
-        Map<Integer , Integer> map = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int num;
-        for ( int i = 1; i <= n; i++){
-            num = sc.nextInt();
-            map.put(num, i);
+//        Scanner sc = new Scanner(new FileReader("in.txt"));
+        int H, U, D, F;
+        String output = "";
+        while (sc.hasNextInt()){
+            H = sc.nextInt();
+            if( H == 0 )
+                break;
+            U = sc.nextInt();
+            D = sc.nextInt();
+            F = sc.nextInt();
+            int count = 0;
+            double decrease = 1.0 * (U * F ) /100;
+            double currentU = U, HDay = 0, HNight = 0;
+            boolean success = false;
+            while (true){
+                currentU = U - decrease * count ;
+                if( currentU <= 0) currentU = 0;
+                HDay = HNight + currentU;
+                if( HDay > H){
+                    success = true;
+                    break;
+                }
+                HNight = HDay - D;
+                if(HNight < 0){
+                    success= false;
+                    break;
+                }
+                count++;
+//                System.out.println(" after day: " + count + " U: " + currentU + " Hday: " + HDay + " Night: " + HNight );
+            }
+//            System.out.println(" after day: " + count + " U: " + currentU + " Hday: " + HDay + " Night: " + HNight );
+            String s = (success ? "success" : "failure") + " on day " + (count+ 1) + "\n";
+            output += s;
         }
-        BigInteger sum = BigInteger.valueOf(0);
-        for( int i = 1; i < n; i++){
-            sum = sum.add(BigInteger.valueOf(Math.abs(map.get(i + 1) - map.get(i))));
-        }
-        System.out.println(sum);
-    }
+        System.out.println(output);
 
+    }
 }
