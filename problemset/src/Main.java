f8ae50bdd5e33be6e3f82e5c34256d4088cd2ead
super1ha1 <khue0011@gmail.com>
2016-01-03 22:47:29 +0800

    Input for 482 - Permutation Arrays

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -8,61 +8,40 @@ public  class Main
 {
     public static void main (String args[])  throws Exception// entry point from OS
     {
-        Scanner sc = new Scanner(System.in);
-//        Scanner sc = new Scanner(new FileReader("in.txt"));
+//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(new FileReader("in.txt"));
         int n, m, c;
-        int count = 1;
-        while (sc.hasNextInt()){
-            n = sc.nextInt();
-            if( n == 0 )
-                break;
-            m = sc.nextInt();
-            c = sc.nextInt();
+        n = Integer.valueOf(sc.nextLine());
+        int a, b;
+        for ( int i = 0 ; i < n; i++){
+            Vector<Integer> index = new Vector<>();
+            Vector<Integer> value = new Vector<>();
+            String first = sc.nextLine();
+            String indexStr = sc.nextLine();
+            String valueStr = sc.nextLine();
 
-            int [] device = new int[n];
-            int [] device_on = new int[n];
-            int [] sequence = new int[m];
+            System.out.println("index; " + indexStr);
+            System.out.println("value; " + valueStr);
+            for( String s: indexStr.split(" ")){
+                System.out.print(s);
 
-            int sum = 0 , max = 0 ;
-            boolean blown = false;
-
-            for( int i = 0 ; i < n; i++){
-                device[i] = sc.nextInt();
-                device_on[i] = 0;
+//                index.add(Integer.valueOf(s));
             }
+            for( String s: valueStr.split(" ")){
+                System.out.print(s);
 
-            for( int i =0 ; i < m; i++){
-                sequence[i] = sc.nextInt();
+//                value.add(Integer.valueOf(s));
             }
-
-            for( int i = 0 ; i < m; i++){
-                device_on[sequence[i] -1 ] =  device_on[sequence[i] -1 ] == 0 ? 1 : 0 ;
-                sum = getSum(device, device_on);
-                if( sum > max ) max = sum;
-                if( sum > c ){
-                    blown = true;
-                    break;
-                }
-            }
-            System.out.println("Sequence " + count);
-            if(blown){
-                System.out.println("Fuse was blown.");
-            }else {
-                System.out.println("Fuse was not blown.");
-                System.out.println("Maximal power consumption was " + max + " amperes.");
-            }
-            System.out.println();
-            count++;
+            print(index);
+            print(value);
         }
+
 
     }
-    public static int getSum(int [] device, int[] device_on)
-    {
-        int sum = 0 ;
-        for ( int i = 0 ; i < device.length; i++){
-            if( device_on[i] == 1)
-                sum += device[i];
+    public static void print(Vector<Integer> integers){
+        for( Integer integer: integers){
+            System.out.print(integer);
         }
-        return sum;
+        System.out.println();
     }
 }
