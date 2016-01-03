9ea91f12515d455d627048691bbce3ef96f1b05d
super1ha1 <khue0011@gmail.com>
2016-01-03 22:08:20 +0800

    661 Blowing Fuses

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -10,40 +10,59 @@ public  class Main
     {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new FileReader("in.txt"));
-        int H, U, D, F;
-        String output = "";
+        int n, m, c;
+        int count = 1;
         while (sc.hasNextInt()){
-            H = sc.nextInt();
-            if( H == 0 )
+            n = sc.nextInt();
+            if( n == 0 )
                 break;
-            U = sc.nextInt();
-            D = sc.nextInt();
-            F = sc.nextInt();
-            int count = 0;
-            double decrease = 1.0 * (U * F ) /100;
-            double currentU = U, HDay = 0, HNight = 0;
-            boolean success = false;
-            while (true){
-                currentU = U - decrease * count ;
-                if( currentU <= 0) currentU = 0;
-                HDay = HNight + currentU;
-                if( HDay > H){
-                    success = true;
-                    break;
-                }
-                HNight = HDay - D;
-                if(HNight < 0){
-                    success= false;
-                    break;
-                }
-                count++;
-//                System.out.println(" after day: " + count + " U: " + currentU + " Hday: " + HDay + " Night: " + HNight );
-            }
-//            System.out.println(" after day: " + count + " U: " + currentU + " Hday: " + HDay + " Night: " + HNight );
-            String s = (success ? "success" : "failure") + " on day " + (count+ 1) + "\n";
-            output += s;
-        }
-        System.out.println(output);
+            m = sc.nextInt();
+            c = sc.nextInt();
 
+            int [] device = new int[n];
+            int [] device_on = new int[n];
+            int [] sequence = new int[m];
+
+            int sum = 0 , max = 0 ;
+            boolean blown = false;
+
+            for( int i = 0 ; i < n; i++){
+                device[i] = sc.nextInt();
+                device_on[i] = 0;
+            }
+
+            for( int i =0 ; i < m; i++){
+                sequence[i] = sc.nextInt();
+            }
+
+            for( int i = 0 ; i < m; i++){
+                device_on[sequence[i] -1 ] =  device_on[sequence[i] -1 ] == 0 ? 1 : 0 ;
+                sum = getSum(device, device_on);
+                if( sum > max ) max = sum;
+                if( sum > c ){
+                    blown = true;
+                    break;
+                }
+            }
+            System.out.println("Sequence " + count);
+            if(blown){
+                System.out.println("Fuse was blown.");
+            }else {
+                System.out.println("Fuse was not blown.");
+                System.out.println("Maximal power consumption was " + max + " amperes.");
+            }
+            System.out.println();
+            count++;
+        }
+
+    }
+    public static int getSum(int [] device, int[] device_on)
+    {
+        int sum = 0 ;
+        for ( int i = 0 ; i < device.length; i++){
+            if( device_on[i] == 1)
+                sum += device[i];
+        }
+        return sum;
     }
 }
