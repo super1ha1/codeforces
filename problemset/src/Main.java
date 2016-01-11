74cedc147667819b966b392801772c2548a13716
super1ha1 <khue0011@gmail.com>
2016-01-12 00:28:33 +0800

    codeforce education round 5

--- problemset/src/Main.java
+++ problemset/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
@@ -8,32 +6,35 @@ public  class Main
 {
     public static void main (String args[])  throws Exception// entry point from OS
     {
-        Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new FileReader("in.txt"));
         int n, m;
-        while (sc.hasNextInt()){
-            n = sc.nextInt();
-            m = sc.nextInt();
-            int a ;
-            int [] array = new int[m];
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-            while (sc.hasNextInt()){
-                a = sc.nextInt();
-                array[a - 1 ] = 1;
+        long [] min = new long[n];
+        long [][] array = new long [n][m];
+        for ( int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < m; j++){
+                array[i][j] = sc.nextLong();
+                if( j == 0 ) min[i] = array[i][j];
+                if( array[i][j] < min[i])
+                    min[i] = array[i][j];
             }
-
-            boolean yes = true;
-            for ( int i  =0 ; i < m; i++){
-                if(array[i] == 0){
-                    yes = false;
-                    break;
-                }
-            }
-            String s = yes ? "YES" : "NO" ;
-            System.out.println(s);
-
         }
-
+        int max_index = -1;
+        long max_value = -1;
+        for ( int i = 0 ;i < n; i++){
+            if( i == 0 ){
+                max_value = min[i];
+                max_index = i;
+            }
+            if( min[i] > max_value){
+                max_value = min[i];
+                max_index = i;
+            }
+        }
+        System.out.println(max_value);
     }
 
 }
