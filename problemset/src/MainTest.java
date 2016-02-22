eba6c1dab533b6cf87cacd3e731c54ddb4f84f56
super1ha1 <khue0011@gmail.com>
2016-02-22 22:33:55 +0800

    Caesar cipher encrypt and decrypt

--- problemset/src/MainTest.java
+++ problemset/src/MainTest.java
@@ -5,15 +5,48 @@ import java.util.*;
 
 public class MainTest {
     public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-        int n, b,p;
-        n = sc.nextInt();
-        b = sc.nextInt();
-        p = sc.nextInt();
-        int bottleEachMatch = 2 * b + 1;
 
-        int matches = findMatch(n);
-        System.out.println( (matches * bottleEachMatch) + " " + ( n * p));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Enter the key: ");
+        int key = sc.nextInt();
+
+        String encrypt = encryptString(str, key);
+        System.out.println("Encrypted string: " + encrypt);
+        System.out.println("Ori string: " + encryptString(encrypt, key * -1));
+    }
+
+    private static String encryptString(String str, int key) {
+        if(key < 0){
+            key = 26 - (-key %26);
+        }
+
+        String result = "";
+        for( int i = 0 ; i < str.length(); i++){
+            char c = str.charAt(i);
+            result += encryptChar(c, key);
+        }
+        return result;
+
+    }
+
+    private static String encryptChar(char c, int key) {
+        if( !Character.isLetter(c))
+            return "";
+
+        if(Character.isUpperCase(c)){
+            char newChar = (char) ( 'A' +  ((c  -'A' + key) % 26 )) ;
+            return String.valueOf(newChar);
+        }
+
+        if(Character.isLowerCase(c)){
+            char newChar = (char) ( 'a' + ((c -'a' + key) % 26));
+            return String.valueOf(newChar);
+        }
+        return "";
     }
 
     private static int findMatch(int n) {
@@ -25,6 +58,19 @@ public class MainTest {
             n = left + k;
         }
         return matches;
+    }
+
+    private static char toLower(char ch){
+
+        String s = "Aafasdfds";
+        s.toUpperCase();
+        System.out.println(" s upper: " + s.toUpperCase());
+
+        System.out.println(" s: " + s);
+        if( ch >= 'A' && ch <= 'Z'){
+            return (char)((ch - 'A' + 'a'));
+        }
+        return ch;
     }
 
 
