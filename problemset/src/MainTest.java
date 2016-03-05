97385d01b000f98ad6fca46927d507d471104dc3
super1ha1 <khue0011@gmail.com>
2016-03-05 18:09:45 +0800

    update C(k, n)

--- problemset/src/MainTest.java
+++ problemset/src/MainTest.java
@@ -10,23 +10,14 @@ public class MainTest {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        int [] array = {1, 2, 3, 4, 9 , 10, 15, 20, 25};
-        System.out.println(binarySearch(array, 0, array.length -1, 25));
-
+        System.out.println(C(10, 8));
     }
 
-    public static int binarySearch (int[] array, int low, int high, int key){
-        if(array.length == 0 ) return -1;
-        if( high < low ) return  -1;
-
-        int mid = low + (high - low) /2;
-        if( array[mid] == key)
-            return mid;
-        else if( array[mid] < key){
-            return binarySearch(array, mid+1, high, key);
-        }else return binarySearch(array, low, mid -1, key);
-
-
+    public static int C(int n, int k){
+        if( k == 0 || k == n){
+            return 1;
+        }else
+            return C(n-1, k-1) + C(n-1, k);
     }
 
 
