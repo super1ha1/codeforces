9ecdde9a1da32285c46776acb3a254d24868b784
super1ha1 <khue0011@gmail.com>
2016-03-06 14:28:37 +0800

    List String permutation

--- problemset/src/MainTest.java
+++ problemset/src/MainTest.java
@@ -10,14 +10,23 @@ public class MainTest {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println(C(10, 8));
+        listPermute("abc");
     }
 
-    public static int C(int n, int k){
-        if( k == 0 || k == n){
-            return 1;
-        }else
-            return C(n-1, k-1) + C(n-1, k);
+    public static void recPermute( String soFar, String rest){
+        if( rest.equals("")){
+            System.out.println(soFar);
+        }else {
+            for( int i = 0 ; i < rest.length(); i++){
+                String next = soFar + rest.charAt(i);
+                String remain = rest.substring(0, i) + rest.substring(i+1);
+                recPermute(next, remain);
+            }
+        }
+    }
+
+    public static void listPermute(String s){
+        recPermute("", s);
     }
 
 
