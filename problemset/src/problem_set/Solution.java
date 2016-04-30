32de25706da1dfcf4b0dc554be794eda7ef476a5
super1ha1 <khue0011@gmail.com>
2016-04-30 15:26:30 +0800

    Q2

--- problemset/src/problem_set/Solution.java
+++ problemset/src/problem_set/Solution.java
@@ -2,30 +2,43 @@ package problem_set;
 
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int [] array = new int[n];
-
-        for( int i = 0 ; i < n; i++){
-            array[i] = sc.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c[] = new int[n];
+        for(int c_i=0; c_i < n; c_i++){
+            c[c_i] = in.nextInt();
         }
 
-        int count = 0 ;
-        for(int i = 0 ; i <= n-2; i++){
-            int nextIndex = Arrays.binarySearch(array, i, n, array[i] + k);
-            if(nextIndex > 0){
-                int secondIndex = Arrays.binarySearch(array, nextIndex, n, array[nextIndex] + k);
-                if(secondIndex > 0){
-                    count ++;
-                }
+        int current = 0, count = 0;
+
+        while (true){
+            if(current == n-1) break;
+
+            if(current+2 <= n-1 && c[current + 2] == 0) {
+                current += 2;
+                count++;
+                if(current == n-1) break;
+            }
+            else if( current +1 <= n-1 && c[current + 1] == 0) {
+                current += 1;
+                count++;
+                if(current == n-1) break;
+            }
+            else {
+                break;
             }
         }
-        System.out.println(count);
+
+        if(current == n-1){
+            System.out.println(count);
+        }
+
     }
 }
