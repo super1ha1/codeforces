63535c17f2dfc2934c94a695424ca3bdab9ca141
super1ha1 <khue0011@gmail.com>
2016-06-29 17:54:08 +0800

    https://www.hackerrank.com/challenges/two-strings O(n^2)

--- problemset/src/problem_set/Main.java
+++ problemset/src/problem_set/Main.java
@@ -12,23 +12,40 @@ public class Main {
     private static DecimalFormat df=new DecimalFormat("0.000000");
     public static void main(String[] args){
         int n  = sc.nextInt();
-        int[] M = new int[n];
-        int[] R = new int[n];
-        for( int i = 0 ; i < n; i++){
-            M[i] = sc.nextInt();
-        }
-        for( int i = 0 ; i < n; i++){
-            R[i] = sc.nextInt();
+        String temp = sc.nextLine();
+        String first, second;
+        Set<String> firstSubstringSet;
+        while (n-- > 0){
+            first = sc.nextLine();
+            second = sc.nextLine();
+            firstSubstringSet = getSubStringSet(first);
+            boolean result = false;
+            if(firstSubstringSet != null){
+                for(String s: firstSubstringSet){
+                    if(second.contains(s)) {
+                        result = true;
+                    }
+                }
+            }
+
+            String str = result ? "YES" : "NO";
+            System.out.println(str);
         }
 
-        double left = 1.0, result = 0.0;
-        for( int i = 0 ; i < n; i++){
-            result += left * 1/M[i];
-            left = left - (double) 1/M[i];
-        }
+    }
 
-//        0.666667
-        System.out.println(df.format(result));
+    private static Set<String> getSubStringSet(String first) {
+        if(first == null || first.length() == 0) return  null;
+        Set<String> result = new TreeSet<>();
+        for( int i = 0; i < first.length(); i++){
+            for( int j = i + 1; j <= first.length(); j++){
+                result.add(first.substring(i, j));
+            }
+        }
+//        for( String s: result){
+//            System.out.println(s);
+//        }
+        return result;
 
     }
 
