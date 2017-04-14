813ab674a9b1853e9bb2831ca572044950f64b8e
super1ha1 <khue0011@gmail.com>
2017-04-14 10:31:56 +0800

    UVA 739 Soundex Indexing: note: check all requirement, col 10 mean 9 space before, parse map input correctly

--- problemset/src/problem_set/Main.java
+++ problemset/src/problem_set/Main.java
@@ -1,52 +1,86 @@
 package problem_set;
 
-import java.io.BufferedReader;
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.io.FileReader;
-import java.text.DecimalFormat;
 import java.util.*;
 
 
 public class Main {
 
+    private static String [] first = {"B", "P", "F", "V"};
+    private static String [] second = {"C", "S","K","G","J","Q","X","Z"};
+    private static String [] third = {"D", "T"};
+    private static String [] forth = {"L"};
+    private static String [] fifth = {"M", "N"};
+    private static String [] six = {"R"};
+    private static Map<String, Integer> map = new HashMap<String, Integer>();
     public static void main(String[] args) throws  Exception{
 //        Scanner sc = new Scanner(new FileReader("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/problemset/src/problem_set/in.txt"));
+        prepareMap();
         Scanner sc = new Scanner(System.in);
-        String output = "";
+        printPair("NAME", "SOUNDEX CODE");
         while (sc.hasNext()){
-           String input = sc.nextLine();
-           output += printnextLine(input);
-           if(sc.hasNext()){
-               output += '\n';
-           }
+            String line = sc.nextLine().trim();
+            String output = computeOutput(line);
+            printPair(line, output);
         }
-        System.out.println(output);
+        System.out.println("                   END OF OUTPUT");
+
     }
 
-    private static String printnextLine(String input) {
-        StringBuilder output = new StringBuilder();
-        String word = "";
-        for(int i = 0 ; i < input.length(); i++) {
-            if(input.charAt(i) == ' '){
-                output.append(getReverse(word));
-                output.append(input.charAt(i));
-                word = "";
-            }else {
-                word += input.charAt(i);
+    private static void prepareMap() {
+        for(String s: first){
+            map.put(s, 1);
+        }
+        for(String s: second){
+            map.put(s, 2);
+        }
+        for(String s: third){
+            map.put(s, 3);
+        }
+        for(String s: forth){
+            map.put(s, 4);
+        }
+        for(String s: fifth){
+            map.put(s, 5);
+        }
+        for(String s: six){
+            map.put(s, 6);
+        }
+
+    }
+
+    private static String computeOutput(String input) {
+        String output = "" + input.charAt(0);
+        for(int i = 1; i < input.length(); i++){
+            if(map.containsKey(String.valueOf(input.charAt(i)))){
+                int number = map.get(String.valueOf(input.charAt(i)));
+
+                if(map.containsKey(String.valueOf(input.charAt(i -1)))){
+                    int previous = map.get(String.valueOf(input.charAt(i -1)));
+                    if(number == previous){
+                        continue;
+                    }
+                }
+                output += String.valueOf(number);
+
             }
         }
-        output.append(getReverse(word));
-        return output.toString();
+        while (output.length() < 4){
+            output += "0";
+        }
+
+        return output.substring(0,4);
     }
 
-    private static String getReverse(String word) {
-        if(word == null || word.length() == 0){
-            return "";
+    private static void printPair(String name, String output) {
+        String newLine = "         " + name;
+        while (newLine.length() < 34){
+            newLine += " ";
         }
-        String output = "";
-        for(int i = word.length() -1 ; i >= 0; i--){
-            output+= word.charAt(i);
-        }
-        return output;
+        newLine += output;
+        System.out.println(newLine);
     }
 
 }
