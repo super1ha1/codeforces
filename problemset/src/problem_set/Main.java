646a7e34225a6dcae256bfbf91388e3f5eb3c334
super1ha1 <khue0011@gmail.com>
2017-05-01 10:01:16 +0800

    restructure code

--- problemset/src/problem_set/Main.java
+++ problemset/src/problem_set/Main.java
@@ -1,86 +1,54 @@
 package problem_set;
 
-import com.sun.org.apache.regexp.internal.RE;
-
 import java.io.FileReader;
+import java.math.BigInteger;
 import java.util.*;
-
 
 public class Main {
 
-    private static String [] first = {"B", "P", "F", "V"};
-    private static String [] second = {"C", "S","K","G","J","Q","X","Z"};
-    private static String [] third = {"D", "T"};
-    private static String [] forth = {"L"};
-    private static String [] fifth = {"M", "N"};
-    private static String [] six = {"R"};
-    private static Map<String, Integer> map = new HashMap<String, Integer>();
     public static void main(String[] args) throws  Exception{
-//        Scanner sc = new Scanner(new FileReader("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/problemset/src/problem_set/in.txt"));
-        prepareMap();
-        Scanner sc = new Scanner(System.in);
-        printPair("NAME", "SOUNDEX CODE");
-        while (sc.hasNext()){
-            String line = sc.nextLine().trim();
-            String output = computeOutput(line);
-            printPair(line, output);
+        Scanner sc = new Scanner(new FileReader("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/problemset/src/problem_set/in.txt"));
+//        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0; i < testCase; i++){
+            String line = sc.nextLine();
+            BigInteger order = BigInteger.valueOf(Long.valueOf(sc.nextLine().trim()));
+            compute(line, order.add(BigInteger.ONE));
         }
-        System.out.println("                   END OF OUTPUT");
-
     }
 
-    private static void prepareMap() {
-        for(String s: first){
-            map.put(s, 1);
-        }
-        for(String s: second){
-            map.put(s, 2);
-        }
-        for(String s: third){
-            map.put(s, 3);
-        }
-        for(String s: forth){
-            map.put(s, 4);
-        }
-        for(String s: fifth){
-            map.put(s, 5);
-        }
-        for(String s: six){
-            map.put(s, 6);
-        }
+    private static void compute(String line, BigInteger order) {
+        char[] sortedChar = line.toCharArray();
+        Arrays.sort(sortedChar);
+        String sorted = new String(sortedChar);
 
-    }
+        String output = "";
+        while (output.length() < line.length()){
+            int currentLength = output.length();
+            BigInteger factor = getFactor(sorted.length() - 1);
+            BigInteger index = order.divide(factor);
 
-    private static String computeOutput(String input) {
-        String output = "" + input.charAt(0);
-        for(int i = 1; i < input.length(); i++){
-            if(map.containsKey(String.valueOf(input.charAt(i)))){
-                int number = map.get(String.valueOf(input.charAt(i)));
-
-                if(map.containsKey(String.valueOf(input.charAt(i -1)))){
-                    int previous = map.get(String.valueOf(input.charAt(i -1)));
-                    if(number == previous){
-                        continue;
-                    }
-                }
-                output += String.valueOf(number);
-
+            if(index.compareTo(BigInteger.valueOf(sorted.length())) > 0){
+                index = index.mod(BigInteger.valueOf(sorted.length()));
             }
-        }
-        while (output.length() < 4){
-            output += "0";
+
+            int indexToBeReplace = (index.intValue() - 1 >= 0) ? index.intValue() -1 : 0;
+            output += sorted.charAt(indexToBeReplace);
+            sorted = sorted.substring(0,indexToBeReplace) + sorted.substring(indexToBeReplace + 1);
+            order = order.subtract(factor.multiply(BigInteger.valueOf(indexToBeReplace)));
         }
 
-        return output.substring(0,4);
+        System.out.println(output);
     }
 
-    private static void printPair(String name, String output) {
-        String newLine = "         " + name;
-        while (newLine.length() < 34){
-            newLine += " ";
+    private static BigInteger getFactor(int currentLength) {
+        BigInteger result = BigInteger.ONE;
+        for(int i = 2; i <= currentLength; i++){
+            result = result.multiply(BigInteger.valueOf(i));
         }
-        newLine += output;
-        System.out.println(newLine);
+        return result;
     }
+
 
 }
