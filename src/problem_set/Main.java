d1fe963b0cd34a5f8d0b185118500522a777bf04
super1ha1 <khue0011@gmail.com>
2017-05-05 13:28:54 +0800

    update bottom up approach: TODO: cont

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -6,19 +6,19 @@ import java.util.*;
 public class Main {
 
     static final int LARGE_INT = -1000000000;
-    static int [][] memo, price;
+    static int [][] can_reach, price;
     static int M, C;
-    static Scanner sc = new Scanner(System.in);
+    //    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 
         int testCase = sc.nextInt();
         while (testCase-- > 0){
             M  = sc.nextInt();
             C = sc.nextInt();
-            memo = new int[210][25];
-            resetMemo(memo);
+            can_reach = new int[210][25];
+            resetMemo(can_reach);
             price = new int[25][25];
             for(int i = 0 ; i < C; i++){
                 price[i][0] = sc.nextInt();
@@ -46,22 +46,50 @@ public class Main {
         if(garmentId == C ){
             return M - moneyLeft;
         }
-        if(memo[moneyLeft][garmentId] != -1){
-            return memo[moneyLeft][garmentId];
+        if(can_reach[moneyLeft][garmentId] != -1){
+            return can_reach[moneyLeft][garmentId];
         }
         int maxValue = LARGE_INT;
         for(int model = 1 ; model <= price[garmentId][0]; model++){
             maxValue = Math.max(maxValue, shop(moneyLeft - price[garmentId][model], garmentId + 1));
         }
-        return (memo[moneyLeft][garmentId] = maxValue);
+        return (can_reach[moneyLeft][garmentId] = maxValue);
     }
 
     private static void processInput() {
-        int maxValue = shop(M, 0);
-        if(maxValue < 0){
+        fillTable();
+        int minValue = -1;
+        for(int i = 0; i <= M; i++){
+            if(can_reach[i][C-1] != -1){
+                minValue = i;
+            }
+        }
+        if(minValue < 0){
             System.out.println("no solution");
         }else {
-            System.out.println(maxValue);
+            System.out.println(M - minValue);
+        }
+    }
+
+    private static void fillTable(){
+        for(int model = 1 ; model <= price[0][0]; model++){
+            if(M - price[0][model] >= 0){
+                can_reach[M - price[0][model]][0] = 1;
+            }
+        }
+
+        for(int garmentId = 1; garmentId < C; garmentId++){
+            for(int possibleMoneyLeft = 0; possibleMoneyLeft <= M; possibleMoneyLeft++){
+                if(can_reach[possibleMoneyLeft][garmentId] != -1){
+                    for(int model = 1 ; model <= price[garmentId][0]; model++){
+                        if(M - price[garmentId][model] >= 0){
+                            can_reach[M - price[garmentId][model]][garmentId] = 1;
+                        }
+                    }
+                }
+
+            }
+
         }
     }
 
