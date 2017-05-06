dbb0f76e6102f38d7b6b9fd3ebed75a6284f5804
super1ha1 <khue0011@gmail.com>
2017-05-07 00:03:01 +0800

    UVA 154 Recycling

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -8,100 +8,86 @@ public class Main {
     static final int LARGE_INT = -1000000000;
     static int [][] can_reach, price;
     static int M, C;
-    //    static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-
-        int testCase = sc.nextInt();
-        while (testCase-- > 0){
-            M  = sc.nextInt();
-            C = sc.nextInt();
-            can_reach = new int[210][25];
-            resetMemo(can_reach);
-            price = new int[25][25];
-            for(int i = 0 ; i < C; i++){
-                price[i][0] = sc.nextInt();
-                for(int j = 1 ; j <= price[i][0]; j++){
-                    price[i][j] = sc.nextInt();
-                }
+//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        String line;
+        List<char[]> list = new ArrayList<char[]>();
+        while (sc.hasNext()){
+            line = sc.nextLine().trim();
+            if(line.equals("#")){
+                break;
             }
-            //            printInput(price);
-            processInput();
-        }
-    }
-
-    private static void resetMemo(int[][] memo) {
-        for(int i = 0; i < memo.length; i++){
-            for(int j = 0; j < memo[i].length; j++){
-                memo[i][j] = -1;
+            if(line.startsWith("e")){
+                process(list);
+                list.clear();
+            }else {
+                list.add(getInput(line));
             }
         }
     }
 
-    private static int shop(int moneyLeft, int garmentId){
-        if(moneyLeft < 0){
-            return LARGE_INT;
+    private static char[] getInput(String line) {
+        char[] array = new char[5];
+        String[] lines = line.split(",");
+        for(String s: lines){
+            char first = s.charAt(0);
+            switch (first){
+                case 'r':
+                    array[0] = s.charAt(2);
+                    break;
+                case 'o':
+                    array[1] = s.charAt(2);
+                    break;
+                case 'y':
+                    array[2] = s.charAt(2);
+                    break;
+                case 'g':
+                    array[3] = s.charAt(2);
+                    break;
+                case 'b':
+                    array[4] = s.charAt(2);
+                    break;
+                default:
+                    break;
+            }
         }
-        if(garmentId == C ){
-            return M - moneyLeft;
-        }
-        if(can_reach[moneyLeft][garmentId] != -1){
-            return can_reach[moneyLeft][garmentId];
-        }
-        int maxValue = LARGE_INT;
-        for(int model = 1 ; model <= price[garmentId][0]; model++){
-            maxValue = Math.max(maxValue, shop(moneyLeft - price[garmentId][model], garmentId + 1));
-        }
-        return (can_reach[moneyLeft][garmentId] = maxValue);
+        return array;
     }
 
-    private static void processInput() {
-        fillTable();
-        int minValue = -1;
-        for(int i = 0; i <= M; i++){
-            if(can_reach[i][C-1] != -1){
-                minValue = i;
+    private static void process(List<char[]> list) {
+
+
+        int minChange = Integer.MAX_VALUE;
+        int currentIndex = -1;
+        for(int i = 0 ; i < list.size(); i++){
+            char[] currentChars = list.get(i);
+            int value = getValue(currentChars, list);
+            if(value < minChange){
+                minChange = value;
+                currentIndex = i;
             }
         }
-        if(minValue < 0){
-            System.out.println("no solution");
-        }else {
-            System.out.println(M - minValue);
-        }
+        System.out.println(currentIndex + 1);
     }
 
-    private static void fillTable(){
-        for(int model = 1 ; model <= price[0][0]; model++){
-            if(M - price[0][model] >= 0){
-                can_reach[M - price[0][model]][0] = 1;
-            }
+    private static int getValue(char[] currentChars, List<char[]> list) {
+        int count = 0 ;
+        for(char[] chars: list){
+            count += compare2Chars(currentChars, chars);
         }
-
-        for(int garmentId = 1; garmentId < C; garmentId++){
-            for(int possibleMoneyLeft = 0; possibleMoneyLeft <= M; possibleMoneyLeft++){
-                if(can_reach[possibleMoneyLeft][garmentId] != -1){
-                    for(int model = 1 ; model <= price[garmentId][0]; model++){
-                        if(M - price[garmentId][model] >= 0){
-                            can_reach[M - price[garmentId][model]][garmentId] = 1;
-                        }
-                    }
-                }
-
-            }
-
-        }
+        return count;
     }
 
-    private static void printInput(int[][] input) {
-        String output = "";
-        for(int i = 0 ; i < input.length; i++){
-            output = "";
-            for(int j = 0; j < input[i].length; j++){
-                output += " " + input[i][j];
+    private static int compare2Chars(char[] currentChars, char[] chars) {
+        int count = 0;
+        for(int i = 0; i < currentChars.length; i++){
+            if(currentChars[i] != chars[i]){
+                count++;
             }
-            System.out.println(output);
         }
+        return count;
     }
 
 }
