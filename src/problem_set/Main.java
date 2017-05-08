6120deb8bcb61ed7963600421287a41fb541fc17
super1ha1 <khue0011@gmail.com>
2017-05-08 13:16:05 +0800

    UVA 193: Graph coloring: not finished

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -5,85 +5,109 @@ import java.util.*;
 
 public class Main {
 
-    static Scanner sc = new Scanner(System.in);
-    static List<Integer> list = new ArrayList<Integer>();
-//    static List<Set<Integer>> setList = new ArrayList<Set<Integer>>();
+    static final int LARGE_INT = -1000000000;
+    static int [][] can_reach, price;
+    static int n, k, sum = 0;
+    static List<List<Integer>> adList = new ArrayList<List<Integer>>();
+    static List<Integer> resultList = new ArrayList<Integer>();
+    static List<Integer> tempList = new ArrayList<Integer>();
+    static int[] visisted;
+    //    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        int n = sc.nextInt();
-        while (n > 0){
-            list.clear();
-//            setList.clear();
-            for(int i = 0; i < n; i++){
-                list.add(sc.nextInt());
-            }
-            Collections.sort(list);
-            process();
+        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+
+        int testCase = sc.nextInt();
+        while (testCase-- > 0){
             n = sc.nextInt();
-            if(n == 0){
-                break;
-            }else {
-                System.out.println("");
+            k = sc.nextInt();
+            adList.clear();
+            visisted = new int[n+1];
+            for(int i = 0 ; i <= n ; i++){
+                List<Integer> newList = new ArrayList<Integer>();
+                adList.add(newList);
             }
-        }
-    }
-
-    private static void process() {
-        List<Integer> left = new ArrayList<Integer>();
-        recursive(left, list);
-    }
-
-    private static List<Integer> getList(List<Integer> list, int index) {
-        List<Integer> newList = new ArrayList<Integer>();
-        for(int i = 0; i <= list.size() - index; i++){
-            newList.add(list.get(i));
-        }
-        return newList;
-    }
-
-    private static boolean recursive(List<Integer> currentList, List<Integer> left) {
-        if(currentList.size() == 6){
-            printList(currentList);
-            return true;
-        }
-        if(left.size() == 0){
-            return false;
-        }
-        for(int i = 0 ; i < left.size(); i++){
-            int member = left.get(i);
-            if(currentList.size() > 0 && member < currentList.get(currentList.size() - 1)){
-                continue;
+            for(int i = 0 ; i < k; i++){
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                adList.get(first).add(second);
+                adList.get(second).add(first);
             }
-            currentList.add(member);
-            left.remove(i);
-            if(recursive(currentList, left)){
-
-            }else {
-
-            }
-            currentList.remove(currentList.size() - 1);
-            left.add(i, member);
+            processInput();
+            printResult();
         }
-        return false;
     }
 
-    private static void printList(List<Integer> currentList) {
-//        Set<Integer> currentSet = new HashSet<Integer>();
-//        currentSet.addAll(currentList);
-//
-//        for(Set<Integer> set: setList){
-//            if(set.containsAll(currentSet)){
-//                return;
-//            }
-//        }
-//        setList.add(currentSet);
-
+    private static void printResult() {
+        Collections.sort(resultList);
+        System.out.println(resultList.size());
         String output = "";
-        for(int i: currentList){
+        for(int i: resultList){
             output += i + " ";
         }
         System.out.println(output.trim());
     }
+
+
+
+    private static void processInput() {
+        int maxValue = Integer.MIN_VALUE;
+        List<Integer> currentList = new ArrayList<Integer>();
+        for(int i = 1; i < adList.size(); i++){
+
+            tempList.clear();
+            currentList.clear();
+
+            recursive(i, currentList);
+
+            if(tempList.size() > maxValue){
+                maxValue = tempList.size();
+                resultList.clear();
+                resultList.addAll(tempList);
+
+                //take all alone node
+                for(int j = 1 ; j <= n; j++){
+                    if(visisted[j] != 1){
+                        resultList.add(j);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static void recursive(int nodeIndex, List<Integer> currentList) {
+        if(currentList.size() == n || visisted[nodeIndex] == 1 ){
+            return;
+        }
+
+        visisted[nodeIndex] = 1;
+        currentList.add(nodeIndex);
+
+        if(okToMark(nodeIndex)){
+            tempList.add(nodeIndex);
+        }else {
+
+        }
+
+        for(int neighbor: adList.get(nodeIndex)){
+            if(visisted[neighbor] != 1){
+                //mark as black
+                recursive(neighbor, currentList);
+            }
+        }
+
+    }
+
+    private static boolean okToMark(int nodeIndex) {
+        for(int neighbor: adList.get(nodeIndex)){
+            if(visisted[neighbor] == 1 && tempList.contains(neighbor)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
 
