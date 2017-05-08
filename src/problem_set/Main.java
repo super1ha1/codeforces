c784ec9de6a802be9e1b89cd6d32372a12bbfb19
super1ha1 <khue0011@gmail.com>
2017-05-08 23:21:10 +0800

    cont refactor code

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -7,22 +7,24 @@ public class Main {
 
     static final int LARGE_INT = -1000000000;
     static int [][] can_reach, price;
-    static int n, k, sum = 0;
+    static int n, k, currentIndex, maxValue;
     static List<List<Integer>> adList = new ArrayList<List<Integer>>();
     static List<Integer> resultList = new ArrayList<Integer>();
-    static List<Integer> tempList = new ArrayList<Integer>();
+    static List<Integer> blackList = new ArrayList<Integer>();
+    static List<Integer> whiteList = new ArrayList<Integer>();
+    static List<Integer> notConnectList = new ArrayList<Integer>();
     static int[] visisted;
     //    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(new File("/Users/macbook/workspace/codeforces/in.txt"));
 
         int testCase = sc.nextInt();
         while (testCase-- > 0){
             n = sc.nextInt();
             k = sc.nextInt();
             adList.clear();
-            visisted = new int[n+1];
             for(int i = 0 ; i <= n ; i++){
                 List<Integer> newList = new ArrayList<Integer>();
                 adList.add(newList);
@@ -33,12 +35,20 @@ public class Main {
                 adList.get(first).add(second);
                 adList.get(second).add(first);
             }
+
+            for(int i = 1; i < adList.size(); i++){
+                if(adList.get(i).size() == 0){
+                    notConnectList.add(i);
+                }
+            }
+
             processInput();
             printResult();
         }
     }
 
     private static void printResult() {
+        resultList.addAll(notConnectList);
         Collections.sort(resultList);
         System.out.println(resultList.size());
         String output = "";
@@ -51,49 +61,53 @@ public class Main {
 
 
     private static void processInput() {
-        int maxValue = Integer.MIN_VALUE;
-        List<Integer> currentList = new ArrayList<Integer>();
+        maxValue = Integer.MIN_VALUE;
         for(int i = 1; i < adList.size(); i++){
 
-            tempList.clear();
-            currentList.clear();
+            blackList.clear();
+            whiteList.clear();
+            visisted = new int[n+1];
 
-            recursive(i, currentList);
+            currentIndex = i;
+            recursive(i);
 
-            if(tempList.size() > maxValue){
-                maxValue = tempList.size();
-                resultList.clear();
-                resultList.addAll(tempList);
-
-                //take all alone node
-                for(int j = 1 ; j <= n; j++){
-                    if(visisted[j] != 1){
-                        resultList.add(j);
-                    }
-                }
-            }
         }
 
     }
 
-    private static void recursive(int nodeIndex, List<Integer> currentList) {
-        if(currentList.size() == n || visisted[nodeIndex] == 1 ){
+    private static void recursive(int nodeIndex) {
+        if(blackList.size() + whiteList.size() + notConnectList.size() == n){
+            if(blackList.size() >= maxValue){
+                maxValue = blackList.size();
+                resultList.clear();
+                resultList.addAll(blackList);
+            }
+        }
+        if(visisted[nodeIndex] == 1 ){
             return;
         }
 
         visisted[nodeIndex] = 1;
-        currentList.add(nodeIndex);
 
-        if(okToMark(nodeIndex)){
-            tempList.add(nodeIndex);
-        }else {
-
-        }
-
-        for(int neighbor: adList.get(nodeIndex)){
-            if(visisted[neighbor] != 1){
-                //mark as black
-                recursive(neighbor, currentList);
+        for(int i = 0; i <= 1; i++){
+            if(i == 0){
+                blackList.add(nodeIndex);
+                for(int neighbor: adList.get(nodeIndex)){
+                    if(visisted[neighbor] != 1){
+                        //mark as black
+                        recursive(neighbor);
+                    }
+                }
+                blackList.remove(Integer.valueOf(nodeIndex));
+            }else {
+                whiteList.add(nodeIndex);
+                for(int neighbor: adList.get(nodeIndex)){
+                    if(visisted[neighbor] != 1){
+                        //mark as black
+                        recursive(neighbor);
+                    }
+                }
+                whiteList.remove(Integer.valueOf(nodeIndex));
             }
         }
 
@@ -101,7 +115,7 @@ public class Main {
 
     private static boolean okToMark(int nodeIndex) {
         for(int neighbor: adList.get(nodeIndex)){
-            if(visisted[neighbor] == 1 && tempList.contains(neighbor)){
+            if(visisted[neighbor] == 1 && blackList.contains(neighbor)){
                 return false;
             }
         }
