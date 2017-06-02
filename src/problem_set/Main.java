a692d370e5267a646fc2f6b3ef71819cda234cf9
super1ha1 <khue0011@gmail.com>
2017-06-02 16:14:52 +0800

    update quiz challenge

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -12,44 +12,113 @@ public class Main {
     static List<Integer> resultList = new ArrayList<Integer>();
     static List<Integer> tempList = new ArrayList<Integer>();
     static List<Integer> notConnectedList = new ArrayList<Integer>();
-    static int[] visisted;
+    static int[][] visit;
+    static int min = Integer.MAX_VALUE;
     //    static Scanner sc = new Scanner(System.in);
 
     private static Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-        int count = 1;
-        while (sc.hasNext()){
-            int numberOfPair = sc.nextInt();
-            if(numberOfPair == 0){
-                break;
-            }
-            map.clear();
-            while (numberOfPair-- > 0){
-                int first = sc.nextInt();
-                int second = sc.nextInt();
-                if(!map.keySet().contains(first)) {
-                    map.put(first, new ArrayList<Integer>());
-                }
-                map.get(first).add(second);
-
-                if(!map.keySet().contains(second)){
-                    map.put(second, new ArrayList<Integer>());
-                }
-                map.get(second).add(first);
-            }
-
-            while (true){
-                int start = sc.nextInt();
-                int ttl = sc.nextInt();
-                if(start == 0 && ttl == 0){
-                    break;
-                }
-                process(start, ttl, count++);
-            }
-        }
+//        int [] input = {2,3,6};
+//        int [] input = {4,1,2};
+//        int [] input = {3,3,8};
+//        int [] input = {5,2,8};
+        int [] input = {3,5,14};
+        calculate(input[0], input[1], input[2]);
+        System.out.println(min);
     }
 
+    private static void calculate(int row, int col, int people) {
+        int[][] visit = new int[row][col];
+        backTrack(visit, people);
+    }
+
+    private static boolean backTrack(int [][] visit, int people) {
+        if(people == 0 || full(visit)){
+            min = Math.min(min, takeMin(visit));
+            return true;
+        }
+        for(int i = 0; i < visit.length; i++){
+            for(int j = 0; j < visit[0].length; j++){
+                if(visit[i][j] == 0){
+                    visit[i][j] = 1;
+                    if(takeMin(visit) < min){
+                        if(backTrack(visit, people -1)){
+
+                        }
+                    }
+                    visit[i][j] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean full(int[][] visit) {
+        for(int i = 0; i < visit.length; i++){
+            for(int j = 0; j < visit[0].length; j++){
+                if(visit[i][j] == 0){
+                   return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static int takeMin(int[][] visit) {
+        Set<Point> set = new HashSet<Point>();
+        for(int i = 0; i < visit.length; i++){
+            for(int j = 0; j < visit[0].length; j++){
+                if(visit[i][j] == 1){
+                    int top = i > 0 ? i-1 : 0;
+                    int bottom = i + 1 < visit.length ? i+ 1 : i;
+                    int left = j > 0 ? j-1 : 0;
+                    int right = j + 1 < visit[0].length ? j + 1 : j;
+                    for(int row = top; row <= bottom; row++){
+                        for(int col = left; col <= right; col++){
+                            if(!(row == i && col == j) && !((Math.abs(row - i) == 1 && Math.abs(col - j) == 1)) && visit[row][col] == 1){
+                                set.add(new Point(i, j, row, col));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return set.size();
+    }
+
+    static class Point {
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+
+        public Point(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.x1 + this.x2 + this.y1 + this.y2;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == this) return true;
+            if ((obj == null) || (obj.getClass() != this.getClass()))
+                return false;
+            Point that = (Point) obj;
+
+            return (
+                            that.x1 == this.x1 &&
+                            that.x2 == this.x2 &&
+                            that.y1 == this.y1 &&
+                            that.y2 == this.y2
+            );
+        }
+    }
     private static void process(int start, int ttl, int count) {
         int node = getCount(start, ttl);
         System.out.println("Case " + count + ": " + node + " nodes not reachable from node " + start + " with TTL = " + ttl + ".");
