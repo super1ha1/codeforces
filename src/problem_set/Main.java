d149223743e298f1524cc014bf98a27018a65fe4
super1ha1 <khue0011@gmail.com>
2017-06-06 13:02:04 +0800

    use greedy approach: the next point of each step is the one with minimum current point: not work in all case: e.g 3,3,8 > the middle point is selected > need to use dynamic programming (update from complete search) [x,y,a] = [x,y,a-1] + best place to put 1 more

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -12,23 +12,27 @@ public class Main {
     static List<Integer> resultList = new ArrayList<Integer>();
     static List<Integer> tempList = new ArrayList<Integer>();
     static List<Integer> notConnectedList = new ArrayList<Integer>();
-    static int[][] visit;
-    static int min = Integer.MAX_VALUE;
+    static int[][] track;
+    static int min;
+    static int previousMin = 0;
     //    static Scanner sc = new Scanner(System.in);
 
     private static Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
     public static void main(String[] args) throws Exception {
 //        int [] input = {2,3,6};
 //        int [] input = {4,1,2};
-//        int [] input = {3,3,8};
+        int [] input = {3,3,8};
 //        int [] input = {5,2,8};
-        int [] input = {3,5,14};
+//        int [] input = {3,5,14};
+//        int [] input = {1,16,1};
         calculate(input[0], input[1], input[2]);
         System.out.println(min);
     }
 
     private static void calculate(int row, int col, int people) {
+        min = Integer.MAX_VALUE;
         int[][] visit = new int[row][col];
+        track = new int[row][col];
         backTrack(visit, people);
     }
 
@@ -37,20 +41,54 @@ public class Main {
             min = Math.min(min, takeMin(visit));
             return true;
         }
+        int[] nextPoint = getNextPoint(visit);
+        visit[nextPoint[0]][nextPoint[1]] = 1;
+        updateTrack(visit);
+        if(backTrack(visit, people -1)){
+            return true;
+        }
+        visit[nextPoint[0]][nextPoint[1]] = 0;
+
+        return false;
+    }
+
+    private static void updateTrack(int[][] visit) {
         for(int i = 0; i < visit.length; i++){
             for(int j = 0; j < visit[0].length; j++){
-                if(visit[i][j] == 0){
-                    visit[i][j] = 1;
-                    if(takeMin(visit) < min){
-                        if(backTrack(visit, people -1)){
+                track[i][j] = getSum(visit, i, j);
+            }
+        }
+    }
 
-                        }
-                    }
-                    visit[i][j] = 0;
+    private static int getSum(int[][] visit, int i, int j) {
+        int sum = 0;
+        int top = i > 0 ? i-1 : 0;
+        int bottom = i + 1 < track.length ? i+ 1 : i;
+        int left = j > 0 ? j-1 : 0;
+        int right = j + 1 < track[0].length ? j + 1 : j;
+        for(int row = top; row <= bottom; row++){
+            for(int col = left; col <= right; col++){
+                if(!(row == i && col == j) && !((Math.abs(row - i) == 1 && Math.abs(col - j) == 1)) && visit[row][col] == 1){
+                    sum++;
                 }
             }
         }
-        return false;
+        return sum;
+    }
+
+    private static int[] getNextPoint(int [][]visit) {
+        int minCount = Integer.MAX_VALUE;
+        int[] result = new int[2];
+        for(int i = 0; i < track.length; i++){
+            for(int j = 0; j < track[0].length; j++){
+                if(visit[i][j] == 0 && track[i][j] <= minCount){
+                    minCount = track[i][j];
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
     }
 
     private static boolean full(int[][] visit) {
