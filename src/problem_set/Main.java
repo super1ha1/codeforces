6e0ac1c0b24ba23601a2089dc285d4c8232054ea
super1ha1 <khue0011@gmail.com>
2017-07-31 13:00:39 +0800

    cont: UVA 639

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,6 +1,5 @@
 package problem_set;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,43 +8,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
 //        sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 
-        int count = 0;
         while (true){
-            int N = sc.nextInt();
-            if(N == 0){
+            int n = sc.nextInt();
+            if(n == 0){
                 break;
             }
-            if(count > 0){
-                System.out.println("");
+            sc.nextLine();
+            char[][] array = new char[n][n];
+            for(int i = 0; i < n; i++){
+                String line = sc.nextLine();
+                array[i] = line.trim().toCharArray();
             }
-            process(N);
-            count++;
+            System.out.println(process(array));
         }
     }
 
-    private static void process(int N) {
-        boolean hasResult = false;
-        for(int i = 1234; i <= 98765; i++){
-            int j = i * N;
-            if(j > 98765){
-                break;
-            }
-            if(check(i, j)){
-                hasResult = true;
-                System.out.println(String.format("%05d / %05d = %d", j, i, N));
-            }
-        }
-        if(!hasResult){
-            System.out.println(String.format("There are no solutions for %d.", N));
-        }
+    private static int process(char[][] array) {
+        int len = array.length;
+        return 0;
     }
 
-    private static boolean check(int i, int j) {
-        String figure = String.format("%05d%05d", i, j);
-        char[] array = figure.toCharArray();
-        Arrays.sort(array);
-        String sortedStr = new String(array);
-        return "0123456789".equals(sortedStr);
-    }
 }
 
