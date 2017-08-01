91badfa7a28c57cf2c6e47b7d6ea91a7d692d173
super1ha1 <khue0011@gmail.com>
2017-08-01 23:18:59 +0800

    UVA 11223	O: dah dah dah! note: end of word not always append space (if end of sentence) prepare the mapping correctly (easy to do it wrong)

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,56 +1,106 @@
 package problem_set;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    private static Map<String, Character> map = createMap();
 
-    private static int[][] result;
+    private static Map<String,Character> createMap() {
+        Map<String, Character> map = new HashMap<String, Character>();
+        map.put(".-",'A');
+        map.put(".---",'J');
+        map.put("...",'S');
+        map.put(".----",'1');
+        map.put(".-.-.-",'.');
+        map.put("---...",':');
+        map.put("-...",'B');
+        map.put("-.-",'K');
+        map.put("-",'T');
+        map.put("..---",'2');
+        map.put("--..--",',');
+        map.put("-.-.-.",';');
+        map.put("-.-.",'C');
+        map.put(".-..",'L');
+        map.put("..-",'U');
+        map.put("...--",'3');
+        map.put("..--..",'?');
+        map.put("-...-",'=');
+        map.put("-..",'D');
+        map.put("--",'M');
+        map.put("...-",'V');
+        map.put("....-",'4');
+        map.put(".----.",'\'');
+        map.put(".-.-.",'+');
+        map.put(".",'E');
+        map.put("-.",'N');
+        map.put(".--",'W');
+        map.put(".....",'5');
+        map.put("-.-.--",'!');
+        map.put("-....-",'-');
+        map.put("..-.",'F');
+        map.put("---",'O');
+        map.put("-..-",'X');
+        map.put("-....",'6');
+        map.put("-..-.",'/');
+        map.put("..--.-",'_');
+        map.put("--.",'G');
+        map.put(".--.",'P');
+        map.put("-.--",'Y');
+        map.put("--...",'7');
+        map.put("-.--.",'(');
+        map.put(".-..-.",'"');
+        map.put("....",'H');
+        map.put("--.-",'Q');
+        map.put("--..",'Z');
+        map.put("---..",'8');
+        map.put("-.--.-",')');
+        map.put(".--.-.",'@');
+        map.put("..",'I');
+        map.put(".-.",'R');
+        map.put("-----",'0');
+        map.put("----.",'9');
+        map.put(".-...",'&');
+        return map;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
         int testCase = sc.nextInt();
+        sc.nextLine();
         for(int i = 0; i < testCase; i++){
-            result = new int[1025][1025];
-            int d = sc.nextInt();
-            int n = sc.nextInt();
-            for(int j = 0; j < n; j++){
-                int x  = sc.nextInt();
-                int y = sc.nextInt();
-                int size = sc.nextInt();
-                update(result, x, y,  size, d);
+            String line = sc.nextLine();
+            process(line, i + 1);
+            if(i + 1 < testCase){
+                System.out.println("");
             }
-            process(result);
         }
     }
 
-    private static void process(int[][] result) {
-        int maxValue = -1;
-        int currentX = -1, currentY = -1;
-        for(int i = 0; i < result.length; i++){
-            for(int j = 0; j < result[0].length; j++){
-                if(result[i][j] > maxValue){
-                    maxValue = result[i][j];
-                    currentX = i;
-                    currentY = j;
+    private static void process(String line, int n) {
+        String output = "";
+        String[] splitWord = line.split("  ");
+        for(int i  = 0; i < splitWord.length; i++){
+            String word = splitWord[i];
+            String[] chars = word.split(" ");
+            for(int j = 0; j < chars.length; j++){
+                String c = chars[j];
+                if(map.keySet().contains(c)){
+                    output += map.get(c);
+                }else {
+//                    System.out.println("Not recognize: index: " + j +  " str: " + c);
                 }
             }
-        }
-        System.out.println(currentX + " " + currentY + " " + maxValue);
-    }
-
-    private static void update(int[][] result, int x, int y, int size, int d) {
-        int lowX = x - d >= 0 ? x - d : 0;
-        int highX = x + d <= 1024 ? x + d : 1024;
-        int lowY = y - d >= 0 ? y - d : 0;
-        int highY = y + d <= 1024 ? y + d : 1024;
-        for(int i = lowX; i <= highX ; i++){
-            for(int j = lowY; j <= highY; j ++){
-                result[i][j] += size;
+            if(i + 1 < splitWord.length){
+                output += " ";
             }
         }
-
+        System.out.println("Message #" + n);
+        System.out.println(output);
     }
 
 }
