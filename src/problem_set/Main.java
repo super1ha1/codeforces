7f3ed4f379e8e11d50ce1ae722b303574aab0a79
super1ha1 <khue0011@gmail.com>
2017-08-10 23:31:27 +0800

    solve UVA 344	Roman Digititis convert from int to roman number from 1 to 100

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,64 +1,111 @@
 package problem_set;
 
+import java.io.File;
 import java.util.*;
 
 public class Main {
 
-    private static int[] colors;
-    private static List<Integer> resultList = new ArrayList<>();
-    private static Map<Integer, List<String>> map = new HashMap<>();
-
+    private static Map<Integer, Map<Character, Integer>> map = new HashMap<>();
+    private static List<Character> list = Arrays.asList('i','v','x','l','c');
+    private static int[][] totalChar = new int[101][5];
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
+        prepareMap();
         int M = sc.nextInt();
-        while (M-- > 0){
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            sc.nextLine();
-            map.clear();
-            for(int i = 0; i < m; i++){
-                processEachLine(sc.nextLine());
-            }
-            sortMap(map);
-            if(M > 0){
-                System.out.println("");
+        while (M > 0){
+            printNumberEachChar(M);
+            M = sc.nextInt();
+        }
+    }
+
+    private static void prepareMap() {
+        for(int i = 1; i <= 100; i++){
+            map.put(i, prepareEachValue(i));
+        }
+        for(int i = 1; i <= 100; i++){
+            for(int j = 0; j < 5; j++){
+                totalChar[i][j] = totalChar[i-1][j] + map.get(i).get(list.get(j));
             }
         }
     }
 
-    private static void sortMap(Map<Integer, List<String>> map) {
-        List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list);
-        for(int i = 0; i < list.size(); i++){
-            for(String s: map.get(list.get(i))){
-                System.out.println(s);
+    private static Map<Character, Integer> prepareEachValue(int value) {
+        Map<Character, Integer> currentMap = new HashMap<>();
+        for(char c: list){
+            currentMap.put(c, 0);
+        }
+        int firstDigit = value / 10;
+        int secondDigit = value % 10;
+
+        String romanValue = getFirstValue(firstDigit) + getSeconValue(secondDigit);
+
+        char[] array = romanValue.toCharArray();
+        for(char c: array){
+            if(currentMap.keySet().contains(c)){
+                int currentValue = currentMap.get(c);
+                currentMap.put(c, currentValue + 1);
             }
         }
+        return currentMap;
     }
 
-    private static void processEachLine(String line) {
-        int value = getSortedness(line);
-        if(!map.keySet().contains(value)){
-            map.put(value, new ArrayList<>());
+    private static String getSeconValue(int secondDigit) {
+        switch (secondDigit){
+            case 1:
+                return "i";
+            case 2:
+                return "ii";
+            case 3:
+                return "iii";
+            case 4:
+                return "iv";
+            case 5:
+                return "v";
+            case 6:
+                return "vi";
+            case 7:
+                return "vii";
+            case 8:
+                return "viii";
+            case 9:
+                return "ix";
         }
-        map.get(value).add(line);
+        return "";
     }
 
-    private static int getSortedness(String line) {
-        char[] array = line.trim().toCharArray();
-        int count = 0;
-        for(int i = 0; i < array.length - 1; i++){
-            for(int j = i + 1; j < array.length; j++){
-                if(array[j] < array[i]){
-                    count++;
-                }
-            }
+    private static String getFirstValue(int firstDigit) {
+        switch (firstDigit){
+            case 1:
+                return "x";
+            case 2:
+                return "xx";
+            case 3:
+                return "xxx";
+            case 4:
+                return "xl";
+            case 5:
+                return "l";
+            case 6:
+                return "lx";
+            case 7:
+                return "lxx";
+            case 8:
+                return "lxxx";
+            case 9:
+                return "xc";
+            case 10:
+                return "c";
         }
-        return count;
+        return "";
+    }
+
+    private static void printNumberEachChar(int M) {
+        System.out.println(String.format("%d: %d i, %d v, %d x, %d l, %d c", M, totalChar[M][0],
+                totalChar[M][1], totalChar[M][2], totalChar[M][3], totalChar[M][4]));
     }
 
 }
