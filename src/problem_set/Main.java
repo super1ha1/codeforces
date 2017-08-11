6c4cd14134d74e5753f63a17a10dc761778f954b
super1ha1 <khue0011@gmail.com>
2017-08-11 12:50:23 +0800

    solve UVA 10346Peter's Smokes: note the left over of all previous cigarette

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,6 +1,5 @@
 package problem_set;
 
-import java.io.File;
 import java.util.*;
 
 public class Main {
@@ -13,100 +12,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-
-        prepareMap();
-        int M = sc.nextInt();
-        while (M > 0){
-            printNumberEachChar(M);
-            M = sc.nextInt();
-        }
-    }
-
-    private static void prepareMap() {
-        for(int i = 1; i <= 100; i++){
-            map.put(i, prepareEachValue(i));
-        }
-        for(int i = 1; i <= 100; i++){
-            for(int j = 0; j < 5; j++){
-                totalChar[i][j] = totalChar[i-1][j] + map.get(i).get(list.get(j));
+        while (sc.hasNext()){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int sum = n;
+            int left = n / k;
+            int leftOver = n - left * k;
+            while (left > 0){
+                sum += left;
+                left = left + leftOver;
+                int newLeft = left/k;
+                leftOver = left - newLeft * k;
+                left = newLeft;
             }
+            System.out.println(sum);
         }
     }
-
-    private static Map<Character, Integer> prepareEachValue(int value) {
-        Map<Character, Integer> currentMap = new HashMap<>();
-        for(char c: list){
-            currentMap.put(c, 0);
-        }
-        int firstDigit = value / 10;
-        int secondDigit = value % 10;
-
-        String romanValue = getFirstValue(firstDigit) + getSeconValue(secondDigit);
-
-        char[] array = romanValue.toCharArray();
-        for(char c: array){
-            if(currentMap.keySet().contains(c)){
-                int currentValue = currentMap.get(c);
-                currentMap.put(c, currentValue + 1);
-            }
-        }
-        return currentMap;
-    }
-
-    private static String getSeconValue(int secondDigit) {
-        switch (secondDigit){
-            case 1:
-                return "i";
-            case 2:
-                return "ii";
-            case 3:
-                return "iii";
-            case 4:
-                return "iv";
-            case 5:
-                return "v";
-            case 6:
-                return "vi";
-            case 7:
-                return "vii";
-            case 8:
-                return "viii";
-            case 9:
-                return "ix";
-        }
-        return "";
-    }
-
-    private static String getFirstValue(int firstDigit) {
-        switch (firstDigit){
-            case 1:
-                return "x";
-            case 2:
-                return "xx";
-            case 3:
-                return "xxx";
-            case 4:
-                return "xl";
-            case 5:
-                return "l";
-            case 6:
-                return "lx";
-            case 7:
-                return "lxx";
-            case 8:
-                return "lxxx";
-            case 9:
-                return "xc";
-            case 10:
-                return "c";
-        }
-        return "";
-    }
-
-    private static void printNumberEachChar(int M) {
-        System.out.println(String.format("%d: %d i, %d v, %d x, %d l, %d c", M, totalChar[M][0],
-                totalChar[M][1], totalChar[M][2], totalChar[M][3], totalChar[M][4]));
-    }
-
 }
 
