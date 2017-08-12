012397eef90f3fd2ef63ab8930fe57b0c3840728
super1ha1 <khue0011@gmail.com>
2017-08-12 23:46:58 +0800

    codeforces http://codeforces.com/contest/839/problem/A : note reset sum

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,5 +1,6 @@
 package problem_set;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -10,29 +11,35 @@ public class Main {
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        while (testCase-- > 0){
+        while (sc.hasNext()){
             int n = sc.nextInt();
-            int m = sc.nextInt();
+            int k = sc.nextInt();
+            int [] array = new int[n];
+            for(int i  = 0; i < n; i++){
+                array[i] = sc.nextInt();
+            }
+            int currentK = k;
+            int left = 0;
+            int days = 0;
             int sum = 0;
-            int round = n/m;
-            int left = n - round * m;
-            int newTotal = round + left;
-            sum += round;
-            while (newTotal > 1){
-                round = newTotal / m;
-                left = newTotal - round * m;
-                newTotal = round + left;
-                sum += round;
-                if(newTotal > 1 && newTotal < m){
+            for(int i  = 0; i < n; i++){
+                sum = array[i] + left;
+                if(sum > 8){
+                    currentK = currentK - 8;
+                    left =  (sum - 8);
+                }else {
+                    currentK = currentK - sum;
+                    left = 0;
+                }
+                if(currentK <= 0){
+                    days = i;
                     break;
                 }
             }
-
-            if(newTotal == 1){
-                System.out.println(sum);
+            if(currentK <= 0){
+                System.out.println(days + 1);
             }else {
-                System.out.println("cannot do this");
+                System.out.println("-1");
             }
         }
     }
