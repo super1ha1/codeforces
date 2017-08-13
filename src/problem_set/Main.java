e0989942a3290d58bde1ee479ceb585cffab2a5b
super1ha1 <khue0011@gmail.com>
2017-08-13 17:14:40 +0800

    solve UVA 11428 Cubes: do a complete search for all number divisible by N

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -2,88 +2,69 @@ package problem_set;
 
 import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
     private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static Map<Integer, Node> result = new HashMap<>();
-    private static List<Node> list = new ArrayList<>();
+    private static Set<Integer> resultSet = new HashSet<>();
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-
         while (sc.hasNext()){
-            int n = sc.nextInt();
-            map.clear();
-            result.clear();
-            list.clear();
-
-            for(int i = 1; i <= n; i++){
-                map.put(i, new ArrayList<>());
+            int n  = sc.nextInt();
+            if(n == 0){
+                break;
             }
-            for(int i  = 0; i < n-1; i++){
-                int first = sc.nextInt();
-                int second = sc.nextInt();
-                map.get(first).add(second);
-                map.get(second).add(first);
+            resultSet.clear();
+            calculate(n);
+            if(resultSet.size() > 0){
+                int min = Integer.MAX_VALUE;
+                for(int i: resultSet){
+                    if(i < min){
+                        min = i;
+                    }
+                }
+                int x = (int) Math.cbrt(n + min * min * min);
+                System.out.println(x + " " + min);
+            }else {
+                System.out.println("No solution");
             }
-
-            dfs(map, n);
         }
     }
 
-    private static void dfs(Map<Integer, List<Integer>> map, int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        int[] visited = new int[n + 1];
-        result.put(1, new Node(1, 1.0, 0));
-        while (!queue.isEmpty()){
-            int currentIndex = queue.poll();
-            if(visited[currentIndex] == 0){
-                visited[currentIndex] = 1;
-                Node currentNode = result.get(currentIndex);
-
-                List<Integer> connected = map.get(currentIndex);
-                List<Integer> tobeAdd = connected.stream().filter(i -> visited[i] == 0).collect(Collectors.toList());
-
-                int size = tobeAdd.size();
-                if(size == 0){
-                    list.add(currentNode);
-                }
-
-                double nextProb = currentNode.prob *  1.0 / size;
-                double nextValue = currentNode.value + 1;
-
-                for(int i: tobeAdd){
-                    result.put(i, new Node(i, nextProb, nextValue));
-                }
-
-                queue.addAll(tobeAdd);
+    private static void calculate(int n) {
+        Set<Integer> divisibleByN = new HashSet<>();
+        int square = (int) Math.sqrt(n);
+        for(int i = 1; i <= square; i++){
+            if(n % i == 0){
+                divisibleByN.add(i);
+                divisibleByN.add(n/i);
             }
         }
-        double result = 0.0;
-        for(Node node: list){
-            result += node.prob * node.value;
+        for(int A: divisibleByN){
+            int D = n/A;
+            makeXY(A, D);
         }
-        System.out.println(String.format("%.15f", result));
     }
 
-    private static class Node {
-        public int node;
-        public double prob;
-        public double value;
-        public Node(){
-
+    private static void makeXY(int a, int d) {
+        int threeB = d - a * a;
+        if(threeB < 0 || threeB % 3 != 0){
+            return;
         }
-
-        public Node(int node, double prob, double value){
-            this.node = node;
-            this.prob = prob;
-            this.value = value;
+        int B = threeB/3;
+        int delta = a * a + 4 * B;
+        double sqrt = Math.sqrt(delta);
+        int intsqrt = (int) sqrt;
+        if(intsqrt  * 1.0 != sqrt){
+            return;
+        }
+        if((intsqrt - a > 0) && (intsqrt - a) % 2 == 0){
+            int firstY = (int) (intsqrt - a)/2;
+            resultSet.add(firstY);
         }
     }
 }
