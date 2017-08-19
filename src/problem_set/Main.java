89654a365c706f67c84c647e90597f7b62f491fe
super1ha1 <khue0011@gmail.com>
2017-08-19 10:49:42 +0800

    not solve yet http://codeforces.com/contest/841/problem/B Godsend take longest subsequent sum odd or even

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,112 +1,73 @@
 package problem_set;
 
+import com.sun.xml.internal.bind.v2.util.QNameMap;
+
 import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    private static int component, h, w;
-    private static Map<Integer, List<Map.Entry>> resultMap = new HashMap<>();
     public static void main(String[] args) throws Exception {
 
 //        Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int count = 0;
-        while (sc.hasNext()){
-            w = sc.nextInt();
-            h = sc.nextInt();
-            if(w == 0 && h == 0){
-                break;
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int next = sc.nextInt();
+            String result = next % 2 == 0 ? "0" : "1";
+            for(int i = 1; i < n; i++){
+                int value = sc.nextInt();
+                result += String.valueOf(value % 2);
             }
-            sc.nextLine();
-            count++;
-
-            int[][] array = new int[h][w];
-            for(int i = 0; i < h; i++){
-                String line = sc.nextLine();
-                for(int j = 0; j < w; j++){
-                    if (line.charAt(j) == '.'){
-                        array[i][j] = -1;
-                    }else if(line.charAt(j) == 'X'){
-                        array[i][j] = 1;
-                    }else if(line.charAt(j) == '*'){
-                        array[i][j] = 0;
-                    }
+            int count = 0;
+            while (result.length() > 0){
+                count++;
+                AbstractMap.SimpleEntry<Integer, Integer> pair = getPairOdd(result, count % 2);
+                int lastIndexOf1;
+                if(count % 2 == 0){
+                     lastIndexOf1 = result.lastIndexOf('1');
+                }else {
+                     lastIndexOf1 = result.lastIndexOf('0');
                 }
-            }
-            process(array);
-            for(int i = 2; i <= component; i++){
-                List<Map.Entry> list = resultMap.get(i);
-            }
-            List<Integer> resultList = new ArrayList<>(resultMap.values());
-            Collections.sort(resultList);
+                String first = result.substring(0, lastIndexOf1 < 0 ? 0 : lastIndexOf1);
+                String second = result.substring(lastIndexOf1 + 1 > result.length() ? result.length() : lastIndexOf1 + 1, result.length());
+                if(first.length() > second.length()){
+                    result = second;
+                }else {
+                    result = first;
+                }
 
-            System.out.println(String.format("Throw %d", count));
-            System.out.println(resultList.stream().map(i -> String.valueOf(i)).collect(Collectors.joining(" ")));
-            if(sc.hasNext()){
-                System.out.println("");
-            }else {
-                break;
+                count++;
             }
+            System.out.println(count % 2 == 0 ? "Second" : "First");
         }
+
     }
 
-    private static void process(int[][] array) {
-        component = 1;
-        resultMap.clear();
-        for(int i = 0; i < h; i++){
-            for(int j = 0; j < w; j++){
-                if(array[i][j] == 0){
-                    component++;
-                    array[i][j] = component;
-                    resultMap.put(component, new ArrayList<>());
-                    dfs(i, j, array);
-                }else if(array[i][j] == 1){
-                    component++;
-                    array[i][j] = component;
-                    List<Map.Entry> newList = new ArrayList<>();
-                    newList.add(new AbstractMap.SimpleEntry<>(i, j));
-                    resultMap.put(component, newList);
-                    dfs(i, j, array);
+    private static AbstractMap.SimpleEntry<Integer, Integer>  getPairOdd(String result, int turn) {
+        int[] array = new int[result.length()];
+        array[0] = 1;
+        for(int i = 1; i < array.length; i++){
+            int value = 1;
+            for(int j = 0; j < i; j++){
+                if(((int) result.charAt(j) + value) % 2 == 1){
+                    value++;
                 }
             }
         }
+        return null;
     }
 
-    private static void dfs(int row, int col, int[][] array) {
-        int lowX = row - 1 >= 0 ? row - 1 : 0;
-        int lowY = col - 1 >= 0 ? col - 1 : 0;
-        int highX = row + 1 <= h -1 ? row + 1 : h -1;
-        int highY = col + 1 <= w +1 ? col + 1: w -1;
-
-        //same col
-        for(int i = lowX; i <= highX; i++){
-            if(array[i][col] == 0){
-                array[i][col] = component;
-                dfs(i, col, array);
-            }else if(array[i][col] == 1){
-                resultMap.get(component).add(new AbstractMap.SimpleEntry<>(i, col));
-                array[i][col] = component;
-                dfs(i, col, array);
+    private static boolean canDeliver(Map<Character, Integer> map, int k) {
+        for(char c: map.keySet()){
+            if(map.get(c) > k){
+                return false;
             }
         }
-
-        //same row
-        for(int j = lowY; j <= highY; j++){
-            if(array[row][j] == 0){
-                array[row][j] = component;
-                dfs(row, j, array);
-            }else if(array[row][j] == 1){
-                resultMap.get(component).add(new AbstractMap.SimpleEntry<>(row, j));
-                array[row][j] = component;
-                dfs(row, j, array);
-            }
-        }
+        return true;
     }
-
 
 }
 
