a4fed7138b46a2c5533d8098d0ae00b6a435e2dc
super1ha1 <khue0011@gmail.com>
2017-08-19 15:24:02 +0800

    solve 776	Monkeys in a Regular Forest note: normal floodfill problem, just that to present output differently find the max value of each column, then adjust value of each row with that max

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -2,12 +2,15 @@ package problem_set;
 
 import java.io.File;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private static int numComponent, m, n;
+    private static int numComponent, height, width;
     private static int[][] array;
+    private static char[][] chars;
+    private static List<String> queryList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
@@ -15,37 +18,76 @@ public class Main {
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         while (sc.hasNext()){
-            m = sc.nextInt();
-            if(m == 0){
+            String line = sc.nextLine().trim();
+            if(line.equals("%")){
+                process();
+            }else {
+                queryList.add(line);
+            }
+            if(!sc.hasNext()){
+                process();
                 break;
             }
-            n = sc.nextInt();
-            sc.nextLine();
-
-            array = new int[m][n];
-            for(int i = 0; i < m; i++){
-                String line = sc.nextLine();
-                for(int j = 0; j < n; j++){
-                    if(line.charAt(j) == '@'){
-                        array[i][j] = 0;
-                    }else {
-                        array[i][j] = -1;
-                    }
-                }
-            }
-
-            numComponent = 0;
-            for(int i = 0; i < m; i++){
-                for(int j = 0; j < n; j++){
-                    if(array[i][j] == 0){
-                        floodfill(i, j, ++numComponent);
-                    }
-                }
-            }
-
-            System.out.println(numComponent);
-
         }
+    }
+
+    private static void process() {
+        height = queryList.size();
+        if(height == 0){
+            return;
+        }
+        width = queryList.get(0).split(" ").length;
+        array = new int[height][width];
+        chars = new char[height][width];
+        for(int i = 0; i < height; i++){
+            String line = queryList.get(i);
+            String[] temp = line.split(" ");
+            for(int j = 0; j < width; j++){
+                chars[i][j] = temp[j].charAt(0);
+            }
+        }
+
+        numComponent = 0;
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                if(array[i][j] == 0){
+                    floodfill(i, j, ++numComponent);
+                }
+            }
+        }
+
+        int[] maxValue = new int[width];
+        for(int j = 0; j < width; j++){
+            for(int i = 0; i < height; i++){
+                if(array[i][j] > maxValue[j]){
+                    maxValue[j] = array[i][j];
+                }
+            }
+        }
+
+        List<String> resultList = new ArrayList<>();
+        for(int i = 0; i < height; i++){
+            String currentRow = "";
+            for(int j = 0; j < width; j++){
+                int maxLen = String.valueOf(maxValue[j]).length();
+                int currentLen = String.valueOf(array[i][j]).length();
+                while (currentLen < maxLen){
+                    currentRow += " ";
+                    currentLen++;
+                }
+                currentRow += array[i][j];
+                if(j < width - 1) {
+                    currentRow += " ";
+                }
+            }
+            resultList.add(currentRow);
+        }
+
+        for(String s: resultList){
+            System.out.println(s);
+        }
+        System.out.println("%");
+        queryList.clear();
     }
 
     private static void floodfill(int row, int col, int color) {
@@ -54,12 +96,12 @@ public class Main {
         //neigh bor
         int lowX = row - 1 >= 0 ? row - 1 : 0;
         int lowY = col - 1 >= 0 ? col - 1 : 0;
-        int highX = row + 1  <= m - 1 ? row + 1 : m -1;
-        int highY = col + 1 <= n - 1 ? col + 1 : n -1;
+        int highX = row + 1  <= height - 1 ? row + 1 : height -1;
+        int highY = col + 1 <= width - 1 ? col + 1 : width -1;
 
         for(int i = lowX; i <= highX; i++){
             for(int j = lowY; j <= highY; j++){
-                if(array[i][j] == 0){
+                if(array[i][j] == 0 && chars[i][j] == chars[row][col]){
                     floodfill(i, j, numComponent);
                 }
             }
