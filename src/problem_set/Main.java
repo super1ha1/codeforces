a2c06f5e638fbf00bc9d1dc0e4d87ab7d2dc6b31
super1ha1 <khue0011@gmail.com>
2017-08-19 13:53:17 +0800

    solve UVA 572 - Oil Deposits

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -6,8 +6,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static int numComponent, h, w, secondRoundNumberComponent;
-    private static Map<Integer, List<Map.Entry>> resultMap = new HashMap<>();
+    private static int numComponent, m, n;
     private static int[][] array;
     public static void main(String[] args) throws Exception {
 
@@ -15,123 +14,57 @@ public class Main {
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int count = 0;
         while (sc.hasNext()){
-            w = sc.nextInt();
-            h = sc.nextInt();
-            if(w == 0 && h == 0){
+            m = sc.nextInt();
+            if(m == 0){
                 break;
             }
+            n = sc.nextInt();
             sc.nextLine();
-            count++;
 
-            array = new int[h][w];
-            for(int i = 0; i < h; i++){
+            array = new int[m][n];
+            for(int i = 0; i < m; i++){
                 String line = sc.nextLine();
-                for(int j = 0; j < w; j++){
-                    if (line.charAt(j) == '.'){
-                        array[i][j] = -1;
-                    }else if(line.charAt(j) == 'X'){
-                        array[i][j] = 1;
-                    }else if(line.charAt(j) == '*'){
+                for(int j = 0; j < n; j++){
+                    if(line.charAt(j) == '@'){
                         array[i][j] = 0;
+                    }else {
+                        array[i][j] = -1;
                     }
                 }
             }
-            process();
 
-            List<Integer> resultList = new ArrayList<>();
-            for(int i = 2; i <= numComponent; i++){
-                List<Map.Entry> currentList = resultMap.get(i);
-                for(Map.Entry entry: currentList){
-                    array[(int) entry.getKey()][(int) entry.getValue()] = 0;
+            numComponent = 0;
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(array[i][j] == 0){
+                        floodfill(i, j, ++numComponent);
+                    }
                 }
-                processSecondRound();
-                resultList.add(secondRoundNumberComponent);
             }
 
-            Collections.sort(resultList);
+            System.out.println(numComponent);
 
-            System.out.println(String.format("Throw %d", count));
-            System.out.println(resultList.stream().map(i -> String.valueOf(i)).collect(Collectors.joining(" ")));
-            System.out.println("");
         }
     }
 
-    private static void processSecondRound() {
-        secondRoundNumberComponent = 0;
-        for(int i = 0; i < h; i++){
-            for(int j = 0; j < w; j++){
-                if(array[i][j] == 0) {
-                    floodFillSecondRound(i, j, ++secondRoundNumberComponent);
-                }
-            }
-        }
-    }
-
-    private static void process() {
-        numComponent = 1;
-        resultMap.clear();
-        for(int i = 0; i < h; i++){
-            for(int j = 0; j < w; j++){
-                if(array[i][j] == 0 || array[i][j] == 1) {
-                    resultMap.put(numComponent + 1, new ArrayList<>());
-                    floodFill(i, j, ++numComponent);
-                }
-            }
-        }
-    }
-
-    private static void floodFill(int row, int col, int color) {
-        if(array[row][col] == 1){
-            resultMap.get(color).add(new AbstractMap.SimpleEntry<>(row, col));
-        }
+    private static void floodfill(int row, int col, int color) {
         array[row][col] = color;
 
-        //visit all neighbor
+        //neigh bor
         int lowX = row - 1 >= 0 ? row - 1 : 0;
         int lowY = col - 1 >= 0 ? col - 1 : 0;
-        int highX = row + 1 <= h -1 ? row + 1 : h -1;
-        int highY = col + 1 <= w - 1 ? col + 1: w -1;
+        int highX = row + 1  <= m - 1 ? row + 1 : m -1;
+        int highY = col + 1 <= n - 1 ? col + 1 : n -1;
 
-        //same col
         for(int i = lowX; i <= highX; i++){
-            if(array[i][col] == 0 || array[i][col] == 1) {
-                floodFill(i, col, color);
-            }
-        }
-
-        //same row
-        for(int j = lowY; j <= highY; j++){
-            if(array[row][j] == 0 || array[row][j] == 1){
-                floodFill(row, j, color);
+            for(int j = lowY; j <= highY; j++){
+                if(array[i][j] == 0){
+                    floodfill(i, j, numComponent);
+                }
             }
         }
     }
 
-
-    private static void floodFillSecondRound(int row, int col, int color) {
-        array[row][col] = color;
-
-        //visit all neighbor
-        int lowX = row - 1 >= 0 ? row - 1 : 0;
-        int lowY = col - 1 >= 0 ? col - 1 : 0;
-        int highX = row + 1 <= h -1 ? row + 1 : h -1;
-        int highY = col + 1 <= w - 1 ? col + 1: w -1;
-
-        //same col
-        for(int i = lowX; i <= highX; i++){
-            if(array[i][col] == 0) {
-                floodFillSecondRound(i, col, color);
-            }
-        }
-
-        //same row
-        for(int j = lowY; j <= highY; j++){
-            if(array[row][j] == 0){
-                floodFillSecondRound(row, j, color);
-            }
-        }
-    }
 }
 
