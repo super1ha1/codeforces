b0bf30a99e5f1c6677e630feaad5c1800942d3b9
super1ha1 <khue0011@gmail.com>
2017-08-20 11:57:42 +0800

    not solve yet UVA 11466 - Largest Prime Divisor - note: all number with single prime factor does not qualify: e.g 2^2, 2^3,... - use max comparison of int correctly: mapToLong().max().getAsLong() - does not handle negative number yet: integer can be negative

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 public class Main {
 
-    private static int numComponent, n, sum;
+    private static long numComponent, n, sum;
     private static int[][] array;
     private static char[][] chars;
     private static List<String> queryList = new ArrayList<>();
     private static List<Integer> primeList = new ArrayList<>();
     private static long sieve_ll;
-    private static BitSet bitSet = new BitSet(10000000);
+    private static BitSet bitSet = new BitSet(10000010);
     public static void main(String[] args) throws Exception {
 
-        sieve(1000000);
+        sieve(10000000);
 
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         while (sc.hasNext()){
-            n = sc.nextInt();
+            n = Long.valueOf(sc.nextLine().trim());
             if(n == 0){
                 break;
             }
@@ -47,26 +47,52 @@ public class Main {
     }
 
     private static void process() {
-        int value = getA();
-        if(value > -1){
-            System.out.println(String.format("%d = %d + %d", n, value, n - value));
+        long result = -1;
+
+        if(isPrime(n)){
+            result = -1;
         }else {
-            System.out.println("Goldbach's conjecture is wrong.");
+            Set<Long> divisorList = new HashSet<>(primeFactor(n));
+            if(divisorList.size() > 1){
+                result = divisorList.stream().mapToLong(Long::valueOf).max().getAsLong();
+            }else {
+                result = -1;
+            }
+            if(result == n){
+                result = -1;
+            }
         }
+        System.out.println(result);
     }
 
-    private static int getA() {
-        for(int i = 1; i < primeList.size(); i++){
-            int value = primeList.get(i);
-            if(value > n/2){
-                break;
+    private static List<Long> primeFactor(long n) {
+        List<Long> resultList = new ArrayList<>();
+        int index = 0;
+        long value = primeList.get(index);
+        while (n != 1 && value * value <= n){
+            while (n % value == 0){
+                n /= value;
+                resultList.add(value);
             }
-            int other = n - value;
-            if(bitSet.get(other)){
-                return value;
+            value = primeList.get(++index);
+        }
+
+        if(n != 1){
+            resultList.add(n);
+        }
+        return resultList;
+    }
+
+    private static boolean isPrime(long n) {
+        if(n <= sieve_ll){
+            return bitSet.get((int) n);
+        }
+        for(int i = 0; i < primeList.size(); i++){
+            if(n % primeList.get(i) == 0){
+                return false;
             }
         }
-        return -1;
+        return true;
     }
 
 }
