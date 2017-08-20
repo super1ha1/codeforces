b06a15c74c0768540635b6125a53a1cf2979c0a6
super1ha1 <khue0011@gmail.com>
2017-08-20 22:13:06 +0800

    UVA 332 Rational Numbers from Repeating Fractions run time error

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,88 +1,60 @@
 package problem_set;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    private static List<Integer> primeList = new ArrayList<>();
-    private static long sieve_ll;
-    private static BitSet bitSet = new BitSet(20000010);
-    private static int n;
-    private static List<Map.Entry<Integer, Integer>> twinPrimes = new ArrayList<>();
+    private static int j;
+    private static double value;
+    private static long numerator, denominator;
     public static void main(String[] args) throws Exception {
-
-        sieve(20000000);
-        processTwinPrime();
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()){
-            int n = sc.nextInt();
-            Map.Entry<Integer, Integer> entry = twinPrimes.get(n - 1);
-            if(entry != null){
-                System.out.println(String.format("(%d, %d)", entry.getKey(), entry.getValue()));
+            int count = 0;
+            while (sc.hasNext()){
+            j = sc.nextInt();
+            if(j == -1){
+                break;
             }
+            value = sc.nextDouble();
+            if(j < 0){
+                continue;
+            }
+            count++;
+            String str = String.format("%.9f", value);
+            while (str.lastIndexOf('0') == str.length() - 1){
+                str = str.substring(0, str.length() - 1);
+            }
+            String fraction = str.split("\\.")[1];
+            int k = fraction.length() - j;
+            String repeated = fraction.substring(k);
+            str = str + repeated;
+            value = Double.valueOf(str);
+
+            if(j == 0){
+                numerator = (long) (Math.pow(10, k) * value);
+                denominator = (long) (Math.pow(10, k));
+            }else {
+                long firstPart = (long)(Math.pow(10, k + j) * value);
+                long secondPart = (long) (Math.pow(10, k) * value);
+                numerator =  firstPart - secondPart;
+                denominator = (long) (Math.pow(10, k + j) - Math.pow(10, k));
+            }
+            long gcd = gcd(numerator, denominator);
+            numerator = numerator/gcd;
+            denominator = denominator/gcd;
+            System.out.println(String.format("Case %d: %d/%d", count, numerator, denominator));
         }
 
     }
 
-    private static void processTwinPrime() {
-        for(int i = 0; i < primeList.size() - 1; i++){
-            if(primeList.get(i + 1) - primeList.get(i) == 2){
-                twinPrimes.add(new AbstractMap.SimpleEntry<>(primeList.get(i), primeList.get(i + 1)));
-            }
-        }
-    }
-
-
-    private static void sieve(long size) {
-        sieve_ll = size + 1;
-        bitSet.set(0, bitSet.size(), true);
-        bitSet.set(0, false);
-        bitSet.set(1, false);
-
-        for (long i = 2; i <= sieve_ll; i++){
-            if(bitSet.get((int)i)){
-                for(long j = i * i; j <= sieve_ll; j += i){
-                    bitSet.set((int) j, false);
-                }
-                primeList.add((int)i);
-            }
-        }
-    }
-
-    private static List<Long> primeFactor(long n) {
-        List<Long> resultList = new ArrayList<>();
-        int index = 0;
-        long value = primeList.get(index);
-        while (n != 1 && value * value <= n){
-            while (n % value == 0){
-                n /= value;
-                resultList.add(value);
-            }
-            value = primeList.get(++index);
-        }
-
-        if(n != 1){
-            resultList.add(n);
-        }
-        return resultList;
-    }
-
-    private static boolean isPrime(long n) {
-        if(n <= sieve_ll){
-            return bitSet.get((int) n);
-        }
-        for(int i = 0; i < primeList.size(); i++){
-            if(n % primeList.get(i) == 0){
-                return false;
-            }
-        }
-        return true;
+    private static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 
 }
