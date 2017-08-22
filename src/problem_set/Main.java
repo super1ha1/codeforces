2daaf3a89e6cd54a8311b54d3610d5026bb6e353
super1ha1 <khue0011@gmail.com>
2017-08-22 13:07:45 +0800

    not solve yet 10104 Euclid Problem - need to find pair x, y that min (|x| + |y|)

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,50 +1,54 @@
 package problem_set;
 
 import java.io.File;
-import java.net.CookieHandler;
-import java.util.*;
+import java.util.BitSet;
+import java.util.Scanner;
 
 public class Main {
 
     private static BitSet firstBitSet = new BitSet(1000000010);
     private static BitSet secondBitSet = new BitSet(1000000010);
 
+    private static long a, b, x, y, d;
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-//                Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-
-        while (sc.hasNext()) {
-            int n = sc.nextInt();
-            boolean result = true;
-            setup();
-            for (int i = 0; i < n; i++) {
-                int start = sc.nextInt();
-                int end = sc.nextInt();
-                if (isFirstOK(start, end)) {
-                    firstBitSet.set(start, end, true);
-                } else if (isSecondOK(start, end)) {
-                    secondBitSet.set(start, end, true);
-                } else {
-                    result = false;
-                }
-            }
-            System.out.println(result ? "YES" : "NO");
+//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        //                Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        while (sc.hasNext()){
+            a =sc.nextLong();
+            b = sc.nextLong();
+            extendEuclid(a, b);
+            update();
+            System.out.println(String.format("%d %d %d", x, y, d));
         }
     }
 
-    private static boolean isSecondOK(int start, int end) {
-        return secondBitSet.get(start, end).cardinality() == 0;
+    private static void update() {
+        if(Math.abs(x) < Math.abs(y)){
+            // increase x, reduce y
+            
+        } else if(Math.abs(x) > Math.abs(y)){
+            // decrease x, increase y
+
+        } else {
+            //do nothing
+        }
     }
 
-    private static void setup() {
-        firstBitSet.set(0, firstBitSet.size(), false);
-        secondBitSet.set(0, firstBitSet.size(), false);
+    private static void extendEuclid(long a, long b) {
+        if(b == 0){
+            x = 1;
+            y = 0;
+            d = a;
+            return;
+        }
+        extendEuclid(b, a % b);
+        long x1 = y;
+        long y1 = x - (a/b) * y;
+        x = x1;
+        y = y1;
     }
 
-    public static boolean isFirstOK(int start, int end) {
-        return firstBitSet.get(start, end).cardinality() == 0;
-    }
 }
 
