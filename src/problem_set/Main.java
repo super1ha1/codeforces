987ff097eb9dc4270af67b4540c0900eec417bbe
super1ha1 <khue0011@gmail.com>
2017-08-23 23:49:26 +0800

    time limit exceed UVA 374	Big Mod

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,53 +1,65 @@
 package problem_set;
 
-import java.io.File;
-import java.util.BitSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    private static BitSet firstBitSet = new BitSet(1000000010);
-    private static BitSet secondBitSet = new BitSet(1000000010);
-
-    private static long a, b, x, y, d;
+    private static List<String> list = new ArrayList<>();
     public static void main(String[] args) throws Exception {
 
-//        Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-        //                Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        while (sc.hasNext()){
-            a =sc.nextLong();
-            b = sc.nextLong();
-            extendEuclid(a, b);
-            update();
-            System.out.println(String.format("%d %d %d", x, y, d));
+        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        while (sc.hasNext()) {
+            String line = sc.nextLine().trim();
+            if(line.matches("\\d+")){
+                list.add(line);
+            }else {
+                process();
+                list.clear();
+            }
+            if(!sc.hasNext()){
+                process();
+            }
         }
     }
 
-    private static void update() {
-        if(Math.abs(x) < Math.abs(y)){
-            // increase x, reduce y
-            
-        } else if(Math.abs(x) > Math.abs(y)){
-            // decrease x, increase y
-
-        } else {
-            //do nothing
-        }
-    }
-
-    private static void extendEuclid(long a, long b) {
-        if(b == 0){
-            x = 1;
-            y = 0;
-            d = a;
+    private static void process() {
+        if(list.size() != 3){
             return;
         }
-        extendEuclid(b, a % b);
-        long x1 = y;
-        long y1 = x - (a/b) * y;
-        x = x1;
-        y = y1;
+        long b = Long.valueOf(list.get(0));
+        long p = Long.valueOf(list.get(1));
+        long m = Long.valueOf(list.get(2));
+        while (b >= m){
+            b = b % m;
+        }
+        if( b == 0 || b == 1){
+            System.out.println(b);
+            return;
+        }
+        long mod1 = find(b, m);
+        long left = p % mod1;
+        long result = 1;
+        for(int i = 0; i < left; i++){
+            result = (result * b) % m;
+        }
+        System.out.println(result);
+    }
+
+    private static long find(long b, long m) {
+        int i = 1;
+        long value = (long) Math.pow(b, i);
+        while (value % m != 1){
+            while (value >= m){
+                value = value % m;
+            }
+            i++;
+            value = (value * b) % m;
+        }
+        return i;
     }
 
 }
