a1a0bee0f58aa6188e0e451310811b173c977b30
super1ha1 <khue0011@gmail.com>
2017-08-26 16:45:31 +0800

    solve 11415	Count the Factorials very impressive manually think through each case of n and number of 0...a, such that count (a!) <= n

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,51 +1,96 @@
 package problem_set;
 
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
 
-    private static List<BigInteger> fibonaci = new ArrayList<>();
-    private static List<BigInteger> sumfibonaci = new ArrayList<>();
-    private static BigInteger bigIntegerTwo = BigInteger.valueOf(2);
+    private static BitSet bitSet = new BitSet(10000010);
+    private static List<Long> primeList = new ArrayList<>();
+    private static long[] result = new long[10000010];
+    private static long[] countValue = new long[10000010];
+    private static long size_ll;
+
     public static void main(String[] args) throws Exception {
 
-        prepareFibonaci(1510);
+        sieve(10000000);
+        prepareCount(10000000);
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        int counter = 0;
-        while (sc.hasNext()) {
+
+        int t = sc.nextInt();
+        while (t-- > 0){
             int n = sc.nextInt();
-            if(n <= 0){
+            System.out.println(result[n]);
+        }
+    }
+
+    private static void prepareCount(int bound) {
+        countValue[0] = 1;
+        countValue[1] = 1;
+        countValue[2] = 1;
+
+        int currentNumber = 2;
+        int n = 1;
+        long numbersCountLessThanEqualN = 3;
+        result[n] = numbersCountLessThanEqualN;
+
+        while (true){
+            currentNumber++;
+            long nextRange = primeFactor(currentNumber).size();
+            countValue[currentNumber] = countValue[currentNumber -1] + nextRange;
+
+            if(countValue[currentNumber] <= n){
+                numbersCountLessThanEqualN++;
+                result[n] = numbersCountLessThanEqualN;
+            }else {
+                while (n < countValue[currentNumber]){
+                    result[n] = numbersCountLessThanEqualN;
+                    n++;
+                }
+                numbersCountLessThanEqualN++;
+                result[n] = numbersCountLessThanEqualN;
+            }
+
+            if(n >= bound){
                 break;
             }
-            System.out.println(String.format("Set %d:", ++counter));
-            System.out.println(String.format("%d", calculate(n)));
         }
     }
 
-    private static BigInteger calculate(int line) {
-        BigInteger startNumber = sumfibonaci.get(line -1);
-        BigInteger totalRange = fibonaci.get(line);
-        boolean divideByTwo = totalRange.mod(bigIntegerTwo).equals(BigInteger.ZERO);
-        BigInteger toBeAdd = divideByTwo ?
-                totalRange.divide(bigIntegerTwo) :
-                totalRange.add(BigInteger.ONE).divide(bigIntegerTwo);
-        return startNumber.add(toBeAdd).subtract(BigInteger.ONE);
-    }
+    private static void sieve(long bound) {
+        size_ll = bound + 1;
+        bitSet.set(0, bitSet.size(), true);
+        bitSet.set(0, false);
+        bitSet.set(1, false);
 
-    public static void prepareFibonaci(int max) {
-        fibonaci.add(BigInteger.ZERO);
-        fibonaci.add(BigInteger.ONE);
-        sumfibonaci.add(BigInteger.ZERO);
-        sumfibonaci.add(BigInteger.ONE);
-        for(int i = 2; i <= max; i++){
-            fibonaci.add(fibonaci.get(i-1).add(fibonaci.get(i -2)));
-            sumfibonaci.add(fibonaci.get(i).add(sumfibonaci.get(i-1)));
+        for(long i = 2; i <= size_ll; i++){
+            if(bitSet.get((int) i)){
+                for(long j = i * i; j <= size_ll; j += i){
+                    bitSet.set((int) j, false);
+                }
+                primeList.add(i);
+            }
         }
     }
 
+    private static List<Long> primeFactor(long n) {
+        List<Long> list = new ArrayList<>();
+        int index = 0;
+        long value = primeList.get(index);
+        while (n != 1 && value * value <= n){
+            while (n % value == 0){
+                n = n /value;
+                list.add(value);
+            }
+            value = primeList.get(++index);
+        }
+        if(n != 1){
+            list.add(n);
+        }
+
+        return list;
+    }
 }
 
