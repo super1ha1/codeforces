7a92f9eb52b89ec9621de134bd7bd1b8557b3983
super1ha1 <khue0011@gmail.com>
2017-08-26 14:58:59 +0800

    solve 11161	Help My Brother (II) - find median of n th line range each line = fib(n) numbering start from 0

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,43 +1,49 @@
 package problem_set;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
 
-    private static List<Map.Entry<Long, Long>> bees = new ArrayList<>();
+    private static List<BigInteger> fibonaci = new ArrayList<>();
+    private static List<BigInteger> sumfibonaci = new ArrayList<>();
+    private static BigInteger bigIntegerTwo = BigInteger.valueOf(2);
     public static void main(String[] args) throws Exception {
 
-        prepareBee();
+        prepareFibonaci(1510);
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        int counter = 0;
         while (sc.hasNext()) {
-            long n = sc.nextLong();
-            if(n < 0){
+            int n = sc.nextInt();
+            if(n <= 0){
                 break;
             }
-            if(n >= bees.size()){
-                continue;
-            }
-            System.out.println(String.format("%d %d", bees.get((int)n).getKey(),
-                    bees.get((int)n).getKey() + bees.get((int)n).getValue()));
+            System.out.println(String.format("Set %d:", ++counter));
+            System.out.println(String.format("%d", calculate(n)));
         }
     }
 
-    private static void prepareBee() {
-        bees.add(new AbstractMap.SimpleEntry<>(0L, 1L));
-        int index = 0;
-        while (true){
-            index++;
-            Map.Entry<Long, Long> previous = bees.get(index -1);
-            long nextMale = previous.getKey() + previous.getValue();
-            long nextFemale = previous.getKey() + 1;
-            bees.add(new AbstractMap.SimpleEntry<>(nextMale, nextFemale));
+    private static BigInteger calculate(int line) {
+        BigInteger startNumber = sumfibonaci.get(line -1);
+        BigInteger totalRange = fibonaci.get(line);
+        boolean divideByTwo = totalRange.mod(bigIntegerTwo).equals(BigInteger.ZERO);
+        BigInteger toBeAdd = divideByTwo ?
+                totalRange.divide(bigIntegerTwo) :
+                totalRange.add(BigInteger.ONE).divide(bigIntegerTwo);
+        return startNumber.add(toBeAdd).subtract(BigInteger.ONE);
+    }
 
-            if(nextMale > Integer.MAX_VALUE || nextFemale > Integer.MAX_VALUE){
-                break;
-            }
+    public static void prepareFibonaci(int max) {
+        fibonaci.add(BigInteger.ZERO);
+        fibonaci.add(BigInteger.ONE);
+        sumfibonaci.add(BigInteger.ZERO);
+        sumfibonaci.add(BigInteger.ONE);
+        for(int i = 2; i <= max; i++){
+            fibonaci.add(fibonaci.get(i-1).add(fibonaci.get(i -2)));
+            sumfibonaci.add(fibonaci.get(i).add(sumfibonaci.get(i-1)));
         }
     }
 
