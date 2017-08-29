1d78c9842dd814e980857b0d6365bc586c8874a6
super1ha1 <khue0011@gmail.com>
2017-08-29 23:13:13 +0800

    solve 350	Pseudo-Random Numbers implement floyd_cycle_find algorithm
    f = (Z × L + I) mod M
    
    find cycle

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,89 +4,54 @@ import java.util.*;
 
 public class Main {
 
-    private static BitSet bitSet = new BitSet(10000010);
-    private static List<Long> primeList = new ArrayList<>();
-    private static long[] result = new long[10000010];
-    private static long[] countValue = new long[10000010];
-    private static long size_ll;
-
     public static void main(String[] args) throws Exception {
-
-        sieve(10000000);
-        prepareCount(10000000);
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int t = sc.nextInt();
-        while (t-- > 0){
-            int n = sc.nextInt();
-            System.out.println(result[n]);
-        }
-    }
-
-    private static void prepareCount(int bound) {
-        //prepare count value
-        countValue[0] = 1;
-        countValue[1] = 1;
-        countValue[2] = 1;
-        int currentNumber = 2;
-        while (true){
-            currentNumber++;
-            countValue[currentNumber] = countValue[currentNumber -1] + primeFactor(currentNumber).size();
-            if(countValue[currentNumber] >= bound){
+        int counter = 0;
+        while (sc.hasNext()){
+            int Z = sc.nextInt();
+            int I = sc.nextInt();
+            int M = sc.nextInt();
+            int L = sc.nextInt();
+            if(Z == 0 && I == 0 && M == 0 && L == 0){
                 break;
             }
-        }
-
-        //calculate n
-        long currentN = 1, currentCount = 0;
-        for(int i = 0; i < countValue.length; i++){
-            if(countValue[i] <= currentN){
-                currentCount++;
-            }else {
-                for(long index = currentN; index < countValue[i]; index++){
-                    result[(int)index] = currentCount;
-                }
-                currentN = countValue[i];
-                currentCount = i + 1;
-            }
+            System.out.println(String.format("Case %d: %d", ++counter, calculate(Z, I, M, L)));
         }
     }
 
-    private static void sieve(long bound) {
-        size_ll = bound + 1;
-        bitSet.set(0, bitSet.size(), true);
-        bitSet.set(0, false);
-        bitSet.set(1, false);
-
-        for(long i = 2; i <= size_ll; i++){
-            if(bitSet.get((int) i)){
-                for(long j = i * i; j <= size_ll; j += i){
-                    bitSet.set((int) j, false);
-                }
-                primeList.add(i);
-            }
+    private static int calculate(int z, int i, int m, int length) {
+        int tortoise, bare;
+        tortoise = function(z, i, m, length);
+        bare = function(z, i, m, function(z, i, m, length));
+        while (tortoise != bare){
+            tortoise = function(z, i, m, tortoise);
+            bare = function(z, i, m, function(z, i, m, bare));
         }
+
+        int u = 0;
+        bare = length;
+        while (bare != tortoise){
+            bare = function(z, i, m, bare);
+            tortoise = function(z, i, m, tortoise);
+            u++;
+        }
+
+        int mu = 1;
+        tortoise = function(z, i, m , bare);
+        while (tortoise != bare){
+            tortoise = function(z, i, m, tortoise);
+            mu++;
+        }
+
+        return mu;
     }
 
-    private static List<Long> primeFactor(long n) {
-        List<Long> list = new ArrayList<>();
-        int index = 0;
-        long value = primeList.get(index);
-        while (n != 1 && value * value <= n){
-            while (n % value == 0){
-                n = n /value;
-                list.add(value);
-            }
-            value = primeList.get(++index);
-        }
-        if(n != 1){
-            list.add(n);
-        }
-
-        return list;
+    private static int function(int z, int i, int m, int l){
+        return (z * l + i) % m;
     }
 }
 
