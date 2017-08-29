884781107bf32d91dc871bc4f1a9d5f449dfdbb0
super1ha1 <khue0011@gmail.com>
2017-08-29 23:38:22 +0800

    solve 10591	Happy Number
    If Si = 1 for some i ≥ 1, then the original integer S0 is said to be Happy number, else not
    
    4 → 16 → 37 → 58 → 89 → 145 → 42 → 20 → 4

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -11,12 +11,39 @@ public class Main {
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()){
-            int B = sc.nextInt();
-            int P = sc.nextInt();
-            int M = sc.nextInt();
-            System.out.println(BigInteger.valueOf(B).modPow(BigInteger.valueOf(P), BigInteger.valueOf(M)));
+        int counter = 0;
+        int testCase = sc.nextInt();
+        while (testCase-- > 0){
+            long n = sc.nextLong();
+            System.out.println(String.format("Case #%d: %d is ", ++counter, n) + (isHappyNumber(n) ? "a Happy number." : "an Unhappy number."));
         }
+
+    }
+
+    private static boolean isHappyNumber(long n) {
+        long tortoise, bare;
+        tortoise = function(n);
+        bare = function(function(n));
+        if( tortoise == 1 || bare == 1){
+            return true;
+        }
+        while (tortoise != bare){
+            tortoise = function(tortoise);
+            bare = function(function(bare));
+            if(tortoise == 1 || bare == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static long function(long n){
+        long sum = 0;
+        char[] array = String.valueOf(n).toCharArray();
+        for(char c: array){
+            sum += (long) Math.pow(Integer.valueOf(String.valueOf(c)), 2);
+        }
+        return sum;
     }
 }
 
