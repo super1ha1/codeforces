74969530ad4c610b81970c87689a091848592c09
super1ha1 <khue0011@gmail.com>
2017-08-29 23:20:11 +0800

    solve UVA 374	Big Mod R := B^P mod M
    use BigInteger.modPow(P, M)

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,5 +1,6 @@
 package problem_set;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
@@ -10,48 +11,12 @@ public class Main {
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int counter = 0;
         while (sc.hasNext()){
-            int Z = sc.nextInt();
-            int I = sc.nextInt();
+            int B = sc.nextInt();
+            int P = sc.nextInt();
             int M = sc.nextInt();
-            int L = sc.nextInt();
-            if(Z == 0 && I == 0 && M == 0 && L == 0){
-                break;
-            }
-            System.out.println(String.format("Case %d: %d", ++counter, calculate(Z, I, M, L)));
+            System.out.println(BigInteger.valueOf(B).modPow(BigInteger.valueOf(P), BigInteger.valueOf(M)));
         }
-    }
-
-    private static int calculate(int z, int i, int m, int length) {
-        int tortoise, bare;
-        tortoise = function(z, i, m, length);
-        bare = function(z, i, m, function(z, i, m, length));
-        while (tortoise != bare){
-            tortoise = function(z, i, m, tortoise);
-            bare = function(z, i, m, function(z, i, m, bare));
-        }
-
-        int u = 0;
-        bare = length;
-        while (bare != tortoise){
-            bare = function(z, i, m, bare);
-            tortoise = function(z, i, m, tortoise);
-            u++;
-        }
-
-        int mu = 1;
-        tortoise = function(z, i, m , bare);
-        while (tortoise != bare){
-            tortoise = function(z, i, m, tortoise);
-            mu++;
-        }
-
-        return mu;
-    }
-
-    private static int function(int z, int i, int m, int l){
-        return (z * l + i) % m;
     }
 }
 
