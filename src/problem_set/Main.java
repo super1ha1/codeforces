28a6dbcaee04b4345b82ab63ed2a6b1fd6e95d6c
super1ha1 <khue0011@gmail.com>
2017-08-31 23:44:05 +0800

    solve 10193	All You Need Is Love: reason get run time error: BigInteger first = new BigInteger(sc.nextLine(), 2); or not trim the sc.nextLine().trim()

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,5 +1,6 @@
 package problem_set;
 
+import java.io.File;
 import java.math.BigInteger;
 import java.util.*;
 
@@ -11,39 +12,26 @@ public class Main {
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int counter = 0;
         int testCase = sc.nextInt();
-        while (testCase-- > 0){
-            long n = sc.nextLong();
-            System.out.println(String.format("Case #%d: %d is ", ++counter, n) + (isHappyNumber(n) ? "a Happy number." : "an Unhappy number."));
+        sc.nextLine();
+        int count = 0;
+        for(int i = 0 ; i < testCase; i++){
+            String firstLine = sc.nextLine().trim();
+            String secondLine = sc.nextLine().trim();
+            BigInteger first = new BigInteger(firstLine, 2);
+            BigInteger second = new BigInteger(secondLine, 2);
+            System.out.println(String.format("Pair #%d: ", ++count) +
+                    (isLove(first, second) ? "All you need is love!" : "Love is not all you need!"));
         }
-
     }
 
-    private static boolean isHappyNumber(long n) {
-        long tortoise, bare;
-        tortoise = function(n);
-        bare = function(function(n));
-        if( tortoise == 1 || bare == 1){
-            return true;
-        }
-        while (tortoise != bare){
-            tortoise = function(tortoise);
-            bare = function(function(bare));
-            if(tortoise == 1 || bare == 1){
-                return true;
-            }
-        }
-        return false;
+    private static boolean isLove(BigInteger first, BigInteger second) {
+        BigInteger gcd = gcd(first, second);
+        return gcd.compareTo(BigInteger.ONE) >= 1;
     }
 
-    private static long function(long n){
-        long sum = 0;
-        char[] array = String.valueOf(n).toCharArray();
-        for(char c: array){
-            sum += (long) Math.pow(Integer.valueOf(String.valueOf(c)), 2);
-        }
-        return sum;
+    private static BigInteger gcd(BigInteger first, BigInteger second) {
+        return second.equals(BigInteger.ZERO) ? first : gcd(second, first.mod(second));
     }
 }
 
