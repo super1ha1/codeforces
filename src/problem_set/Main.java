ed76bf0588cdf2100922d0f6ac42021f47ca0c66
super1ha1 <khue0011@gmail.com>
2017-09-02 15:15:57 +0800

    not solve yet UVA 530	Binomial Showdown still get time limit exceed when use prime factor and divide

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,14 +1,20 @@
 package problem_set;
 
-import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+    private static long sieve_ll;
+    private static List<Long> primeList = new ArrayList<>();
+    private static BitSet bitSet = new BitSet(10000010);
+
     public static void main(String[] args) throws Exception {
+        sieve(10000000);
+
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+
         while (sc.hasNext()) {
             int n = sc.nextInt();
             int k = sc.nextInt();
@@ -19,21 +25,70 @@ public class Main {
         }
     }
 
-    private static int process(int n, int k) {
+    private static long process(int n, int k) {
         if(k == 0){
             return 0;
         }
-        BigInteger value = BigInteger.ONE;
+        if(k == 1){
+            return n;
+        }
+        List<Long> numerator = new ArrayList<>();
         for(int i = n - k + 1 ; i <= n; i++){
-            value = value.multiply(BigInteger.valueOf(i));
+            numerator.addAll(primeFactors(i));
+        }
+        Collections.sort(numerator);
+
+        List<Long> denominator = new ArrayList<>();
+        for(int i = 2; i <= k; i++){
+            denominator.addAll(primeFactors(i));
+        }
+        Collections.sort(denominator);
+        for(long i: denominator){
+            if(numerator.contains(i)){
+                numerator.remove(Long.valueOf(i));
+            }
         }
 
-        BigInteger denominator = BigInteger.ONE;
-        for(int i = 2; i <= k; i++){
-            denominator = denominator.multiply(BigInteger.valueOf(i));
+        long value = 1;
+        for(long i: numerator){
+            value = value * i;
         }
-        value = value.divide(denominator);
-        return value.intValueExact();
+        return value;
+    }
+
+    private static void sieve(long bound){
+        sieve_ll = bound + 1;
+        bitSet.set(0, bitSet.size(), true);
+        bitSet.set(0, false);
+        bitSet.set(1, false);
+
+        for(long i = 2; i <= sieve_ll; i++){
+            if(bitSet.get((int) i)){
+                for(long j = i * i; j <= sieve_ll; j += i){
+                    bitSet.set((int) j, false);
+                }
+                primeList.add(i);
+            }
+        }
+    }
+
+    private static List<Long> primeFactors(long n){
+        List<Long> list = new ArrayList<>();
+        int index = 0;
+        long value = primeList.get(index);
+
+        while (n != 1 && value * value <= n){
+            while (n % value == 0){
+                n = n / value;
+                list.add(value);
+            }
+            value = primeList.get(++index);
+        }
+
+        if(n != 1){
+            list.add(n);
+        }
+        return list;
     }
 }
 
