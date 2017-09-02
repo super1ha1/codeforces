b0eb609f7a33eddf0e6b05042d4a739bc6aff48d
super1ha1 <khue0011@gmail.com>
2017-09-02 14:44:57 +0800

    not solve yet UVA 530	Binomial Showdown get time limit exceed when use BigInteger to calculate

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,5 +1,6 @@
 package problem_set;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -10,50 +11,29 @@ public class Main {
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
         while (sc.hasNext()) {
             int n = sc.nextInt();
-            int[] array = new int[n];
-            for(int i = 0; i < n; i++){
-                array[i] = sc.nextInt();
+            int k = sc.nextInt();
+            if(n == 0 && k == 0){
+                break;
             }
-            System.out.println(test(array) ? "Yes" : "No");
+            System.out.println(process(n, k));
         }
     }
 
-    private static boolean test(int[]array) {
-        if(isOdd(array, 0, array.length - 1)){
-            return true;
+    private static int process(int n, int k) {
+        if(k == 0){
+            return 0;
         }
-        for(int i = 1; i < array.length - 2 ; i += 2){
-            int first = backtrack(array, 0, i -1);
-            int second = backtrack(array, i, array.length -1);
-            if(first > 0 && second > 0 && (first + second) % 2 == 1){
-                return true;
-            }
+        BigInteger value = BigInteger.ONE;
+        for(int i = n - k + 1 ; i <= n; i++){
+            value = value.multiply(BigInteger.valueOf(i));
         }
-        return false;
-    }
 
-    private static int backtrack(int[] array, int start, int end) {
-        if(isOdd(array, start, end)){
-            return 1;
+        BigInteger denominator = BigInteger.ONE;
+        for(int i = 2; i <= k; i++){
+            denominator = denominator.multiply(BigInteger.valueOf(i));
         }
-        for(int i = start + 1; i < end - 1 ; i += 2){
-            int first = backtrack(array, start, i - 1);
-            int second = backtrack(array, i, end);
-            if(first > 0 && second > 0){
-                return first + second;
-            }
-        }
-        return -1;
-    }
-
-    private static boolean isOdd(int[] array, int start, int end) {
-        if((end - start + 1) % 2 == 0){
-            return false;
-        }
-        if(array[start] % 2 == 0 || array[end] % 2 == 0){
-            return false;
-        }
-        return true;
+        value = value.divide(denominator);
+        return value.intValueExact();
     }
 }
 
