1680ba52a09015f2bff1458b23066fdbdfcb6b0e
super1ha1 <khue0011@gmail.com>
2017-09-06 23:09:50 +0800

    UVA 11770 Lighting Away find min number of light need to turn on manually

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -2,13 +2,12 @@ package problem_set;
 
 import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
     private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static int[] num, low, dfs_parent;
-    private static int  dfsNumberCounter;
+    private static int[] visit, low, dfs_parent;
+    private static int  dfsNumberCounter, N, M;
     private static Stack<Integer> stack = new Stack<>();
     private static List<List<Integer>> resultList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
@@ -16,84 +15,49 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        int counter = 0;
-        while (true){
-            int n = sc.nextInt();
-            if (n <= 0){
-                break;
-            }
-            sc.nextLine();
 
-            if(counter > 0){
-                System.out.println();
-            }
-            counter++;
+        int testCase = sc.nextInt();
+        for(int i  = 1; i <= testCase; i++){
             map.clear();
-            resultList.clear();
-
-            while (n-- > 0){
-                String line = sc.nextLine();
-                String[] lines = line.trim().split(" ");
-                int base = lines[lines.length -1].charAt(0) - 'A';
-                if(!map.keySet().contains(base)){
-                    map.put(base, new ArrayList<>());
+            N = sc.nextInt();
+            M = sc.nextInt();
+            for(int j = 0; j < M; j++){
+                int first = sc.nextInt() - 1;
+                int second = sc.nextInt() - 1;
+                if(!map.keySet().contains(first)){
+                    map.put(first, new ArrayList<>());
                 }
-                for(int i  = 0; i < lines.length -1; i++){
-                    int current = lines[i].charAt(0) - 'A';
-                    if(!map.keySet().contains(current)){
-                        map.put(current, new ArrayList<>());
-                    }
-                    map.get(base).add(current);
+                map.get(first).add(second);
+            }
+            process(i);
+        }
+    }
+
+    private static void process(int number) {
+        visit = new int[N];
+        dfsNumberCounter = 0;
+
+        for(int i = 0; i < N; i++){
+            if(visit[i] == 0){
+                dfsNumberCounter++;
+                floodFill(i, dfsNumberCounter);
+            }
+        }
+
+        System.out.println(String.format("Case %d: %d", number, dfsNumberCounter));
+    }
+
+    private static void floodFill(int node, int value) {
+        visit[node] = value;
+        if(map.keySet().contains(node)){
+            for(int i: map.get(node)){
+                if(visit[i] == 0){
+                    floodFill(i, value);
                 }
             }
-            process();
         }
     }
 
-    private static void process() {
-        num = new int[26];
-        low = new int[26];
-        dfsNumberCounter = 1;
-        for (int c: map.keySet()){
-            if(num[c] == 0){
-                stack.clear();
-                tarjanSCC(c);
-            }
-        }
-
-        for(List<Integer> list: resultList){
-            Collections.sort(list);
-        }
-        Collections.sort(resultList, Comparator.comparing(o -> o.get(0)));
-        for(List<Integer> list: resultList){
-            System.out.println(list.stream().map(i -> String.valueOf((char) ('A' + i))).collect(Collectors.joining(" ")));
-        }
-    }
-
-    private static void tarjanSCC(int node) {
-        num[node] = dfsNumberCounter;
-        low[node] = dfsNumberCounter;
-        dfsNumberCounter++;
-        stack.push(node);
-
-        for(int i: map.get(node)){
-            if(num[i] == 0){
-                tarjanSCC(i);
-                low[node] = Math.min(low[node], low[i]);
-            }else if(stack.contains(i)){
-                low[node] = Math.min(low[node], num[i]);
-            }
-        }
-
-        if(num[node] == low[node]){
-            List<Integer> result = new ArrayList<>();
-            while (!stack.isEmpty() && stack.peek() != node){
-                result.add(stack.pop());
-            }
-            result.add(stack.pop());
-            resultList.add(result);
-        }
-    }
 
 }
 
