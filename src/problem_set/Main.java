9ab993dd95f4ff95b23a5822093c34ff983e67e9
super1ha1 <khue0011@gmail.com>
2017-09-06 22:45:08 +0800

    solve 10731	Test given set of questions and answer 4 A B C D E C F C H I J J K B H I F I K C E B J K 0
    find the min number of set, each pair within set create contradictory preference

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,115 +1,99 @@
 package problem_set;
 
+import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static boolean[] articulation;
-    private static int[] dfs_num, dfs_low, dfs_parent;
-    private static int root, numChildren, n, dfsNumberCounter;
-    private static List<Map.Entry<Integer, Integer>> bridgeList = new ArrayList<>();
+    private static int[] num, low, dfs_parent;
+    private static int  dfsNumberCounter;
+    private static Stack<Integer> stack = new Stack<>();
+    private static List<List<Integer>> resultList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        int counter = 0;
         while (true){
-            n = sc.nextInt();
-            if(n == 0){
+            int n = sc.nextInt();
+            if (n <= 0){
                 break;
             }
             sc.nextLine();
+
+            if(counter > 0){
+                System.out.println();
+            }
+            counter++;
             map.clear();
-            List<String> list = new ArrayList<>();
+            resultList.clear();
 
-            while (true){
-                String line = sc.nextLine().trim();
-                if(line.equalsIgnoreCase("0")){
-                    break;
-                } else {
-                    list.add(line);
+            while (n-- > 0){
+                String line = sc.nextLine();
+                String[] lines = line.trim().split(" ");
+                int base = lines[lines.length -1].charAt(0) - 'A';
+                if(!map.keySet().contains(base)){
+                    map.put(base, new ArrayList<>());
+                }
+                for(int i  = 0; i < lines.length -1; i++){
+                    int current = lines[i].charAt(0) - 'A';
+                    if(!map.keySet().contains(current)){
+                        map.put(current, new ArrayList<>());
+                    }
+                    map.get(base).add(current);
                 }
             }
-            process(list);
+            process();
         }
     }
 
-    private static void process(List<String> list) {
-        for(String line: list){
-            String[] strings = line.split(" ");
-            int base = Integer.valueOf(strings[0]) - 1;
-            if(!map.keySet().contains(base)){
-                map.put(base, new ArrayList<>());
-            }
-            for(int i = 1; i < strings.length; i++){
-                int current = Integer.valueOf(strings[i]) - 1;
-                if(!map.keySet().contains(current)){
-                    map.put(current, new ArrayList<>());
-                }
-                map.get(base).add(current);
-                map.get(current).add(base);
-            }
-        }
-
-        dfs_low = new int[n];
-        dfs_num = new int[n];
-        dfs_parent = new int[n];
-        articulation = new boolean[n];
+    private static void process() {
+        num = new int[26];
+        low = new int[26];
         dfsNumberCounter = 1;
-
-        for (int i: map.keySet()){
-            if(dfs_num[i] == 0){
-                root = i;
-                numChildren = 0;
-                articulationPointAndBridge(i);
-                articulation[i] = (numChildren > 1);
+        for (int c: map.keySet()){
+            if(num[c] == 0){
+                stack.clear();
+                tarjanSCC(c);
             }
         }
 
-        int counter = 0;
-        for(int i = 0; i < n; i++){
-            if(articulation[i]){
-                counter++;
-            }
+        for(List<Integer> list: resultList){
+            Collections.sort(list);
         }
-        System.out.println(counter);
+        Collections.sort(resultList, Comparator.comparing(o -> o.get(0)));
+        for(List<Integer> list: resultList){
+            System.out.println(list.stream().map(i -> String.valueOf((char) ('A' + i))).collect(Collectors.joining(" ")));
+        }
     }
 
-    private static void articulationPointAndBridge(int node) {
-        dfs_num[node] = dfsNumberCounter;
-        dfs_low[node] = dfsNumberCounter;
+    private static void tarjanSCC(int node) {
+        num[node] = dfsNumberCounter;
+        low[node] = dfsNumberCounter;
         dfsNumberCounter++;
+        stack.push(node);
 
         for(int i: map.get(node)){
-            if(dfs_num[i] == 0){ //not visit yet
-                dfs_parent[i] = node;
-
-                if(node == root){
-                    numChildren++;
-                }
-
-                articulationPointAndBridge(i);
-
-                //check articulation point
-                if(dfs_low[i]  >= dfs_num[node]){
-                    articulation[node] = true;
-                }
-
-                //check bridge
-                if(dfs_low[i]  > dfs_num[node]){
-                    bridgeList.add(new AbstractMap.SimpleEntry<>(node, i));
-                }
-
-                //update low
-                dfs_low[node] = Math.min(dfs_low[i], dfs_low[node]);
-
-            } else { //already visit
-                if(dfs_parent[node] != i){
-                    dfs_low[node] = Math.min(dfs_num[i], dfs_low[node]);
-                }
+            if(num[i] == 0){
+                tarjanSCC(i);
+                low[node] = Math.min(low[node], low[i]);
+            }else if(stack.contains(i)){
+                low[node] = Math.min(low[node], num[i]);
             }
         }
+
+        if(num[node] == low[node]){
+            List<Integer> result = new ArrayList<>();
+            while (!stack.isEmpty() && stack.peek() != node){
+                result.add(stack.pop());
+            }
+            result.add(stack.pop());
+            resultList.add(result);
+        }
     }
+
 }
 
