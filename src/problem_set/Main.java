54fae8dba9d639c4f6d79fa3d2c9eed650f6a42c
super1ha1 <khue0011@gmail.com>
2017-09-07 23:20:20 +0800

    UVA 11770 Lighting Away still trying to find a good algorithm use reverse thinking: a -> b, map from b to a instead

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -6,10 +6,10 @@ import java.util.*;
 public class Main {
 
     private static Map<Integer, Set<Integer>> map = new HashMap<>();
-    private static int[] visit, low, parent, backEdge;
-    private static int  dfsNumberCounter, N, M;
-    private static Stack<Integer> stack = new Stack<>();
-    private static List<List<Integer>> resultList = new ArrayList<>();
+    private static int[] visit, appear, parent;
+    private static int  dfsNumberCounter, N, M, visitCounter;
+    private static Set<Integer> resultSet = new HashSet<>();
+
     public static void main(String[] args) throws Exception {
 
 //        Scanner sc = new Scanner(System.in);
@@ -19,15 +19,23 @@ public class Main {
         int testCase = sc.nextInt();
         for(int i  = 1; i <= testCase; i++){
             map.clear();
+            resultSet.clear();
             N = sc.nextInt();
             M = sc.nextInt();
+
+            appear= new int[N];
             for(int j = 0; j < M; j++) {
                 int first = sc.nextInt() - 1;
                 int second = sc.nextInt() - 1;
-                if(!map.keySet().contains(first)){
-                    map.put(first, new HashSet<>());
+                appear[first] = 1;
+                appear[second] = 1;
+
+                if(!map.keySet().contains(second)){
+                    map.put(second, new HashSet<>());
                 }
-                map.get(first).add(second);
+                if(second != first){
+                    map.get(second).add(first);
+                }
             }
             process(i);
         }
@@ -35,47 +43,37 @@ public class Main {
 
     private static void process(int number) {
         visit = new int[N];
-        parent = new int[N];
-        backEdge = new int[N];
         dfsNumberCounter = 0;
+        visitCounter = 0;
+
+        for(int i = 0; i < N; i++){
+            if(appear[i] == 0){
+                resultSet.add(i);
+            }
+        }
 
         for(int i = 0; i < N; i++){
             if(visit[i] == 0){
-                dfsNumberCounter++;
-                floodFill(i, dfsNumberCounter, -1);
+                visitCounter++;
+                floodFill(i, visitCounter);
             }
         }
 
-        int counter = 0;
-        for(int i = 0; i < N; i++){
-            if(parent[i] == -1){
-                counter++;
-            }
-        }
-
-        System.out.println(String.format("Case %d: %d", number, counter));
+        System.out.println(String.format("Case %d: %d", number, resultSet.size() > 0 ? resultSet.size() : 1));
     }
 
-    private static void floodFill(int node, int value, int parentNode) {
+    private static void floodFill(int node, int value) {
         visit[node] = value;
-        parent[node] = parentNode;
 
-        if(map.keySet().contains(node)){
+        if(map.keySet().contains(node) && map.get(node).size() > 0){
             for(int i: map.get(node)){
                 if(visit[i] == 0){
-                    floodFill(i, value, node);
-                }else {
-                    //already visit: 3 case: forward, cross, back
-                    if(visit[i] == visit[node]){ //same tree
-                        //ignore
-                        backEdge[i] = 1;
-                    }else if(visit[i] != visit[node]){ //diff tree
-                        if(backEdge[i] == 1){
-                            floodFill(i, value, node);
-                        }
-                    }
+                    floodFill(i, value);
                 }
             }
+        }else {
+            dfsNumberCounter++;
+            resultSet.add(node);
         }
     }
 
