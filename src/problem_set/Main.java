da61207b5aa081a4e48702c79a799bc4668f1405
super1ha1 <khue0011@gmail.com>
2017-09-07 22:30:47 +0800

    UVA 11770 Lighting Away find the correct algorithm, but get time exceed limit

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -5,27 +5,27 @@ import java.util.*;
 
 public class Main {
 
-    private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static int[] visit, low, dfs_parent;
+    private static Map<Integer, Set<Integer>> map = new HashMap<>();
+    private static int[] visit, low, parent, backEdge;
     private static int  dfsNumberCounter, N, M;
     private static Stack<Integer> stack = new Stack<>();
     private static List<List<Integer>> resultList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         int testCase = sc.nextInt();
         for(int i  = 1; i <= testCase; i++){
             map.clear();
             N = sc.nextInt();
             M = sc.nextInt();
-            for(int j = 0; j < M; j++){
+            for(int j = 0; j < M; j++) {
                 int first = sc.nextInt() - 1;
                 int second = sc.nextInt() - 1;
                 if(!map.keySet().contains(first)){
-                    map.put(first, new ArrayList<>());
+                    map.put(first, new HashSet<>());
                 }
                 map.get(first).add(second);
             }
@@ -35,24 +35,45 @@ public class Main {
 
     private static void process(int number) {
         visit = new int[N];
+        parent = new int[N];
+        backEdge = new int[N];
         dfsNumberCounter = 0;
 
         for(int i = 0; i < N; i++){
             if(visit[i] == 0){
                 dfsNumberCounter++;
-                floodFill(i, dfsNumberCounter);
+                floodFill(i, dfsNumberCounter, -1);
             }
         }
 
-        System.out.println(String.format("Case %d: %d", number, dfsNumberCounter));
+        int counter = 0;
+        for(int i = 0; i < N; i++){
+            if(parent[i] == -1){
+                counter++;
+            }
+        }
+
+        System.out.println(String.format("Case %d: %d", number, counter));
     }
 
-    private static void floodFill(int node, int value) {
+    private static void floodFill(int node, int value, int parentNode) {
         visit[node] = value;
+        parent[node] = parentNode;
+
         if(map.keySet().contains(node)){
             for(int i: map.get(node)){
                 if(visit[i] == 0){
-                    floodFill(i, value);
+                    floodFill(i, value, node);
+                }else {
+                    //already visit: 3 case: forward, cross, back
+                    if(visit[i] == visit[node]){ //same tree
+                        //ignore
+                        backEdge[i] = 1;
+                    }else if(visit[i] != visit[node]){ //diff tree
+                        if(backEdge[i] == 1){
+                            floodFill(i, value, node);
+                        }
+                    }
                 }
             }
         }
