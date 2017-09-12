221d39228461e814a9edd25ad7a78dd50a59384f
super1ha1 <khue0011@gmail.com>
2017-09-12 16:49:56 +0800

    solve UVA 336	A Node Too Far given list of network, find number node x reachable with given ttl
    use BFS, please note that some node not in spanning tree of BFS need also be count.
    
    use distanceMap.containsKey(neighbor) to check whether neighbor has been visited before => count ttl, also reduce the need to filter duplicate input, e.g connect to itself, 1 1, or same edge again
    10 20 then 20 10

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -7,57 +7,87 @@ import java.util.stream.Collectors;
 public class Main {
 
     private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static int[] visit;
-    private static List<Integer> resultList = new ArrayList<>();
-
-    private static int n, m;
+    private static Map<Integer, Map<Integer, Integer>> resultMap = new HashMap<>();
+    private static int source;
+    private static int ttl;
+    private static int counter;
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
+        counter = 0;
         while (sc.hasNext()){
-            n = sc.nextInt();
-            m = sc.nextInt();
-            if (n == 0 && m == 0){
+            int n = sc.nextInt();
+            if(n == 0){
                 break;
             }
             map.clear();
-            resultList.clear();
-            for(int i = 0; i < m; i++){
-                int first = sc.nextInt() -1;
-                int second = sc.nextInt() -1;
-                if(!map.keySet().contains(first)){
+            resultMap.clear();
+            for(int i = 0; i < n; i++){
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                if(!map.containsKey(first)){
                     map.put(first, new ArrayList<>());
                 }
+                if(!map.containsKey(second)){
+                    map.put(second, new ArrayList<>());
+                }
+
                 map.get(first).add(second);
+                map.get(second).add(first);
             }
-            process();
+
+            while (true){
+                source = sc.nextInt();
+                ttl = sc.nextInt();
+                if(source == 0 && ttl == 0){
+                    break;
+                }
+                counter++;
+                process();
+            }
         }
     }
 
     private static void process() {
-        visit = new int[n];
-        for(int i = 0; i < n; i++){
-            if(visit[i] == 0){
-                dfs(i);
-            }
-        }
-        Collections.reverse(resultList);
-        System.out.println(resultList.stream().map(i -> String.valueOf(i + 1)).collect(Collectors.joining(" ")));
-    }
+        if(!resultMap.containsKey(source)){
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(source);
+            Map<Integer, Integer> distanceMap = new HashMap<>();
+            distanceMap.put(source, 0);
 
-    private static void dfs(int node) {
-        visit[node] = 1;
-        if(map.keySet().contains(node)){
-            for(int i: map.get(node)){
-                if(visit[i] == 0){
-                    dfs(i);
+            while (!queue.isEmpty()){
+                int node = queue.poll();
+
+                if(map.containsKey(node)){
+                    for(int neighbor: map.get(node)){
+                        if(!distanceMap.containsKey(neighbor)){
+                            distanceMap.put(neighbor, distanceMap.get(node) + 1);
+                            queue.add(neighbor);
+                        }
+                    }
                 }
             }
+
+            resultMap.put(source, distanceMap);
         }
-        resultList.add(node);
+       int notVisit = resultMap.get(source).values().stream().reduce(0, (sum, nextValue) -> {
+          if(nextValue > ttl){
+              return sum + 1;
+          }
+          return sum;
+       });
+
+        notVisit += map.keySet().size() - resultMap.get(source).keySet().size();
+
+        if(!map.containsKey(source)){
+            notVisit = map.keySet().size();
+        }
+
+        System.out.println(String.format("Case %d: %d nodes not reachable from node %d with TTL = %d.", counter, notVisit, source, ttl));
     }
+
 }
 
