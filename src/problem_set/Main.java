a230cb73ddbd9237e6e1f10dd0fe3439ba1a3d9e
super1ha1 <khue0011@gmail.com>
2017-09-16 11:55:02 +0800

    solve UVA 10150	Doublets given list of dictionary and pair source, destination, find shortest path or no sol
    Great, always remember to check all test case:
    - Tricky: a = a (source = destination) => have to put first in condition check
    - Boundary: have to optimize large input
        + if len source != dest > no sol
        + not process all dictionary, only process len when has query
        + after that, if no connection to source or destination > no sol
        + after BFS of source, if no destination > no sol
    
    => GREAT example

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -2,67 +2,47 @@ package problem_set;
 
 import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     private static final Map<Integer, List<String>> lengMap = new HashMap<>();
     private static final Map<String, List<String>> map = new HashMap<>();
-    private static final Map<String, Map<String, Integer>> resultMap = new HashMap<>();
+    private static final Map<String, Map<String, String>> resultMap = new HashMap<>();
+    private static boolean[] updateMapLength = new boolean[20];
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        sc.nextLine();
-        sc.nextLine();
+        int counter = 0;
+        lengMap.clear();
+        map.clear();
+        resultMap.clear();
 
-        while (testCase-- > 0){
-            lengMap.clear();
-            map.clear();
-            resultMap.clear();
-
-            while (true){
-                String word = sc.nextLine().trim();
-                if(word.equalsIgnoreCase("*")){
-                    break;
-                } else {
-                    if(!lengMap.containsKey(word.length())){
-                        lengMap.put(word.length(), new ArrayList<>());
-                    }
-                    lengMap.get(word.length()).add(word);
+        while (sc.hasNext()) {
+            String word = sc.nextLine().trim();
+            if (word.isEmpty()) {
+                break;
+            } else {
+                if (!lengMap.containsKey(word.length())) {
+                    lengMap.put(word.length(), new ArrayList<>());
                 }
+                lengMap.get(word.length()).add(word);
             }
-            for(int i: lengMap.keySet()){
-                List<String> currentSet = lengMap.get(i);
-                for(String s: currentSet){
-                    for(String other: currentSet){
-                        if(!other.equals(s) && transform(s, other)){
-                            if(!map.containsKey(s)){
-                                map.put(s, new ArrayList<>());
-                            }
-                            if(!map.containsKey(other)){
-                                map.put(other, new ArrayList<>());
-                            }
+        }
 
-                            map.get(s).add(other);
-                            map.get(other).add(s);
-                        }
-                    }
+        while (sc.hasNext()) {
+            String query = sc.nextLine().trim();
+            if (query.isEmpty()) {
+                break;
+            } else {
+                if (counter > 0) {
+                    System.out.println("");
                 }
-            }
-            while (sc.hasNext()){
-                String query = sc.nextLine().trim();
-                if(query.isEmpty()){
-                    break;
-                }else {
-                    processQuery(query);
-                }
-            }
-
-            if(testCase > 0){
-                System.out.println("");
+                processQuery(query);
+                counter++;
             }
         }
     }
@@ -85,30 +65,86 @@ public class Main {
         String source = array[0];
         String destination = array[1];
 
+        if(source.equals(destination)){
+            System.out.println(source);
+            return;
+        }
+
+        if(source.length() != destination.length()){
+            System.out.println("No solution.");
+            return;
+        }
+
+        int len = source.length();
+        updateMapLength(len);
+
+        if(!map.containsKey(source) || !map.containsKey(destination)){
+            System.out.println("No solution.");
+            return;
+        }
+
         if(!resultMap.containsKey(source)){
             Queue<String> queue = new LinkedList<>();
             queue.add(source);
-            Map<String, Integer> distanceMap = new HashMap<>();
-            distanceMap.put(source, 0);
+            Map<String, String> parentMap = new HashMap<>();
+            parentMap.put(source, null);
 
             while (!queue.isEmpty()){
                 String current = queue.poll();
                 if(map.containsKey(current)){
                     for(String neighbor: map.get(current)){
-                        if(!distanceMap.containsKey(neighbor)){
-                            distanceMap.put(neighbor, distanceMap.get(current) + 1);
+                        if(!parentMap.containsKey(neighbor)){
+                            parentMap.put(neighbor, current);
                             queue.add(neighbor);
                         }
                     }
                 }
             }
-            resultMap.put(source, distanceMap);
+            resultMap.put(source, parentMap);
         }
 
         if(resultMap.get(source).containsKey(destination)){
-            int distance = resultMap.get(source).get(destination);
-            System.out.println(String.format("%s %s %d", source, destination, distance));
+            List<String> traceList = new ArrayList<>();
+            traceList.add(destination);
+
+            Map<String, String> parentMapLocal = resultMap.get(source);
+            while (true){
+                String parent = parentMapLocal.get(traceList.get(traceList.size() -1));
+                traceList.add(parent);
+                if(parent.equals(source)){
+                    break;
+                }
+            }
+            Collections.reverse(traceList);
+            for(String member: traceList){
+                System.out.println(member);
+            }
+        }else {
+            System.out.println("No solution.");
         }
+    }
+
+    private static void updateMapLength(int len) {
+        if(updateMapLength[len]){
+           return;
+        }
+        List<String> currentSet = lengMap.get(len);
+        for (String s : currentSet) {
+            for (String other : currentSet) {
+                if (!other.equals(s) && transform(s, other)) {
+                    if (!map.containsKey(s)) {
+                        map.put(s, new ArrayList<>());
+                    }
+                    if (!map.containsKey(other)) {
+                        map.put(other, new ArrayList<>());
+                    }
+
+                    map.get(s).add(other);
+                    map.get(other).add(s);
+                }
+            }
+        }
+        updateMapLength[len] = true;
     }
 }
 
