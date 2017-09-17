18135f290eb7b823a04fc7138ae7947327e21573
super1ha1 <khue0011@gmail.com>
2017-09-17 12:33:09 +0800

    not solve yet UVA 11804 Argentina get time limit exceed

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -8,27 +8,33 @@ public class Main {
 
 
     private static int maxAttack;
-    private static List<Set<Integer>> resultList = new ArrayList<>();
-    private static List<Player> list = new ArrayList<>();
-    private static Comparator<Player> playerComparator = Comparator.comparing(o -> o.name);
+    private static final List<Set<Integer>> maxAttackResultList = new ArrayList<>();
+    private static final List<Player> inputPlayList = new ArrayList<>();
+    private static final List<Integer> fixIndexList = new ArrayList<>();
+
+    private static final Comparator<Player> playerComparatorByName = Comparator.comparing(o -> o.name);
 
     public static void main(String[] args) throws Exception {
 
-//        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            fixIndexList.add(i);
+        }
+
+        Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+//        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         int T = sc.nextInt();
         sc.nextLine();
         for(int i = 1; i <= T; i++){
-            resultList.clear();
-            list.clear();
+            maxAttackResultList.clear();
+            inputPlayList.clear();
 
             for(int j = 0; j < 10; j++){
                 String line = sc.nextLine().trim();
                 String[] array = line.split("[\\s]+");
                 Player player = new Player(array[0], Integer.valueOf(array[1]), Integer.valueOf(array[2]));
-                list.add(player);
+                inputPlayList.add(player);
             }
             System.out.println(String.format("Case %d:", i));
             process();
@@ -37,44 +43,55 @@ public class Main {
 
     private static void process() {
         maxAttack();
-        if(resultList.size() == 1){
-            printResult(setToSortedList(resultList.get(0)));
+        if(maxAttackResultList.size() == 1){
+            Set<Integer> defendSet = getDefendFromAttack(maxAttackResultList.get(0));
+            printResult(setToSortedListByName(maxAttackResultList.get(0)));
+            printResult(setToSortedListByName(defendSet));
         }else {
             List<Set<Integer>> maxDefendList = maxDefendList();
             if(maxDefendList.size() == 1){
-                printResult(setToSortedList(maxDefendList.get(0)));
+                Set<Integer> defendSet = getDefendFromAttack(maxDefendList.get(0));
+                printResult(setToSortedListByName(maxDefendList.get(0)));
+                printResult(setToSortedListByName(defendSet));
             }else {
-                List<List<Player>> finalPlayer = new ArrayList<>();
+                Map<String, String> resultMap = new HashMap<>();
                 for(Set<Integer> set: maxDefendList){
-                    finalPlayer.add(setToSortedList(set));
+                    String attack = getNamePlayer(setToSortedListByName(set));
+                    String defend = getNamePlayer(setToSortedListByName(getDefendFromAttack(set)));
+                    resultMap.put(attack, defend);
                 }
-                finalPlayer.forEach(singleList -> singleList.sort(playerComparator));
-                finalPlayer.sort((o1, o2) -> {
-                    int index = 0;
-                    while (playerComparator.compare(o1.get(index), o2.get(index)) == 0) {
-                        index++;
-                    }
-                    return playerComparator.compare(o1.get(index), o2.get(index));
-                });
+                List<String> attackSting = new ArrayList<>(resultMap.keySet());
+                Collections.sort(attackSting);
+                System.out.println("(" + attackSting.get(0) + ")");
+                System.out.println("(" + resultMap.get(attackSting.get(0)) + ")");
             }
         }
     }
 
-    private static List<Player> setToSortedList(Set<Integer> set) {
-        List<Player> localList = set.stream().map(i -> list.get(i)).collect(Collectors.toList());
-        localList.sort(playerComparator);
+    private static Set<Integer> getDefendFromAttack(Set<Integer> integers) {
+        return fixIndexList.stream().filter(i -> !integers.contains(i)).collect(Collectors.toSet());
+    }
+
+    private static List<Player> setToSortedListByName(Set<Integer> set) {
+        List<Player> localList = set.stream().map(inputPlayList::get).collect(Collectors.toList());
+        localList.sort(playerComparatorByName);
         return localList;
     }
 
     private static void printResult(List<Player> result) {
+        System.out.println("(" + getNamePlayer(result) + ")");
+    }
 
+    private static String getNamePlayer(List<Player> result) {
+        return result.stream().map(i -> i.name).collect(Collectors.joining(", "));
     }
 
     private static List<Set<Integer>> maxDefendList() {
         int maxDefend = Integer.MIN_VALUE;
         List<Set<Integer>> defendList = new ArrayList<>();
-        for(Set<Integer> set: resultList){
-            int value = getMaxDefend(set);
+        for(Set<Integer> set: maxAttackResultList){
+            Set<Integer> defendSet = getDefendFromAttack(set);
+            int value = getDefendValue(defendSet);
             if(value > maxDefend){
                 maxDefend = value;
                 defendList.clear();
@@ -88,35 +105,32 @@ public class Main {
 
     private static void maxAttack() {
         maxAttack = Integer.MIN_VALUE;
-        List<Integer> valueList = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            valueList.add(i);
-        }
-        backTrack(new HashSet<>(), valueList);
+        backTrack(new HashSet<>(), new ArrayList<>(fixIndexList));
     }
 
-    private static int getMaxDefend(Set<Integer> set) {
+    private static int getDefendValue(Set<Integer> set) {
         int value = 0;
-        for(Integer player: set){
-            value += list.get(player).defend;
+        for(Integer playerIndex: set){
+            value += inputPlayList.get(playerIndex).defend;
         }
         return value;
     }
 
     private static void backTrack(Set<Integer> currentSet, List<Integer> leftOverList){
         if(currentSet.size() == 5){
-            int value = getValue(currentSet);
+            int value = getAttackValue(currentSet);
             if(value == maxAttack){
                 if(notYetResult(currentSet)){
-                    resultList.add(currentSet);
+                    maxAttackResultList.add(currentSet);
                 }
-            }else if(value > maxAttack){
+            } else if(value > maxAttack){
                 maxAttack = value;
-                resultList.clear();
-                resultList.add(currentSet);
+                maxAttackResultList.clear();
+                maxAttackResultList.add(currentSet);
             }
             return;
         }
+
         List<Integer> integrationList = new ArrayList<>(leftOverList);
         for(Integer player: integrationList){
             currentSet.add(player);
@@ -130,7 +144,7 @@ public class Main {
     }
 
     private static boolean notYetResult(Set<Integer> currentSet) {
-        for(Set<Integer> set: resultList){
+        for(Set<Integer> set: maxAttackResultList){
             if(set.equals(currentSet)){
                 return false;
             }
@@ -138,10 +152,10 @@ public class Main {
         return true;
     }
 
-    private static int getValue(Set<Integer> currentList) {
+    private static int getAttackValue(Set<Integer> currentList) {
         int value = 0;
         for(Integer player: currentList){
-            value += list.get(player).attack;
+            value += inputPlayList.get(player).attack;
         }
         return value;
     }
