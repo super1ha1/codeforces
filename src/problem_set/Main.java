56b5ee24960804e19cfdb398eed0fe455e8d1673
super1ha1 <khue0011@gmail.com>
2017-09-18 22:44:43 +0800

    solve UVA 908	Re-connecting Computer Sites - given list of computer sites and edge, find min spanning tree
    - use Kruskal's minimum spanning tree and union disjoint set to solve problem
    
    - PriorityQueue in Java: can provide custom Comparator when create queue
    - if duplicate entry, queue still put in as normal, e.g edge 1 2 5, 1 2 5 still both exist in queue

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,116 +1,93 @@
 package problem_set;
 
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
-import java.util.Scanner;
+import java.io.File;
+import java.util.*;
 
 public class Main {
 
-    private static BitSet bitset = new BitSet(100010);
-    private static List<Long> primeList = new ArrayList<>();
-    private static long sieve_ll;
-
-    private static long n;
-    private static int k;
-    private static int[] visit, allocation;
+    private static int n, m, k, oldMin, newMin;
+    private static PriorityQueue<Map.Entry<Integer, Map.Entry<Integer, Integer>>> queue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getKey));
+    private static int[] pset;
     public static void main(String[] args) throws Exception {
-            sieve(100000);
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()){
-             n = sc.nextLong();
-             k = sc.nextInt();
-            if(k == 0){
-                System.out.println(n);
-            }else {
-                List<Long> factors = getFactors(n);
-                int counter2 = k;
-                int counter5 = k;
-                for(long value: factors){
-                    if(value == 2){
-                        counter2--;
-                    }
-                    if(value == 5){
-                        counter5--;
-                    }
-                }
-                if(counter2 > 0 && counter5 > 0){
-                    long result = n * (long) Math.pow(2, counter2) * (long) Math.pow(5, counter5);
-                    System.out.println(result);
-                }else {
-                    System.out.println(n);
-                }
 
-            }
-        }
-    }
-
-    private static void sieve(long bound) {
-        sieve_ll = bound  + 1;
-        bitset.set(0, bitset.size(), true);
-        bitset.set(0, false);
-        bitset.set(1, false);
-
-        for(long i = 2; i <= sieve_ll; i++){
-            if(bitset.get((int) i)) {
-                for (long j = i * i; j <= sieve_ll; j += i) {
-                    bitset.set((int) j, false);
-                }
-                primeList.add(i);
-            }
-        }
-    }
-
-    private static List<Long> getFactors(long n){
-        List<Long> factors = new ArrayList<>();
-        int index = 0;
-        long value = primeList.get(index);
-        while (n != 1 && value * value <= n){
-            while (n % value == 0){
-                n = n /value;
-                factors.add(value);
-            }
-            value = primeList.get(++index);
-        }
-        if(n != 1){
-            factors.add(n);
-        }
-        return factors;
-    }
-
-    private static long process(long n) {
-        int counter = getCounter(n);
-        if(counter >= k){
-            return n;
-        }else {
-            n = n * (long) Math.pow(10, k - counter);
-            return n;
-        }
-    }
-
-    private static int getCounter(long n) {
         int counter = 0;
-        while (n % 10 == 0){
+        while (sc.hasNext()){
+            n = sc.nextInt();
+            oldMin = 0;
+            for(int i = 0 ;i < n-1; i++){
+                int firstPoint = sc.nextInt();
+                int secondPoint = sc.nextInt();
+                int weight = sc.nextInt();
+                oldMin += weight;
+            }
+
+            queue.clear();
+            k = sc.nextInt();
+            for(int i = 0; i < k; i++){
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                int weight = sc.nextInt();
+                queue.add(new AbstractMap.SimpleEntry<>(weight, new AbstractMap.SimpleEntry<>(first, second)));
+            }
+
+            m = sc.nextInt();
+            for(int i = 0; i < m; i++){
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                int weight = sc.nextInt();
+                queue.add(new AbstractMap.SimpleEntry<>(weight, new AbstractMap.SimpleEntry<>(first, second)));
+            }
+
+            if(counter > 0){
+                System.out.println("");
+            }
+
+            process();
+
+            System.out.println(oldMin);
+            System.out.println(newMin);
+
             counter++;
-            n = n/10;
         }
-        return counter;
+
     }
 
-    private static void update(long n, int k) {
-        while (true){
-            for(int i = 1; i <= 9; i++){
-                long value = i * (long) Math.pow(10, k);
-                if(value % n == 0){
-                    System.out.println(value);
-                    return;
-                }
+    private static void process() {
+        initSet(n);
+        newMin = 0;
+        while (!queue.isEmpty()){
+            Map.Entry<Integer, Map.Entry<Integer, Integer>> entry = queue.poll();
+            Map.Entry<Integer, Integer> pair = entry.getValue();
+            int weight = entry.getKey();
+
+            if(!isSameSet(pair.getKey(), pair.getValue())){
+                newMin += weight;
+                unionSet(pair.getKey(), pair.getValue());
             }
-            k++;
         }
+    }
+
+    private static void initSet(int n) {
+        pset = new int[n + 10];
+        for(int i = 0; i <= n; i++){
+            pset[i] = i;
+        }
+    }
+
+    private static boolean isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+
+    private static int findSet(int i){
+        return (pset[i] == i ? i : (pset[i] = findSet(pset[i])));
+    }
+
+    private static void unionSet(int i, int j){
+        pset[findSet(i)] = findSet(j);
     }
 }
 
