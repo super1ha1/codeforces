893b0db4566bf74ccbbc4f5d82260fb0d459bc57
super1ha1 <khue0011@gmail.com>
2017-09-19 14:08:29 +0800

    solve UVA 10307 Killing Aliens in Borg Maze - use BFS to calculate the distance between each vertex first, - note starting point and other location of alien are same - be more careful about navigate top, down, left, right
    Sample input
    6
    6 5
    #####
    #A#A##
    # # A#
    #S  ##
    #####
    7 7
    #####
    #AAA###
    #    A#
    # S ###
    #     #
    #AAA###
    #####
    
    Sample output
    8
    11

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class Main {
 
-    private static int n;
-    private static double newMin;
-    private static PriorityQueue<Map.Entry<Double, Map.Entry<Integer, Integer>>> queue = new PriorityQueue<>(Comparator.comparingDouble(Map.Entry::getKey));
+    private static int COL, ROW, numberOfNode;
+    private static int newMin;
+    private static PriorityQueue<Map.Entry<Integer, Map.Entry<Integer, Integer>>> edgeList = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getKey));
     private static int[] pset;
-    private static List<Map.Entry<Double, Double>> list = new ArrayList<>();
+    private static int[][] square;
+    private static Map<Integer, Map.Entry<Integer, Integer>> nodeMap = new HashMap<>();
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
@@ -17,51 +18,106 @@ public class Main {
         int testCase = sc.nextInt();
 
         while (testCase-- > 0){
-            n = sc.nextInt();
-            list.clear();
-            queue.clear();
+            COL = sc.nextInt();
+            ROW = sc.nextInt();
+            sc.nextLine();
 
-            for(int j = 0 ; j < n; j++){
-                double x = sc.nextDouble();
-                double y = sc.nextDouble();
-                list.add(new AbstractMap.SimpleEntry<>(x, y));
+            square = new int[ROW][COL];
+            numberOfNode = 0;
+            edgeList.clear();
+            nodeMap.clear();
+
+            for (int i = 0; i < ROW; i++) {
+                String line = sc.nextLine();
+                for (int j = 0; j < line.length(); j++) {
+                    switch (line.charAt(j)) {
+                        case '#':
+                            square[i][j] = -1;
+                            break;
+                        case 'S':
+                            square[i][j] = ++numberOfNode;
+                            nodeMap.put(numberOfNode, new AbstractMap.SimpleEntry<>(i, j));
+                            break;
+                        case 'A':
+                            square[i][j] = ++numberOfNode;
+                            nodeMap.put(numberOfNode, new AbstractMap.SimpleEntry<>(i, j));
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
 
+            bfs();
             process();
-
-            System.out.println(String.format("%.2f", newMin));
-
-            if(testCase > 0){
-                System.out.println("");
-            }
+            System.out.println(newMin);
         }
 
     }
 
-    private static void process() {
-        initSet(n);
-        newMin = 0.0;
-        for(int i = 0; i < n; i++){
-            for (int j = i + 1; j < n; j++){
-                Map.Entry<Double, Double> first = list.get(i);
-                Map.Entry<Double, Double> second = list.get(j);
-                queue.add(new AbstractMap.SimpleEntry<>(getDistance(first, second), new AbstractMap.SimpleEntry<>(i, j)));
+    private static void bfs() {
+        for(int currentNode = 1; currentNode <= numberOfNode; currentNode++){
+            Map.Entry<Integer, Integer> entry = nodeMap.get(currentNode);
+
+            Queue<Map.Entry<Integer, Integer>> queue = new LinkedList<>();
+            queue.add(entry);
+
+            int[][] distanceMap = new int[ROW][COL];
+            for(int i = 0; i < ROW; i++){
+                Arrays.fill(distanceMap[i], -1);
+            }
+
+            distanceMap[entry.getKey()][entry.getValue()] = 0;
+
+            while (!queue.isEmpty()){
+                Map.Entry<Integer, Integer> newNode = queue.poll();
+                int row = newNode.getKey();
+                int col = newNode.getValue();
+                int lowX =  row -1 >= 0 ? row -1 : 0;
+                int lowY =  col -1  >= 0 ? col -1 : 0;
+                int highX = row + 1 <= ROW - 1 ? row + 1 : ROW -1;
+                int highY = col + 1 <= COL - 1 ? col + 1 : COL -1;
+
+                //top down, same col
+                for(int newRow = lowX; newRow <= highX; newRow++){
+                    if(newRow != row && square[newRow][col] != -1 && distanceMap[newRow][col] == -1){
+                        distanceMap[newRow][col] = distanceMap[row][col] + 1;
+                        if(square[newRow][col] > 0){
+                            edgeList.add(new AbstractMap.SimpleEntry<>(distanceMap[newRow][col], new AbstractMap.SimpleEntry<>(currentNode, square[newRow][col])));
+                        }
+                        queue.add(new AbstractMap.SimpleEntry<>(newRow, col));
+                    }
+                }
+
+                //left right, same row
+                for(int newCol = lowY; newCol <= highY; newCol++){
+                    if(newCol != col && square[row][newCol] != -1 && distanceMap[row][newCol] == -1){
+                        distanceMap[row][newCol] = distanceMap[row][col] + 1;
+                        if(square[row][newCol] > 0){
+                            edgeList.add(new AbstractMap.SimpleEntry<>(distanceMap[row][newCol],
+                                            new AbstractMap.SimpleEntry<>(currentNode, square[row][newCol])));
+                        }
+                        queue.add(new AbstractMap.SimpleEntry<>(row, newCol));
+                    }
+                }
             }
         }
-        while (!queue.isEmpty()){
-            Map.Entry<Double, Map.Entry<Integer, Integer>> entry = queue.poll();
+    }
+
+    private static void process() {
+        initSet(numberOfNode);
+        newMin = 0;
+
+        while (!edgeList.isEmpty()){
+            Map.Entry<Integer, Map.Entry<Integer, Integer>> entry = edgeList.poll();
             Map.Entry<Integer, Integer> pair = entry.getValue();
-            double weight = entry.getKey();
+            int weight = entry.getKey();
 
             if(!isSameSet(pair.getKey(), pair.getValue())){
                 newMin += weight;
                 unionSet(pair.getKey(), pair.getValue());
             }
         }
-    }
-
-    private static Double getDistance(Map.Entry<Double, Double> first, Map.Entry<Double, Double> second) {
-        return Math.sqrt(Math.pow(Math.abs(first.getKey() - second.getKey()), 2)  + Math.pow(Math.abs(first.getValue() - second.getValue()), 2));
     }
 
     private static void initSet(int n) {
