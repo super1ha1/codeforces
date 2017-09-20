752194239fb11fe2ca2be1b48bc463a40c19abae
super1ha1 <khue0011@gmail.com>
2017-09-20 22:24:45 +0800

    solve again UVA 193	Graph Coloring
    Please Please NOTE WHEN DO BACKTRACKING:
    what are the actual variable: first to last that we are considering!!!
    for this graph color: in can be either 1 or 0, black or white
    
    Why do you do a loop inside the backtracking ??? Not necessary

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,20 +1,16 @@
 package problem_set;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.File;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private  int[] attack, defend;
-    private  String[] names;
-    private int[] visit;
-    private int maxAttack, maxDefend;
-    private String minAttackNames, defendNames;
-    private boolean firstTime;
-
+    private int[] visit = new int[110], allocation = new int[110];
+    private int  n, k;
+    private Map<Integer, List<Integer>> map = new HashMap<>();
+    private List<Integer> resultList = new ArrayList<>();
+    private static final int BLACK = 1, WHITE = 0;
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.run(args);
@@ -23,115 +19,91 @@ public class Main {
     private void run(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-        //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+//         Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         int testCase = sc.nextInt();
-        sc.nextLine();
-        int counter = 0;
         while (testCase-- > 0){
-            counter++;
-            attack = new int[10];
-            defend = new int[10];
-            names = new String[10];
-            visit = new int[10];
-            firstTime = false;
+            n = sc.nextInt();
+            k = sc.nextInt();
 
-            for(int i = 0; i < 10; i++){
-                String line = sc.nextLine().trim();
-                String[] array = line.split("[\\s]+");
-                names[i] = array[0];
-                attack[i] = Integer.parseInt(array[1]);
-                defend[i] = Integer.parseInt(array[2]);
+            map.clear();
+            resultList.clear();
+
+            for(int i = 1; i <= n; i++){
+                visit[i] = 0;
+                allocation[i] = -1;
             }
 
-            backtrack(0);
+            for(int i = 0; i < k; i++){
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                if(!map.containsKey(first)){
+                    map.put(first, new ArrayList<>());
+                }
+                if(!map.containsKey(second)){
+                    map.put(second, new ArrayList<>());
+                }
+                map.get(first).add(second);
+                map.get(second).add(first);
+            }
 
-            System.out.println(String.format("Case %d:", counter));
-            System.out.println(minAttackNames);
-            System.out.println(defendNames);
+            backTrack(1);
+
+            System.out.println(resultList.size());
+            System.out.println(resultList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
         }
+
     }
 
-    private void backtrack(int current) {
-        if(current == 5){
-            if(!firstTime){
-                setUpNewValue();
-                firstTime = true;
-                return;
-            }
-
-            int attackValue = getAttackValueVisit();
-            if(attackValue > maxAttack){
-                setUpNewValue();
-            }else if(attackValue == maxAttack){
-                int defendValue = getDefendValueVisit();
-                if(defendValue > maxDefend){
-                    setUpNewValue();
-                }else if(defendValue == maxDefend){
-                    if(getAttackNamesCurrentVisit().compareTo(minAttackNames) < 0){
-                        setUpNewValue();
-                    }
-                }
+    private void backTrack(int current) {
+        if(current == n + 1){
+            if(getCurrentCount() > resultList.size()){
+                updateResult();
             }
             return;
         }
 
-        for(int i = 0; i < 10; i++){
-            if(visit[i] == 0){
-                visit[i] = 1;
-                backtrack(current + 1);
-                visit[i] = 0;
-            }
+        visit[current] = 1;
+
+        if(isOkToColorBlack(current)){ //can be black/white
+            allocation[current] = 1;
+            backTrack(current + 1);
         }
+        allocation[current] = 0;
+        backTrack(current + 1);
+
+        visit[current] = 0;
+
     }
 
-    private void setUpNewValue() {
-        maxAttack = getAttackValueVisit();
-        maxDefend = getDefendValueVisit();
-        minAttackNames = getAttackNamesCurrentVisit();
-        defendNames = getDefendNamesCurrentVisit();
-    }
-
-    private int getAttackValueVisit() {
-        int total = 0;
-        for(int i = 0; i < 10; i++){
-            if(visit[i] == 1){
-                total += attack[i];
+    private boolean isOkToColorBlack(int node) {
+        if(map.containsKey(node)){
+            for(int i: map.get(node)){
+                if(visit[i] == 1 && allocation[i] == BLACK){
+                    return false;
+                }
             }
         }
-        return total;
+        return true;
     }
 
-    private int getDefendValueVisit() {
-        int total = 0;
-        for(int i = 0; i < 10; i++){
-            if(visit[i] == 0){
-                total += defend[i];
+    private int getCurrentCount() {
+        int counter = 0;
+        for(int i = 1; i <= n; i++){
+            if(allocation[i] == 1){
+                counter++;
             }
         }
-        return total;
+        return counter;
     }
 
-    private String getAttackNamesCurrentVisit() {
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            if(visit[i] == 1){
-                list.add(names[i]);
+    private void updateResult() {
+        resultList.clear();
+        for(int i = 1; i <= n; i++){
+            if(allocation[i] == 1){
+                resultList.add(i);
             }
         }
-        Collections.sort(list);
-        return "(" + list.stream().collect(Collectors.joining(", ")) + ")";
-    }
-
-    private String getDefendNamesCurrentVisit() {
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            if(visit[i] == 0){
-                list.add(names[i]);
-            }
-        }
-        Collections.sort(list);
-        return "(" + list.stream().collect(Collectors.joining(", ")) + ")";
     }
 }
 
