ca9867b9c5e5d92d4c5d0c44fade88856afbc819
super1ha1 <khue0011@gmail.com>
2017-09-21 12:42:19 +0800

    solve UVA 	11631	Dark roads given list of node, edge, provide min optimization
    Sample Input
    7 11
    0 1 7
    0 3 5
    1 2 8
    1 3 9
    1 4 7
    2 4 5
    3 4 15
    3 5 6
    4 5 8
    4 6 9
    5 6 11
    0 0
    Sample Output
    51

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,108 +1,74 @@
 package problem_set;
 
-import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    private int[] visit = new int[110], allocation = new int[110];
-    private int  n, k;
-    private Map<Integer, List<Integer>> map = new HashMap<>();
-    private List<Integer> resultList = new ArrayList<>();
-    private static final int BLACK = 1, WHITE = 0;
+    private int m, n, totalLen, minTotal;
+    private int[] pset = new int[200010];
+    private PriorityQueue<Map.Entry<Integer, Map.Entry<Integer, Integer>>> priorityQueue =
+                    new PriorityQueue<>(Comparator.comparing(Map.Entry::getKey));
+
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.run(args);
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
 //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-//         Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        //         Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        while (testCase-- > 0){
-            n = sc.nextInt();
-            k = sc.nextInt();
-
-            map.clear();
-            resultList.clear();
-
-            for(int i = 1; i <= n; i++){
-                visit[i] = 0;
-                allocation[i] = -1;
+        while (true) {
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            if (m == 0 && n == 0) {
+                break;
             }
 
-            for(int i = 0; i < k; i++){
+            initSet(m);
+            priorityQueue.clear();
+            totalLen = 0;
+            for (int i = 0; i < n; i++) {
                 int first = sc.nextInt();
                 int second = sc.nextInt();
-                if(!map.containsKey(first)){
-                    map.put(first, new ArrayList<>());
-                }
-                if(!map.containsKey(second)){
-                    map.put(second, new ArrayList<>());
-                }
-                map.get(first).add(second);
-                map.get(second).add(first);
+                int weight = sc.nextInt();
+                totalLen += weight;
+                priorityQueue.add(new AbstractMap.SimpleEntry<>(weight, new AbstractMap.SimpleEntry<>(first, second)));
             }
 
-            backTrack(1);
-
-            System.out.println(resultList.size());
-            System.out.println(resultList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+            kruska();
+            System.out.println(totalLen - minTotal);
         }
-
     }
 
-    private void backTrack(int current) {
-        if(current == n + 1){
-            if(getCurrentCount() > resultList.size()){
-                updateResult();
+    private void kruska() {
+        minTotal = 0;
+        while (!priorityQueue.isEmpty()) {
+            Map.Entry<Integer, Map.Entry<Integer, Integer>> entry = priorityQueue.poll();
+            Map.Entry<Integer, Integer> pair = entry.getValue();
+            if (!isSameSet(pair.getKey(), pair.getValue())) {
+                minTotal += entry.getKey();
+                unionSet(pair.getKey(), pair.getValue());
             }
-            return;
         }
-
-        visit[current] = 1;
-
-        if(isOkToColorBlack(current)){ //can be black/white
-            allocation[current] = 1;
-            backTrack(current + 1);
-        }
-        allocation[current] = 0;
-        backTrack(current + 1);
-
-        visit[current] = 0;
-
     }
 
-    private boolean isOkToColorBlack(int node) {
-        if(map.containsKey(node)){
-            for(int i: map.get(node)){
-                if(visit[i] == 1 && allocation[i] == BLACK){
-                    return false;
-                }
-            }
-        }
-        return true;
+    private void unionSet(int i, int j) {
+        pset[findSet(i)] = findSet(j);
     }
 
-    private int getCurrentCount() {
-        int counter = 0;
-        for(int i = 1; i <= n; i++){
-            if(allocation[i] == 1){
-                counter++;
-            }
-        }
-        return counter;
+    private boolean isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
     }
 
-    private void updateResult() {
-        resultList.clear();
-        for(int i = 1; i <= n; i++){
-            if(allocation[i] == 1){
-                resultList.add(i);
-            }
+    private int findSet(int i) {
+        return (pset[i] == i ? i : (pset[i] = findSet(pset[i])));
+    }
+
+    private void initSet(int m) {
+        for (int i = 0; i < m; i++) {
+            pset[i] = i;
         }
     }
 }
