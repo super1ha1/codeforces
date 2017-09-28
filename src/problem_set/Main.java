cf658245fe97a4deacca586df48f2b4012de481c
super1ha1 <khue0011@gmail.com>
2017-09-28 14:01:20 +0800

    solve UVA 341 Non-Stop Travel: use standard dijkstra algorithm

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,10 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private int n, m;
-    private Map<Integer, List<Integer>> map = new HashMap<>();
-    private Map<Integer, Map<Integer, Integer>> resultMap = new HashMap<>();
-    private List<String> output = new ArrayList<>();
+    private int n;
+    private Map<Integer, List<Map.Entry<Integer, Integer>>> map = new HashMap<>();
+    private int[] distance;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -15,85 +14,88 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
-        //         Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+                Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()) {
+        int region = 0;
+        while (true) {
+            n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            region++;
             map.clear();
-            resultMap.clear();
-            output.clear();
-            output.add("-----");
 
-            n = Integer.valueOf(sc.nextLine());
-            for (int i = 0; i < n; i++) {
-                String line = sc.nextLine().trim();
-                String[] array = line.split("-");
-                int first = Integer.valueOf(array[0]);
-                if (!map.containsKey(first)) {
-                    map.put(first, new ArrayList<>());
+            for (int i = 1; i <= n; i++) {
+                if (!map.containsKey(i)) {
+                    map.put(i, new ArrayList<>());
                 }
-                if(array.length > 1){
-                    for (String s : array[1].split(",")) {
-                        map.get(first).add(Integer.valueOf(s));
-                    }
+
+                int numberNode = sc.nextInt();
+                for (int j = 0; j < numberNode; j++) {
+                    int nextPoint = sc.nextInt();
+                    int distance = sc.nextInt();
+                    map.get(i).add(new AbstractMap.SimpleEntry<>(distance, nextPoint));
                 }
             }
+            sc.nextLine();
+            String line = sc.nextLine();
 
-            m = Integer.valueOf(sc.nextLine());
-            for (int i = 0; i < m; i++) {
-                String line = sc.nextLine().trim();
-                String[] array = line.split("[\\s]+");
-                int start = Integer.valueOf(array[0]);
-                int finish = Integer.valueOf(array[1]);
+            String[] array = line.trim().split("[\\s]+");
+            int start = Integer.valueOf(array[0]);
+            int end = Integer.valueOf(array[1]);
 
-                if (map.containsKey(start)) {
-                    if(start == finish){
-                        output.add(String.valueOf(start));
-                    } else {
-                        if (!resultMap.containsKey(start)) {
-                            Queue<Integer> queue = new LinkedList<>();
-                            queue.add(start);
-                            Map<Integer, Integer> parentMap = new HashMap<>();
-                            parentMap.put(start, -1);
+            if (start == end) {
+                List<Integer> pathList = new ArrayList<>();
+                pathList.add(start);
+                System.out.println(String.format("Case %d: Path = %s; %d second delay", region,
+                                pathList.stream().map(String::valueOf).collect(Collectors.joining(" ")), 0));
+            } else {
+                distance = new int[n + 1];
+                for (int i = 0; i <= n; i++) {
+                    distance[i] = Integer.MAX_VALUE;
+                }
+                distance[start] = 0;
 
-                            while (!queue.isEmpty()) {
-                                int current = queue.poll();
-                                if (map.containsKey(current)) {
-                                    for (int neighbor : map.get(current)) {
-                                        if (!parentMap.containsKey(neighbor)) {
-                                            parentMap.put(neighbor, current);
-                                            queue.add(neighbor);
-                                        }
-                                    }
+                PriorityQueue<Map.Entry<Integer, Integer>> priorityQueue =
+                                new PriorityQueue<>(Comparator.comparing(Map.Entry::getKey));
+                priorityQueue.add(new AbstractMap.SimpleEntry<>(0, start));
+                Map<Integer, Integer> parentMap = new HashMap<>();
+
+                while (!priorityQueue.isEmpty()) {
+                    Map.Entry<Integer, Integer> entry = priorityQueue.poll();
+                    int weight = entry.getKey();
+                    int vertex = entry.getValue();
+
+                    if (weight == distance[vertex]) {
+                        if (map.containsKey(vertex)) {
+                            for (Map.Entry<Integer, Integer> neighbor : map.get(vertex)) {
+                                int edgeWeight = neighbor.getKey();
+                                int neighborVertex = neighbor.getValue();
+                                if (edgeWeight + distance[vertex] < distance[neighborVertex]) {
+                                    distance[neighborVertex] = edgeWeight + distance[vertex];
+                                    priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[neighborVertex],
+                                                    neighborVertex));
+                                    parentMap.put(neighborVertex, vertex);
                                 }
                             }
-                            resultMap.put(start, parentMap);
-                        }
-
-                        Map<Integer, Integer> parentMap = resultMap.get(start);
-                        if (parentMap.containsKey(finish)) {
-                            List<Integer> resultList = new ArrayList<>();
-                            resultList.add(finish);
-                            int nextParent = parentMap.get(resultList.get(resultList.size() - 1));
-                            while (nextParent != start) {
-                                resultList.add(nextParent);
-                                nextParent = parentMap.get(resultList.get(resultList.size() - 1));
-                            }
-                            resultList.add(start);
-                            Collections.reverse(resultList);
-                            output.add(resultList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-                        } else {
-                            output.add("connection impossible");
                         }
                     }
-                } else {
-                    output.add("connection impossible");
                 }
-            }
 
-            for (String s : output) {
-                System.out.println(s);
+                List<Integer> pathList = new ArrayList<>();
+                pathList.add(end);
+                int nextParent = parentMap.get(pathList.get(pathList.size() - 1));
+                while (nextParent != start) {
+                    pathList.add(nextParent);
+                    nextParent = parentMap.get(pathList.get(pathList.size() - 1));
+                }
+                pathList.add(start);
+                Collections.reverse(pathList);
+                System.out.println(String.format("Case %d: Path = %s; %d second delay", region,
+                                pathList.stream().map(String::valueOf).collect(Collectors.joining(" ")),
+                                distance[end]));
             }
         }
     }
