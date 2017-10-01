85446d47d0532b9bb53f6c7e5fc35f89785a4bf6
super1ha1 <khue0011@gmail.com>
2017-10-01 12:14:07 +0800

    solve UVA 11831	Sticker Collector Robot simulation the movement of robot: good point: separate into small single funtion, just do 1 thing > easier to code e.g move() turnLef() turnRight() moveForward() getNextX() getNextY() isInArena()

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,15 +1,13 @@
 package problem_set;
 
-import java.io.File;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class Main {
+    private static final int NORMAL_CELL = 0, STICKER = 1, PILLAR = -1;
+    private int n, m, s, total, x, y;
+    private int[][] arena;
+    private DIRECTION currentDirection;
 
-    private int total, n;
-    private int [] value, allocation;
-    private int maxSum;
-    private List<Integer> resultList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.run(args);
@@ -21,48 +19,191 @@ public class Main {
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
 
-        while (sc.hasNext()){
-            total = sc.nextInt();
+        while (true) {
             n = sc.nextInt();
-            value = new int[n];
-            allocation = new int[n];
-
-            for (int i = 0; i < n; i++){
-                value[i] = sc.nextInt();
+            m = sc.nextInt();
+            s = sc.nextInt();
+            if (n == 0 && m == 0 && s == 0) {
+                break;
             }
+            sc.nextLine();
 
-            resultList.clear();
-            maxSum = 0;
-            backTrack(0, 0);
-            System.out.println(getList() + " sum:" + maxSum);
-        }
+            arena = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String line = sc.nextLine().trim();
+                for (int j = 0; j < m; j++) {
+                    switch (line.charAt(j)) {
+                        case '.':
+                            arena[i][j] = NORMAL_CELL;
+                            break;
+                        case '*':
+                            arena[i][j] = STICKER;
+                            break;
 
-    }
+                        case '#':
+                            arena[i][j] = PILLAR;
+                            break;
 
-    private String getList() {
-        return resultList.stream().map(String::valueOf).collect(Collectors.joining(" "));
-    }
+                        case 'N':
+                            currentDirection = DIRECTION.NORTH;
+                            x = i;
+                            y = j;
+                            break;
 
-    private void backTrack(int counter, int currentSum) {
-        if(counter == n){
-            if(currentSum <= total && currentSum > maxSum){
-                maxSum = currentSum;
-                resultList.clear();
-                for(int i = 0; i < n; i++){
-                    if(allocation[i] == 1){
-                        resultList.add(value[i]);
+                        case 'S':
+                            x = i;
+                            y = j;
+                            currentDirection = DIRECTION.SOUTH;
+                            break;
+
+                        case 'L':
+                            x = i;
+                            y = j;
+                            currentDirection = DIRECTION.EAST;
+                            break;
+
+                        case 'O':
+                            x = i;
+                            y = j;
+                            currentDirection = DIRECTION.WEST;
+                            break;
+
+                        default:
+                            break;
                     }
                 }
             }
-            return;
+
+            String order = sc.nextLine();
+            total = 0;
+            for (int i = 0; i < s; i++) {
+                move(order.charAt(i));
+            }
+            System.out.println(total);
         }
+    }
 
-        allocation[counter] = 1;
-        backTrack(counter + 1, currentSum + value[counter]);
+    private void move(char c) {
+        if (c == 'D') {
+            turnRight();
+        } else if (c == 'E') {
+            turnLeft();
+        } else if (c == 'F') {
+            moveForward();
+        }
+    }
 
-        allocation[counter] = 0;
-        backTrack(counter + 1, currentSum);
+    private void moveForward() {
+        int nextX = getNextX();
+        int nextY = getNextY();
+        if(isInArena(nextX, nextY)) {
+            switch (arena[nextX][nextY]){
+                case NORMAL_CELL:
+                    x = nextX;
+                    y = nextY;
+                    break;
 
+                case STICKER:
+                    total++;
+                    x = nextX;
+                    y = nextY;
+                    arena[nextX][nextY] = NORMAL_CELL;
+                    break;
+
+                case PILLAR:
+                    //do nothing
+                    break;
+
+                default:
+                    break;
+            }
+        } else {
+            //do nothing
+        }
+    }
+
+    private boolean isInArena(int nextX, int nextY) {
+        return (0 <= nextX && nextX <= n -1 && 0 <= nextY && nextY <= m -1);
+    }
+
+    private int getNextY() {
+        switch (currentDirection) {
+            case EAST:
+                return y + 1;
+
+            case WEST:
+                return y -1;
+
+            default:
+                return y;
+
+        }
+    }
+
+    private int getNextX() {
+        switch (currentDirection) {
+            case NORTH:
+                return x - 1;
+
+            case SOUTH:
+                return x + 1;
+
+            default:
+                return x;
+
+        }
+    }
+
+    private void turnLeft() {
+        switch (currentDirection) {
+            case NORTH:
+                currentDirection = DIRECTION.WEST;
+                break;
+
+            case SOUTH:
+                currentDirection = DIRECTION.EAST;
+                break;
+
+            case EAST:
+                currentDirection = DIRECTION.NORTH;
+                break;
+
+            case WEST:
+                currentDirection = DIRECTION.SOUTH;
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private void turnRight() {
+        switch (currentDirection) {
+            case NORTH:
+                currentDirection = DIRECTION.EAST;
+                break;
+
+            case SOUTH:
+                currentDirection = DIRECTION.WEST;
+                break;
+
+            case EAST:
+                currentDirection = DIRECTION.SOUTH;
+                break;
+
+            case WEST:
+                currentDirection = DIRECTION.NORTH;
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private enum DIRECTION {
+        NORTH, SOUTH, EAST, WEST;
     }
 }
 
