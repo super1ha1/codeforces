ffd7f3188c3f9750cd66dcedb9a501a6d5183cd7
super1ha1 <khue0011@gmail.com>
2017-10-02 13:17:01 +0800

    use several optimization techniques: still time exceed limit > change algorithm, since start and end point always fixed

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Main {
 
     private int n, m;
-    private int[][] distance, weight;
+    private int[][] distance = new int[1000][1000], weight = new int[1000][1000], visit = new int[1000][1000];
+    private PriorityQueue<Point> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+
     private int[] dx = new int[] {-1, 0, 1, 0};
     private int[] dy = new int[] {0, -1, 0, 1};
 
@@ -16,59 +18,60 @@ public class Main {
 
     private void run(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         int testCase = sc.nextInt();
-        while (testCase-- > 0){
+        while (testCase-- > 0) {
             n = sc.nextInt();
             m = sc.nextInt();
-            weight = new int[n][m];
-            distance = new int[n][m];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
                     weight[i][j] = sc.nextInt();
                     distance[i][j] = Integer.MAX_VALUE;
+                    visit[i][j] = 0;
                 }
             }
 
             distance[0][0] = weight[0][0];
-            PriorityQueue<Map.Entry<Integer, Map.Entry<Integer, Integer>>> priorityQueue = new PriorityQueue<>(
-                            Comparator.comparing(Map.Entry::getKey));
-            priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[0][0], new AbstractMap.SimpleEntry<>(0, 0)));
+            priorityQueue.clear();
+            priorityQueue.add(new Point(0, 0, distance[0][0]));
 
-            while (!priorityQueue.isEmpty()){
-                Map.Entry<Integer, Map.Entry<Integer, Integer>> entry = priorityQueue.poll();
-                Map.Entry<Integer, Integer> position = entry.getValue();
-                int entryDistance = entry.getKey();
+            while (!priorityQueue.isEmpty()) {
+                Point currentPoint = priorityQueue.poll();
+                int entryDistance = currentPoint.weight;
+                visit[currentPoint.x][currentPoint.y] = 1;
 
-                if(distance[position.getKey()][position.getValue()] == entryDistance){
-                    for(Map.Entry<Integer, Integer> neighbor: getNeighBor(position)){
+                if (distance[currentPoint.x][currentPoint.y] == entryDistance) {
+                    for (int i = 0; i < 4; i++) {
+                        int nextX = currentPoint.x + dx[i];
+                        int nextY = currentPoint.y + dy[i];
+                        if (0 <= nextX && nextX < n && 0 <= nextY && nextY < m) {
 
-                        if(entryDistance + weight[neighbor.getKey()][neighbor.getValue()] < distance[neighbor.getKey()][neighbor.getValue()]){
-                            distance[neighbor.getKey()][neighbor.getValue()] = entryDistance + weight[neighbor.getKey()][neighbor.getValue()];
-                            priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[neighbor.getKey()][neighbor.getValue()],
-                                            new AbstractMap.SimpleEntry<>(neighbor)));
+                            if (entryDistance + weight[nextX][nextY] < distance[nextX][nextY]) {
+                                distance[nextX][nextY] = entryDistance + weight[nextX][nextY];
+                                if(visit[nextX][nextY] == 0){
+                                    priorityQueue.add(new Point(nextX, nextY, distance[nextX][nextY]));
+                                }
+                            }
                         }
                     }
                 }
             }
-            System.out.println(distance[n-1][m-1]);
+            System.out.println(distance[n - 1][m - 1]);
         }
     }
 
-    private List<Map.Entry<Integer, Integer>> getNeighBor(Map.Entry<Integer, Integer> position) {
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>();
-        int x = position.getKey();
-        int y = position.getValue();
-        for(int i = 0; i < 4; i++){
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            if(0 <= nextX && nextX < n && 0 <= nextY && nextY < m){
-                list.add(new AbstractMap.SimpleEntry<>(nextX, nextY));
-            }
+    static class Point {
+        public final int x;
+        public final int y;
+        public final int weight;
+
+        public Point(int x, int y, int weight) {
+            this.x = x;
+            this.y = y;
+            this.weight = weight;
         }
-        return list;
     }
 
 }
