89ae416690bfe5c378d8e1e85588c571d76f1157
super1ha1 <khue0011@gmail.com>
2017-10-02 12:36:30 +0800

    not sovle yet 929 Number Maze: time limit exceed

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,12 +1,13 @@
 package problem_set;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    private int n;
-    private Map<Integer, List<Map.Entry<Integer, Integer>>> map = new HashMap<>();
-    private int[] distance;
+
+    private int n, m;
+    private int[][] distance, weight;
+    private int[] dx = new int[] {-1, 0, 1, 0};
+    private int[] dy = new int[] {0, -1, 0, 1};
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -14,90 +15,60 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-                Scanner sc = new Scanner(System.in);
-//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(System.in);
+//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int region = 0;
-        while (true) {
+        int testCase = sc.nextInt();
+        while (testCase-- > 0){
             n = sc.nextInt();
-            if (n == 0) {
-                break;
-            }
-            region++;
-            map.clear();
-
-            for (int i = 1; i <= n; i++) {
-                if (!map.containsKey(i)) {
-                    map.put(i, new ArrayList<>());
-                }
-
-                int numberNode = sc.nextInt();
-                for (int j = 0; j < numberNode; j++) {
-                    int nextPoint = sc.nextInt();
-                    int distance = sc.nextInt();
-                    map.get(i).add(new AbstractMap.SimpleEntry<>(distance, nextPoint));
+            m = sc.nextInt();
+            weight = new int[n][m];
+            distance = new int[n][m];
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    weight[i][j] = sc.nextInt();
+                    distance[i][j] = Integer.MAX_VALUE;
                 }
             }
-            sc.nextLine();
-            String line = sc.nextLine();
 
-            String[] array = line.trim().split("[\\s]+");
-            int start = Integer.valueOf(array[0]);
-            int end = Integer.valueOf(array[1]);
+            distance[0][0] = weight[0][0];
+            PriorityQueue<Map.Entry<Integer, Map.Entry<Integer, Integer>>> priorityQueue = new PriorityQueue<>(
+                            Comparator.comparing(Map.Entry::getKey));
+            priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[0][0], new AbstractMap.SimpleEntry<>(0, 0)));
 
-            if (start == end) {
-                List<Integer> pathList = new ArrayList<>();
-                pathList.add(start);
-                System.out.println(String.format("Case %d: Path = %s; %d second delay", region,
-                                pathList.stream().map(String::valueOf).collect(Collectors.joining(" ")), 0));
-            } else {
-                distance = new int[n + 1];
-                for (int i = 0; i <= n; i++) {
-                    distance[i] = Integer.MAX_VALUE;
-                }
-                distance[start] = 0;
+            while (!priorityQueue.isEmpty()){
+                Map.Entry<Integer, Map.Entry<Integer, Integer>> entry = priorityQueue.poll();
+                Map.Entry<Integer, Integer> position = entry.getValue();
+                int entryDistance = entry.getKey();
 
-                PriorityQueue<Map.Entry<Integer, Integer>> priorityQueue =
-                                new PriorityQueue<>(Comparator.comparing(Map.Entry::getKey));
-                priorityQueue.add(new AbstractMap.SimpleEntry<>(0, start));
-                Map<Integer, Integer> parentMap = new HashMap<>();
+                if(distance[position.getKey()][position.getValue()] == entryDistance){
+                    for(Map.Entry<Integer, Integer> neighbor: getNeighBor(position)){
 
-                while (!priorityQueue.isEmpty()) {
-                    Map.Entry<Integer, Integer> entry = priorityQueue.poll();
-                    int weight = entry.getKey();
-                    int vertex = entry.getValue();
-
-                    if (weight == distance[vertex]) {
-                        if (map.containsKey(vertex)) {
-                            for (Map.Entry<Integer, Integer> neighbor : map.get(vertex)) {
-                                int edgeWeight = neighbor.getKey();
-                                int neighborVertex = neighbor.getValue();
-                                if (edgeWeight + distance[vertex] < distance[neighborVertex]) {
-                                    distance[neighborVertex] = edgeWeight + distance[vertex];
-                                    priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[neighborVertex],
-                                                    neighborVertex));
-                                    parentMap.put(neighborVertex, vertex);
-                                }
-                            }
+                        if(entryDistance + weight[neighbor.getKey()][neighbor.getValue()] < distance[neighbor.getKey()][neighbor.getValue()]){
+                            distance[neighbor.getKey()][neighbor.getValue()] = entryDistance + weight[neighbor.getKey()][neighbor.getValue()];
+                            priorityQueue.add(new AbstractMap.SimpleEntry<>(distance[neighbor.getKey()][neighbor.getValue()],
+                                            new AbstractMap.SimpleEntry<>(neighbor)));
                         }
                     }
                 }
+            }
+            System.out.println(distance[n-1][m-1]);
+        }
+    }
 
-                List<Integer> pathList = new ArrayList<>();
-                pathList.add(end);
-                int nextParent = parentMap.get(pathList.get(pathList.size() - 1));
-                while (nextParent != start) {
-                    pathList.add(nextParent);
-                    nextParent = parentMap.get(pathList.get(pathList.size() - 1));
-                }
-                pathList.add(start);
-                Collections.reverse(pathList);
-                System.out.println(String.format("Case %d: Path = %s; %d second delay", region,
-                                pathList.stream().map(String::valueOf).collect(Collectors.joining(" ")),
-                                distance[end]));
+    private List<Map.Entry<Integer, Integer>> getNeighBor(Map.Entry<Integer, Integer> position) {
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>();
+        int x = position.getKey();
+        int y = position.getValue();
+        for(int i = 0; i < 4; i++){
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if(0 <= nextX && nextX < n && 0 <= nextY && nextY < m){
+                list.add(new AbstractMap.SimpleEntry<>(nextX, nextY));
             }
         }
+        return list;
     }
 
 }
