d0ece9d734c62e29eb0be30150e1807e0af5899b
super1ha1 <khue0011@gmail.com>
2017-10-02 20:52:55 +0800

    solve UVA 729	The Hamming Distance Problem
    given n, h, print binary string length n, with exactly h 1.
    
    define exactly what is variable, first, last.

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -3,10 +3,8 @@ package problem_set;
 import java.util.Scanner;
 
 public class Main {
-    private static final char NORMAL_CELL = '.', STICKER = '*', PILLAR = '#';
-    private int n, m, s, total, x, y;
-    private char[][] arena;
-    private DIRECTION currentDirection;
+    private int n, h;
+    private int[] allocation;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -18,187 +16,45 @@ public class Main {
 //                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
 //        Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-
-        while (true) {
+        int testCase = sc.nextInt();
+        for(int i = 0; i < testCase; i++){
             n = sc.nextInt();
-            m = sc.nextInt();
-            s = sc.nextInt();
-            if (n == 0 && m == 0 && s == 0) {
-                break;
+            h = sc.nextInt();
+            allocation = new int[n];
+            backTrack(0, n - h, h);
+
+            if(i < testCase - 1){
+                System.out.println("");
             }
-            sc.nextLine();
-
-            arena = new char[n][m];
-            for (int i = 0; i < n; i++) {
-                String line = sc.nextLine().trim();
-                arena[i] = line.toCharArray();
-                for (int j = 0; j < m; j++) {
-                    switch (line.charAt(j)) {
-                        case 'N':
-                            currentDirection = DIRECTION.NORTH;
-                            x = i;
-                            y = j;
-                            arena[i][j] = NORMAL_CELL;
-                            break;
-
-                        case 'S':
-                            x = i;
-                            y = j;
-                            currentDirection = DIRECTION.SOUTH;
-                            arena[i][j] = NORMAL_CELL;
-                            break;
-
-                        case 'L':
-                            x = i;
-                            y = j;
-                            currentDirection = DIRECTION.EAST;
-                            arena[i][j] = NORMAL_CELL;
-                            break;
-
-                        case 'O':
-                            x = i;
-                            y = j;
-                            currentDirection = DIRECTION.WEST;
-                            arena[i][j] = NORMAL_CELL;
-                            break;
-
-                        default:
-                            break;
-                    }
-                }
-            }
-
-            String order = sc.nextLine().trim();
-            total = 0;
-            for (int i = 0; i < s; i++) {
-                char c = order.charAt(i);
-                move(c);
-            }
-            System.out.println(total);
         }
     }
 
-    private void move(char c) {
-        if (c == 'D') {
-            turnRight();
-        } else if (c == 'E') {
-            turnLeft();
-        } else if (c == 'F') {
-            moveForward();
+    private void backTrack(int count, int numberZero, int numberOne) {
+        if(count == n){
+            printSolution();
+            return;
+        }
+
+        if(numberZero > 0){
+            //try 0
+            allocation[count] = 0;
+            backTrack(count + 1, numberZero - 1, numberOne);
+
+        }
+
+        if(numberOne > 0){
+            //try 1
+            allocation[count] = 1;
+            backTrack(count + 1, numberZero, numberOne - 1);
         }
     }
 
-    private void moveForward() {
-        int nextX = getNextX();
-        int nextY = getNextY();
-        if(isInArena(nextX, nextY)) {
-            switch (arena[nextX][nextY]){
-                case NORMAL_CELL:
-                    x = nextX;
-                    y = nextY;
-                    break;
-
-                case STICKER:
-                    total++;
-                    x = nextX;
-                    y = nextY;
-                    arena[nextX][nextY] = NORMAL_CELL;
-                    break;
-
-                case PILLAR:
-                    //do nothing
-                    break;
-
-                default:
-                    break;
-            }
-        } else {
-            //do nothing
+    private void printSolution() {
+        String output = "";
+        for(int i = 0; i < n; i++){
+            output += allocation[i];
         }
-    }
-
-    private boolean isInArena(int nextX, int nextY) {
-        return (0 <= nextX && nextX <= n -1 && 0 <= nextY && nextY <= m -1);
-    }
-
-    private int getNextY() {
-        switch (currentDirection) {
-            case EAST:
-                return y + 1;
-
-            case WEST:
-                return y -1;
-
-            default:
-                return y;
-
-        }
-    }
-
-    private int getNextX() {
-        switch (currentDirection) {
-            case NORTH:
-                return x - 1;
-
-            case SOUTH:
-                return x + 1;
-
-            default:
-                return x;
-
-        }
-    }
-
-    private void turnLeft() {
-        switch (currentDirection) {
-            case NORTH:
-                currentDirection = DIRECTION.WEST;
-                break;
-
-            case SOUTH:
-                currentDirection = DIRECTION.EAST;
-                break;
-
-            case EAST:
-                currentDirection = DIRECTION.NORTH;
-                break;
-
-            case WEST:
-                currentDirection = DIRECTION.SOUTH;
-                break;
-
-            default:
-                break;
-
-        }
-    }
-
-    private void turnRight() {
-        switch (currentDirection) {
-            case NORTH:
-                currentDirection = DIRECTION.EAST;
-                break;
-
-            case SOUTH:
-                currentDirection = DIRECTION.WEST;
-                break;
-
-            case EAST:
-                currentDirection = DIRECTION.SOUTH;
-                break;
-
-            case WEST:
-                currentDirection = DIRECTION.NORTH;
-                break;
-
-            default:
-                break;
-
-        }
-    }
-
-    private enum DIRECTION {
-        NORTH, SOUTH, EAST, WEST;
+        System.out.println(output);
     }
 }
 
