e9822f0f9e9adfd0d85010fe56f61f35ac2b51a0
super1ha1 <khue0011@gmail.com>
2017-10-03 14:30:35 +0800

    solve UVA 410 Station Balance use Greedy algorithm to allocate precimen to chamber note: if chamber is empty, there is no space after: 1:..

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,15 +1,17 @@
 package problem_set;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private int n, m;
-    private int[][] distance = new int[1000][1000], weight = new int[1000][1000], visit = new int[1000][1000];
-    private PriorityQueue<Point> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
-
-    private int[] dx = new int[] {-1, 0, 1, 0};
-    private int[] dy = new int[] {0, -1, 0, 1};
+    private int chamber, specimen;
+    private double average, imbalance;
+    private List<Integer> specimensList = new ArrayList<>();
+    private List<List<Integer>> result = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -18,61 +20,67 @@ public class Main {
 
     private void run(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        while (testCase-- > 0) {
-            n = sc.nextInt();
-            m = sc.nextInt();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    weight[i][j] = sc.nextInt();
-                    distance[i][j] = Integer.MAX_VALUE;
-                    visit[i][j] = 0;
-                }
+        int counter = 0;
+        while (sc.hasNext()) {
+            chamber = sc.nextInt();
+            specimen = sc.nextInt();
+            specimensList.clear();
+            result.clear();
+            counter++;
+
+            for (int i = 0; i < specimen; i++) {
+                specimensList.add(sc.nextInt());
             }
 
-            distance[0][0] = weight[0][0];
-            priorityQueue.clear();
-            priorityQueue.add(new Point(0, 0, distance[0][0]));
+            average = ((double) specimensList.stream().reduce(0, (a, b) -> a + b)) / chamber;
 
-            while (!priorityQueue.isEmpty()) {
-                Point currentPoint = priorityQueue.poll();
-                int entryDistance = currentPoint.weight;
-                visit[currentPoint.x][currentPoint.y] = 1;
-
-                if (distance[currentPoint.x][currentPoint.y] == entryDistance) {
-                    for (int i = 0; i < 4; i++) {
-                        int nextX = currentPoint.x + dx[i];
-                        int nextY = currentPoint.y + dy[i];
-                        if (0 <= nextX && nextX < n && 0 <= nextY && nextY < m) {
-
-                            if (entryDistance + weight[nextX][nextY] < distance[nextX][nextY]) {
-                                distance[nextX][nextY] = entryDistance + weight[nextX][nextY];
-                                if(visit[nextX][nextY] == 0){
-                                    priorityQueue.add(new Point(nextX, nextY, distance[nextX][nextY]));
-                                }
-                            }
-                        }
+            if (specimen <= chamber) {
+                for (int i = 0; i < specimen; i++) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(specimensList.get(i));
+                    result.add(list);
+                }
+                for (int i = specimen; i < chamber; i++) {
+                    List<Integer> list = new ArrayList<>();
+                    result.add(list);
+                }
+            } else {
+                for (int i = specimen; i < 2 * chamber; i++) {
+                    specimensList.add(0);
+                }
+                Collections.sort(specimensList);
+                for (int i = 0; i < chamber; i++) {
+                    List<Integer> list = new ArrayList<>();
+                    if(specimensList.get(i) > 0){
+                        list.add(specimensList.get(i));
                     }
+                    list.add(specimensList.get(2 * chamber - 1 - i));
+                    result.add(list);
                 }
             }
-            System.out.println(distance[n - 1][m - 1]);
+
+            imbalance = 0.0;
+            for(int i = 0; i < chamber; i++){
+                if(result.get(i).size() == 0){
+                    imbalance += average;
+                } else {
+                    imbalance += Math.abs(average - result.get(i).stream().reduce(0, (a, b) -> a + b));
+                }
+            }
+            System.out.println(String.format("Set #%d", counter));
+            for(int i = 0; i < chamber; i++){
+                if(result.get(i).size() > 0){
+                    System.out.println(String.format(" %d: %s", i, result.get(i).stream().map(String::valueOf).collect(Collectors.joining(" "))));
+                } else {
+                    System.out.println(String.format(" %d:", i));
+                }
+            }
+            System.out.println(String.format("IMBALANCE = %.5f", imbalance));
+            System.out.println("");
         }
     }
-
-    static class Point {
-        public final int x;
-        public final int y;
-        public final int weight;
-
-        public Point(int x, int y, int weight) {
-            this.x = x;
-            this.y = y;
-            this.weight = weight;
-        }
-    }
-
 }
 
