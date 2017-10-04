cb4a6faf1016fd9e1b9d6dbe200d32c0aaad63b1
super1ha1 <khue0011@gmail.com>
2017-10-04 14:29:32 +0800

    not solve yet UVA 10020	Minimal coverage get wrong answer: use greedy add member with max end to list

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,8 +1,13 @@
 package problem_set;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    private int m, low;
+    private List<Edge> resultList = new ArrayList<>();
+    private List<Edge> list = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -11,30 +16,63 @@ public class Main {
 
     private void run(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()) {
-            String line = sc.nextLine().trim();
-            String[] array = line.split("[\\s]+");
-            System.out.println(isSubSequence(array[0], array[1]) ? "Yes" : "No");
+        int testCase = sc.nextInt();
+        for (int i = 0; i < testCase; i++) {
+            m = sc.nextInt();
+            resultList.clear();
+            list.clear();
+
+            while (true) {
+                int start = sc.nextInt();
+                int end = sc.nextInt();
+                if (start == 0 && end == 0) {
+                    break;
+                }
+                if(end <= 0 || start >= m){
+
+                } else {
+                    list.add(new Edge(start, end));
+                }
+            }
+
+            low = 0;
+
+            while (true){
+                List<Edge> updatedList = list.stream().filter(e -> e.start <= low && e.end > low).collect(Collectors.toList());
+                if(updatedList.size() == 0){
+                    break;
+                }
+                Collections.sort(updatedList, (o1, o2) -> o2.end - o1.end);
+                Edge selectedEdge = updatedList.get(0);
+                resultList.add(selectedEdge);
+                low = selectedEdge.end;
+            }
+
+            if(low >= m) {
+                Collections.sort(resultList, Comparator.comparingInt(o -> o.start));
+                System.out.println(resultList.size());
+                for(Edge e: resultList){
+                    System.out.println(e.start + " " + e.end);
+                }
+            } else {
+                System.out.println(0);
+            }
+            if (i < testCase - 1) {
+                System.out.println("");
+            }
         }
     }
 
-    private boolean isSubSequence(String s, String t) {
-        if (t.length() < s.length()) {
-            return false;
+    static class Edge {
+        public final int start, end;
+
+        public Edge(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
-        while (s.length() > 0) {
-            char first = s.charAt(0);
-            int index = t.indexOf(first);
-            if(index < 0){
-                return false;
-            }
-            s = s.substring(1);
-            t = t.substring(index + 1);
-        }
-        return true;
     }
 }
 
