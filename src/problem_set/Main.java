55dfe7b838dbc248a22762f2257ef369ddde0bc4
super1ha1 <khue0011@gmail.com>
2017-10-05 13:57:10 +0800

    not solve yet: correct algo, might be miss tricky case UVA 796 - Critical Links

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,13 +1,14 @@
 package problem_set;
 
+import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    private int m, low;
+    private int n, dfsComponent;
     private List<Edge> resultList = new ArrayList<>();
-    private List<Edge> list = new ArrayList<>();
+    private Map<Integer, Set<Integer>> map = new HashMap<>();
+    private int[] dfsLow, dfsNum, dfsParent;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -15,53 +16,76 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        for (int i = 0; i < testCase; i++) {
-            m = sc.nextInt();
+        while (sc.hasNext()) {
+            n = sc.nextInt();
+            sc.nextLine();
+            map.clear();
+
+            for (int i = 0; i < n; i++) {
+                String line = sc.nextLine().trim();
+                String[] array = line.split("[\\s]+");
+                int first = Integer.valueOf(array[0]);
+                if (!map.containsKey(first)) {
+                    map.put(first, new HashSet<>());
+                }
+                // parse (1)
+                int num = Integer.valueOf(String.valueOf(array[1].charAt(1)));
+
+                for (int j = 0; j < num; j++) {
+                    int second = Integer.valueOf(array[2 + j]);
+                    if (!map.containsKey(second)) {
+                        map.put(second, new HashSet<>());
+                    }
+                    map.get(first).add(second);
+                    map.get(second).add(first);
+                }
+            }
+
             resultList.clear();
-            list.clear();
+            dfsLow = new int[n];
+            dfsNum = new int[n];
+            dfsParent = new int[n];
+            dfsComponent = 0;
 
-            while (true) {
-                int start = sc.nextInt();
-                int end = sc.nextInt();
-                if (start == 0 && end == 0) {
-                    break;
-                }
-                if(end <= 0 || start >= m){
-
-                } else {
-                    list.add(new Edge(start, end));
+            for (int i = 0; i < n; i++) {
+                if (dfsNum[i] == 0) {
+                    articulationPoint(i);
                 }
             }
-
-            low = 0;
-
-            while (true){
-                List<Edge> updatedList = list.stream().filter(e -> e.start <= low && e.end > low).collect(Collectors.toList());
-                if(updatedList.size() == 0){
-                    break;
-                }
-                Collections.sort(updatedList, (o1, o2) -> o2.end - o1.end);
-                Edge selectedEdge = updatedList.get(0);
-                resultList.add(selectedEdge);
-                low = selectedEdge.end;
+            System.out.println(String.format("%d critical links", resultList.size()));
+            Collections.sort(resultList, (Comparator.comparingInt(o -> o.start)));
+            for(Edge edge: resultList){
+                System.out.println(String.format("%d - %d", edge.start, edge.end));
             }
+            System.out.println("");
+        }
 
-            if(low >= m) {
-                Collections.sort(resultList, Comparator.comparingInt(o -> o.start));
-                System.out.println(resultList.size());
-                for(Edge e: resultList){
-                    System.out.println(e.start + " " + e.end);
+    }
+
+    private void articulationPoint(int node) {
+        dfsComponent++;
+        dfsNum[node] = dfsComponent;
+        dfsLow[node] = dfsComponent;
+
+        if (map.containsKey(node)) {
+            for (int neighbor : map.get(node)) {
+                if (dfsNum[neighbor] == 0) {
+                    dfsParent[neighbor] = node;
+                    articulationPoint(neighbor);
+
+                    if (dfsLow[neighbor] > dfsNum[node]) {
+                        resultList.add(new Edge(Math.min(node, neighbor), Math.max(node, neighbor)));
+                    }
+
+                    dfsLow[node] = Math.min(dfsLow[node], dfsLow[neighbor]);
+
+                } else if (dfsParent[node] != neighbor) {
+                    dfsLow[node] = Math.min(dfsLow[node], dfsNum[neighbor]);
                 }
-            } else {
-                System.out.println(0);
-            }
-            if (i < testCase - 1) {
-                System.out.println("");
             }
         }
     }
