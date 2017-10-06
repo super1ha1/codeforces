8be463416d7e4b3269b91347391f02f94baa4ae1
super1ha1 <khue0011@gmail.com>
2017-10-06 13:29:03 +0800

    solve UVA 	11747	Heavy Cycle Edges collect all heavy edge in graph that not in Minimum Spanning tree

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,14 +1,14 @@
 package problem_set;
 
-import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private int n, dfsComponent;
-    private List<Edge> resultList = new ArrayList<>();
-    private Map<Integer, Set<Integer>> map = new HashMap<>();
-    private int[] dfsLow, dfsNum, dfsParent;
+    private List<Edge> edgeList = new ArrayList<>();
+    private int n, m;
+    private int[] pset = new int[1010];
+    private PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -16,86 +16,69 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-//                Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+                Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
         while (sc.hasNext()) {
             n = sc.nextInt();
-            sc.nextLine();
-            map.clear();
-
-            for (int i = 0; i < n; i++) {
-                String line = sc.nextLine().trim();
-                String[] array = line.split("[\\s]+");
-                int first = Integer.valueOf(array[0]);
-                if (!map.containsKey(first)) {
-                    map.put(first, new HashSet<>());
-                }
-                // parse (1)
-                int num = Integer.valueOf(String.valueOf(array[1].charAt(1)));
-
-                for (int j = 0; j < num; j++) {
-                    int second = Integer.valueOf(array[2 + j]);
-                    if (!map.containsKey(second)) {
-                        map.put(second, new HashSet<>());
-                    }
-                    map.get(first).add(second);
-                    map.get(second).add(first);
-                }
+            m = sc.nextInt();
+            if (n == 0 && m == 0) {
+                break;
             }
+            edgeList.clear();
+            priorityQueue.clear();
+            initSet(n);
 
-            resultList.clear();
-            dfsLow = new int[n];
-            dfsNum = new int[n];
-            dfsParent = new int[n];
-            dfsComponent = 0;
-
-            for (int i = 0; i < n; i++) {
-                if (dfsNum[i] == 0) {
-                    articulationPoint(i);
+            for (int i = 0; i < m; i++) {
+                int first = sc.nextInt();
+                int second = sc.nextInt();
+                int weight = sc.nextInt();
+                priorityQueue.add(new Edge(first, second, weight));
+            }
+            while (!priorityQueue.isEmpty()) {
+                Edge currentEdge = priorityQueue.poll();
+                if (!isSameSet(currentEdge.start, currentEdge.end)) {
+                    unionSet(currentEdge.start, currentEdge.end);
+                } else {
+                    edgeList.add(currentEdge);
                 }
             }
-            System.out.println(String.format("%d critical links", resultList.size()));
-            Collections.sort(resultList, (Comparator.comparingInt(o -> o.start)));
-            for(Edge edge: resultList){
-                System.out.println(String.format("%d - %d", edge.start, edge.end));
+            if (edgeList.size() > 0) {
+                Collections.sort(edgeList, Comparator.comparingInt(o -> o.weight));
+                System.out.println(edgeList.stream().map(edge -> edge.weight).map(String::valueOf)
+                                .collect(Collectors.joining(" ")));
+            } else {
+                System.out.println("forest");
             }
-            System.out.println("");
         }
-
     }
 
-    private void articulationPoint(int node) {
-        dfsComponent++;
-        dfsNum[node] = dfsComponent;
-        dfsLow[node] = dfsComponent;
+    private void unionSet(int start, int end) {
+        pset[findSet(start)] = findSet(end);
+    }
 
-        if (map.containsKey(node)) {
-            for (int neighbor : map.get(node)) {
-                if (dfsNum[neighbor] == 0) {
-                    dfsParent[neighbor] = node;
-                    articulationPoint(neighbor);
+    private boolean isSameSet(int start, int end) {
+        return findSet(start) == findSet(end);
+    }
 
-                    if (dfsLow[neighbor] > dfsNum[node]) {
-                        resultList.add(new Edge(Math.min(node, neighbor), Math.max(node, neighbor)));
-                    }
+    private int findSet(int i) {
+        return (pset[i] == i ? i : (pset[i] = findSet(pset[i])));
+    }
 
-                    dfsLow[node] = Math.min(dfsLow[node], dfsLow[neighbor]);
-
-                } else if (dfsParent[node] != neighbor) {
-                    dfsLow[node] = Math.min(dfsLow[node], dfsNum[neighbor]);
-                }
-            }
+    private void initSet(int n) {
+        for (int i = 0; i < n; i++) {
+            pset[i] = i;
         }
     }
 
     static class Edge {
-        public final int start, end;
+        public final int start, end, weight;
 
-        public Edge(int start, int end) {
+        public Edge(int start, int end, int weight) {
             this.start = start;
             this.end = end;
+            this.weight = weight;
         }
     }
 }
