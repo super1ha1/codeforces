9a91d523b9f642ed1d5e8afc70f8ff4c60b23065
super1ha1 <khue0011@gmail.com>
2017-10-09 14:08:30 +0800

    solve UVA 558	Wormholes Bellman Ford SSSP: find whether negative cycle exist

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,14 +1,12 @@
 package problem_set;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    private List<Edge> edgeList = new ArrayList<>();
     private int n, m;
-    private int[] pset = new int[1010];
-    private PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
+    private int[] distance = new int[1010];
+    private Map<Integer, List<Edge>> map = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -16,67 +14,59 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-                Scanner sc = new Scanner(System.in);
-//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(System.in);
+//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        while (sc.hasNext()) {
+        int testCase = sc.nextInt();
+        while (testCase-- > 0) {
             n = sc.nextInt();
             m = sc.nextInt();
-            if (n == 0 && m == 0) {
-                break;
-            }
-            edgeList.clear();
-            priorityQueue.clear();
-            initSet(n);
+            map.clear();
 
             for (int i = 0; i < m; i++) {
                 int first = sc.nextInt();
                 int second = sc.nextInt();
                 int weight = sc.nextInt();
-                priorityQueue.add(new Edge(first, second, weight));
+                if (!map.containsKey(first)) {
+                    map.put(first, new ArrayList<>());
+                }
+                map.get(first).add(new Edge(second, weight));
             }
-            while (!priorityQueue.isEmpty()) {
-                Edge currentEdge = priorityQueue.poll();
-                if (!isSameSet(currentEdge.start, currentEdge.end)) {
-                    unionSet(currentEdge.start, currentEdge.end);
-                } else {
-                    edgeList.add(currentEdge);
+
+            for (int i = 0; i < n; i++) {
+                distance[i] = Integer.MAX_VALUE;
+            }
+            distance[0] = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int node = 0; node < n; node++) {
+                    if (map.containsKey(node)) {
+                        for (Edge neighbor : map.get(node)) {
+                            distance[neighbor.end] = Math.min(distance[neighbor.end], distance[node] + neighbor.weight);
+                        }
+                    }
                 }
             }
-            if (edgeList.size() > 0) {
-                Collections.sort(edgeList, Comparator.comparingInt(o -> o.weight));
-                System.out.println(edgeList.stream().map(edge -> edge.weight).map(String::valueOf)
-                                .collect(Collectors.joining(" ")));
-            } else {
-                System.out.println("forest");
+            boolean possible = false;
+            for (int node = 0; node < n; node++) {
+                if (map.containsKey(node)) {
+                    for (Edge neighbor : map.get(node)) {
+                        if (distance[neighbor.end] > distance[node] + neighbor.weight) {
+                            possible = true;
+                        }
+                    }
+                }
             }
+            System.out.println(possible ? "possible" : "not possible");
         }
     }
 
-    private void unionSet(int start, int end) {
-        pset[findSet(start)] = findSet(end);
-    }
-
-    private boolean isSameSet(int start, int end) {
-        return findSet(start) == findSet(end);
-    }
-
-    private int findSet(int i) {
-        return (pset[i] == i ? i : (pset[i] = findSet(pset[i])));
-    }
-
-    private void initSet(int n) {
-        for (int i = 0; i < n; i++) {
-            pset[i] = i;
-        }
-    }
 
     static class Edge {
-        public final int start, end, weight;
+        public final int end, weight;
 
-        public Edge(int start, int end, int weight) {
-            this.start = start;
+        public Edge(int end, int weight) {
             this.end = end;
             this.weight = weight;
         }
