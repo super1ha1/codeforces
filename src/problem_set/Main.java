93993fb5b454f00caa72aec9c7d2ab1206ecf250
super1ha1 <khue0011@gmail.com>
2017-10-09 14:48:40 +0800

    not solve yet UVA 10557	XYZZY: Wrong answer

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Main {
 
-    private int n, m;
-    private int[] distance = new int[1010];
-    private Map<Integer, List<Edge>> map = new HashMap<>();
+    private int n, start, finish;
+    private int[] distance, weightList;
+    private Map<Integer, List<Integer>> map = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -14,61 +14,78 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+                Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        while (testCase-- > 0) {
+        while (true) {
             n = sc.nextInt();
-            m = sc.nextInt();
+            if (n == -1) {
+                break;
+            }
+
+            weightList = new int[n + 10];
             map.clear();
 
-            for (int i = 0; i < m; i++) {
-                int first = sc.nextInt();
-                int second = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
                 int weight = sc.nextInt();
-                if (!map.containsKey(first)) {
-                    map.put(first, new ArrayList<>());
+                weightList[i] = -1 * weight;
+
+                if (!map.containsKey(i)) {
+                    map.put(i, new ArrayList<>());
                 }
-                map.get(first).add(new Edge(second, weight));
+                int numberOfNode = sc.nextInt();
+                for (int j = 0; j < numberOfNode; j++) {
+                    int next = sc.nextInt();
+                    map.get(i).add(next);
+                }
+
+                if (weight == 0) {
+                    if (numberOfNode > 0) {
+                        start = i;
+                    } else if (numberOfNode == 0) {
+                        finish = i;
+                    }
+                }
             }
 
-            for (int i = 0; i < n; i++) {
+            distance = new int[n + 10];
+            for (int i = 1; i <= n; i++) {
                 distance[i] = Integer.MAX_VALUE;
             }
-            distance[0] = 0;
+            distance[start] = -100;
 
-            for (int i = 0; i < n; i++) {
-                for (int node = 0; node < n; node++) {
+            for (int i = 1; i <= n; i++) {
+                for (int node = 1; node <= n; node++) {
                     if (map.containsKey(node)) {
-                        for (Edge neighbor : map.get(node)) {
-                            distance[neighbor.end] = Math.min(distance[neighbor.end], distance[node] + neighbor.weight);
+                        for (int neighbor : map.get(node)) {
+                            distance[neighbor] = Math.min(distance[neighbor], distance[node] + weightList[neighbor]);
                         }
                     }
                 }
             }
+
             boolean possible = false;
-            for (int node = 0; node < n; node++) {
-                if (map.containsKey(node)) {
-                    for (Edge neighbor : map.get(node)) {
-                        if (distance[neighbor.end] > distance[node] + neighbor.weight) {
-                            possible = true;
+            if (distance[finish] < 0) {
+                possible = true;
+            } else {
+                for (int node = 1; node <= n; node++) {
+                    if (map.containsKey(node)) {
+                        for (int neighbor : map.get(node)) {
+                            if (distance[neighbor] > distance[node] + weightList[neighbor]) {
+                                possible = true;
+                            }
                         }
                     }
                 }
             }
-            System.out.println(possible ? "possible" : "not possible");
-        }
-    }
-
-
-    static class Edge {
-        public final int end, weight;
-
-        public Edge(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
+            if(n == 0){
+                possible = false;
+            }
+            if(n == 1){
+                possible = true;
+            }
+            System.out.println(possible ? "winnable" : "hopeless");
         }
     }
 }
