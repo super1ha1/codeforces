a7e70092787c18c5d9fe00ac35650af6e6aeefee
super1ha1 <khue0011@gmail.com>
2017-10-17 15:02:56 +0800

    not solve yet UVA 10827 - Maximum sum on a torus wrong answer

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    private int number, n;
-    private int[] values;
+    private int testCase, n;
+    private int[][] values;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -16,56 +16,50 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 //                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        number = sc.nextInt();
-        for (int route = 1; route <= number; route++) {
+        testCase = sc.nextInt();
+        for (int route = 1; route <= testCase; route++) {
             n = sc.nextInt();
-            values = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
-                values[i] = sc.nextInt();
-            }
-            int maxSoFar = Integer.MIN_VALUE;
-            int maxEndHere = 0;
-            int start = n;
-            int end = 1;
-
-            int startLocal = 1;
-            int endLocal;
-
-            for (int i = 0; i < n - 1; i++) {
-                maxEndHere += values[i];
-
-                if (maxEndHere > maxSoFar) {
-                    maxSoFar = maxEndHere;
-                    endLocal = i + 2;
-
-                    start = startLocal;
-                    end = endLocal;
+            values = new int[n][n];
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    values[i][j] = sc.nextInt();
+                    if(i > 0) values[i][j] += values[i-1][j];
+                    if(j > 0) values[i][j] += values[i][j -1];
+                    if( i > 0 && j > 0) values[i][j] -= values[i-1][j -1];
                 }
+            }
 
-                if (maxEndHere == maxSoFar) {
-                    endLocal = i + 2;
+            int maxSoFar = -100 * 75 * 75;
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    int maxEndHere = -100 * 75 * 75;
+                    for(int k = i; k < n; k++){
+                        for(int h = j; h < n; h++){
+                            // from i, j to k, l
+                            int sum = values[k][h];
+                            if(i > 0) sum -= values[i-1][h];
+                            if(j > 0) sum -= values[k][j-1];
+                            if(i > 0 && j > 0) sum += values[i-1][j-1];
+                            maxEndHere = Math.max(maxEndHere, sum);
 
-                    // check length
-                    if(endLocal - startLocal > end - start){
-                        start = startLocal;
-                        end = endLocal;
-                    } else if(endLocal - startLocal == end - start){
-                        // no thing
+                            // top down other
+                            int sumTopDown = values[n-1][h];
+                            if(j > 0) sumTopDown -= values[n-1][j-1];
+                            sumTopDown -= sum;
+                            maxEndHere = Math.max(maxEndHere, sumTopDown);
+
+                            // left right other
+                            int sumLeftRight = values[k][n-1];
+                            if(i > 0) sumLeftRight -= values[i-1][n-1];
+                            sumLeftRight -= sum;
+                            maxEndHere = Math.max(maxEndHere, sumLeftRight);
+                        }
                     }
-                }
-
-                if (maxEndHere < 0) {
-                    maxEndHere = 0;
-                    startLocal = i + 2;
+                    maxSoFar = Math.max(maxEndHere, maxSoFar);
                 }
             }
 
-            if (maxSoFar < 0) {
-                System.out.println(String.format("Route %d has no nice parts", route));
-            } else {
-                System.out.println(String.format("The nicest part of route %d is between stops %d and %d", route, start,
-                                end));
-            }
+            System.out.println(maxSoFar);
         }
     }
 }
