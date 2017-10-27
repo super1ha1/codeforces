45d6e977bb2058edd70f5d690e25c96c17c739f5
super1ha1 <khue0011@gmail.com>
2017-10-27 14:28:14 +0800

    solve UVA 10306	e-Coins just change the logic abit, to find the x and y that total square = n^2 then manually find min number of coin to compute x,y

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,11 +1,13 @@
 package problem_set;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    private int testCase, n;
-    private int[][] values;
+    private static final int largeValue = 1000;
+    private int[][] coins;
+    private int value;
+    private int[][] dp;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -14,69 +16,72 @@ public class Main {
 
     private void run(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        testCase = sc.nextInt();
-        for (int route = 1; route <= testCase; route++) {
-            n = sc.nextInt();
-            values = new int[n][n];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    values[i][j] = sc.nextInt();
-                    if(i > 0) values[i][j] += values[i-1][j];
-                    if(j > 0) values[i][j] += values[i][j -1];
-                    if( i > 0 && j > 0) values[i][j] -= values[i-1][j -1];
-                }
+
+        int testCase = sc.nextInt();
+        while (testCase-- > 0) {
+            int n = sc.nextInt();
+            coins = new int[n][2];
+            value = sc.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                coins[i][0] = sc.nextInt();
+                coins[i][1] = sc.nextInt();
+            }
+            List<Map.Entry<Integer, Integer>> square = findSquare(value);
+            int min = largeValue;
+
+            for (Map.Entry<Integer, Integer> entry: square) {
+                min = Math.min(min, findCoin(entry.getKey(), entry.getValue()));
             }
 
-            int maxSoFar = -100 * 75 * 75;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    int maxEndHere = -100 * 75 * 75;
-                    for(int k = i; k < n; k++){
-                        for(int h = j; h < n; h++){
-                            // from i, j to k, l
-                            int sum = values[k][h];
-                            if(i > 0) sum -= values[i-1][h];
-                            if(j > 0) sum -= values[k][j-1];
-                            if(i > 0 && j > 0) sum += values[i-1][j-1];
-                            maxEndHere = Math.max(maxEndHere, sum);
-
-                            // top down other
-                            int sumTopDown = values[n-1][h];
-                            if(j > 0) sumTopDown -= values[n-1][j-1];
-                            if(i > 0 || k < n -1) sumTopDown -= sum;
-                            maxEndHere = Math.max(maxEndHere, sumTopDown);
-
-                            // left right other
-                            int sumLeftRight = values[k][n-1];
-                            if(i > 0) sumLeftRight -= values[i-1][n-1];
-                            if(j > 0 || h < n -1) sumLeftRight -= sum;
-                            maxEndHere = Math.max(maxEndHere, sumLeftRight);
-
-                            // sum 4 corner, ignore case whole sub rectangle
-                            if(i == 0 && j == 0 && k == n-1 && h == n-1){
-                                // do nothing
-                            } else {
-                                int sumCorner = values[n-1][n-1];
-                                if(i == 0 && k == n -1){
-                                    sumCorner -= sumTopDown;
-                                }else if(j == 0 && h == n -1){
-                                    sumCorner -= sumLeftRight;
-                                } else {
-                                    sumCorner -= sumTopDown + sumLeftRight + sum;
-                                }
-                                maxEndHere = Math.max(maxEndHere, sumCorner);
-                            }
-
-                        }
-                    }
-                    maxSoFar = Math.max(maxEndHere, maxSoFar);
-                }
+            if (min > 0 && min < largeValue) {
+                System.out.println(min);
+            } else {
+                System.out.println("not possible");
             }
-
-            System.out.println(maxSoFar);
         }
+    }
+
+    private int findCoin(int x, int y) {
+        dp = new int[310][310];
+        dp[0][0] = 0;
+        return topDownDP(x, y);
+    }
+
+    private int topDownDP(int x, int y) {
+        if(x < 0 || y < 0){
+            return largeValue;
+        }
+        if(x == 0 && y == 0){
+            return 0;
+        }
+
+        if(dp[x][y] > 0){
+            return dp[x][y];
+        }
+        int min = largeValue;
+        for(int[] coinValue: coins){
+            min = Math.min(min, 1 + topDownDP(x - coinValue[0], y - coinValue[1]));
+        }
+        return dp[x][y] = min;
+    }
+
+    private List<Map.Entry<Integer, Integer>> findSquare(int value) {
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= value; i++) {
+            int square = value * value - i * i;
+            if (isPerfectSquare(square)) {
+                list.add(new AbstractMap.SimpleEntry<>(i, (int) Math.sqrt(square)));
+            }
+        }
+        return list;
+    }
+
+    private boolean isPerfectSquare(int square) {
+        int root = (int) Math.sqrt(square);
+        return square == root * root;
     }
 }
 
