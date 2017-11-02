e106efba9bb329e41df3f2217ad48670984b6ed7
super1ha1 <khue0011@gmail.com>
2017-11-02 12:31:31 +0800

    UVA   821 - Page Hopping direct apply of Floyd Warshall algorithm

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,87 +1,82 @@
 package problem_set;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     private static final int largeValue = 1000;
-    private int[][] coins;
-    private int value;
-    private int[][] dp;
-
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.run(args);
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//                        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+                Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
 
-        int testCase = sc.nextInt();
-        while (testCase-- > 0) {
-            int n = sc.nextInt();
-            coins = new int[n][2];
-            value = sc.nextInt();
-
-            for (int i = 0; i < n; i++) {
-                coins[i][0] = sc.nextInt();
-                coins[i][1] = sc.nextInt();
-            }
-            List<Map.Entry<Integer, Integer>> square = findSquare(value);
-            int min = largeValue;
-
-            for (Map.Entry<Integer, Integer> entry: square) {
-                min = Math.min(min, findCoin(entry.getKey(), entry.getValue()));
-            }
-
-            if (min > 0 && min < largeValue) {
-                System.out.println(min);
+        int count = 0;
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
             } else {
-                System.out.println("not possible");
+                List<Edge> list = new ArrayList<>();
+                list.add(new Edge(a, b));
+                count++;
+                while (true) {
+                    int c = sc.nextInt();
+                    int d = sc.nextInt();
+                    if (c == 0 && d == 0) {
+                        break;
+                    }
+                    list.add(new Edge(c, d));
+                }
+
+                int[][] array = new int[101][101];
+                for (int i = 1; i < 101; i++) {
+                    for (int j = 1; j < 101; j++) {
+                       array[i][j] = largeValue;
+                    }
+                }
+
+                for (Edge e : list) {
+                    array[e.x][e.y] = 1;
+                }
+                for (int k = 1; k < 101; k++) {
+                    for (int i = 1; i < 101; i++) {
+                        for (int j = 1; j < 101; j++) {
+                            // only if there is path i to k and k to j
+                            array[i][j] = Math.min(array[i][j], array[i][k] + array[k][j]);
+                        }
+                    }
+                }
+                int counter = 0;
+                int sum = 0;
+                for (int i = 1; i < 101; i++) {
+                    for (int j = 1; j < 101; j++) {
+                        if (i != j && array[i][j] < largeValue) {
+                            sum += array[i][j];
+                            counter++;
+                        }
+                    }
+                }
+                double result = (1.0 * sum)/counter;
+                System.out.println(String.format("Case %d: average length between pages = %.3f clicks", count, result));
             }
         }
     }
 
-    private int findCoin(int x, int y) {
-        dp = new int[310][310];
-        dp[0][0] = 0;
-        return topDownDP(x, y);
-    }
+    static class Edge {
+        public final int x, y;
 
-    private int topDownDP(int x, int y) {
-        if(x < 0 || y < 0){
-            return largeValue;
+        public Edge(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        if(x == 0 && y == 0){
-            return 0;
-        }
-
-        if(dp[x][y] > 0){
-            return dp[x][y];
-        }
-        int min = largeValue;
-        for(int[] coinValue: coins){
-            min = Math.min(min, 1 + topDownDP(x - coinValue[0], y - coinValue[1]));
-        }
-        return dp[x][y] = min;
-    }
-
-    private List<Map.Entry<Integer, Integer>> findSquare(int value) {
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= value; i++) {
-            int square = value * value - i * i;
-            if (isPerfectSquare(square)) {
-                list.add(new AbstractMap.SimpleEntry<>(i, (int) Math.sqrt(square)));
-            }
-        }
-        return list;
-    }
-
-    private boolean isPerfectSquare(int square) {
-        int root = (int) Math.sqrt(square);
-        return square == root * root;
     }
 }
 
