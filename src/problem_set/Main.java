c50fc0805eab20595faa41d588e6918acb5b34bb
super1ha1 <khue0011@gmail.com>
2017-12-15 13:17:38 +0800

    UVA 787 Maximum Sub-Sequence Product: - use straight forward way: input size is small: 100 => O(n^2) - use BigInteger, since the value is very large

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,7 +1,7 @@
 package problem_set;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,90 +15,38 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//                Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+                Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-
-        int value = -1;
-        try {
+        while (sc.hasNext()) {
             inputList = new ArrayList<>();
-            while (sc.hasNext()) {
-                value = sc.nextInt();
+            while (true){
+                int value = sc.nextInt();
+                if(value == -999999) {
+                    break;
+                }
                 inputList.add(value);
             }
-        } catch (Exception e) {
-            System.out.println("value: " + value);
-            e.printStackTrace();
-        }
+            if(inputList.size() == 0) {
+                continue;
+            }
 
-        int n = inputList.size();
-        List<Integer> indexList = new ArrayList<>();
-        indexList.add(0);
-        int[] parent = new int[n];
-        for(int i = 0; i < n; i++){
-            parent[i] = -1;
-        }
+            BigInteger maxValue = BigInteger.valueOf(Long.MIN_VALUE);
+            for(int i = 0; i < inputList.size(); i++) {
+                BigInteger max = BigInteger.valueOf(inputList.get(i));
+                BigInteger value = BigInteger.valueOf(inputList.get(i));
+                for (int j = i + 1; j < inputList.size(); j++) {
+                    value = value.multiply(BigInteger.valueOf(inputList.get(j)));
+                    if(value.compareTo(max) > 0) {
+                        max = value;
+                    }
+                }
 
-        for (int i = 1; i < n; i++) {
-            int currentValue = inputList.get(i);
-            int lastValue = inputList.get(indexList.get(indexList.size() - 1));
-            if (currentValue > lastValue) {
-                // case 3: value larger than all in inputList
-                parent[i] = indexList.get(indexList.size() - 1);
-                indexList.add(i);
-
-            } else {
-                // case 2: in the middle
-                int indexMinLarger = customBinarySearch(indexList, currentValue);
-                indexList.add(indexMinLarger + 1, i);
-                indexList.remove(indexMinLarger);
-
-                if(indexMinLarger > 0) {
-                    parent[i] = indexList.get(indexMinLarger -1);
+                if(max.compareTo(maxValue) > 0)  {
+                    maxValue = max;
                 }
             }
-        }
-
-        List<Integer> resultList = new ArrayList<>();
-        int parentIndex = indexList.get(indexList.size() -1);
-        resultList.add(inputList.get(parentIndex));
-        while (true) {
-            parentIndex = parent[parentIndex];
-            if(parentIndex == -1) {
-                break;
-            }
-            resultList.add(inputList.get(parentIndex));
-        }
-
-        Collections.reverse(resultList);
-        System.out.println(resultList.size());
-        System.out.println("-");
-        for (int i : resultList) {
-            System.out.println(i);
-        }
-    }
-
-    private int customBinarySearch(List<Integer> list, int currentValue) {
-        return binarySearch(list, 0, list.size() -1, currentValue);
-    }
-
-    private int binarySearch(List<Integer> list, int low, int high, int value) {
-        if(high == low) {
-            if(inputList.get(list.get(low)) >= value) {
-                return low;
-            } else {
-                return low + 1;
-            }
-        }
-
-        int mid = (high + low) /2;
-        int midValue = inputList.get(list.get(mid));
-        if(midValue < value) {
-            return binarySearch(list, mid + 1, high, value);
-        } else if(midValue > value) {
-            return binarySearch(list, low, mid, value);
-        } else {
-            return mid;
+            System.out.println(maxValue);
         }
     }
 }
