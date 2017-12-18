9ee3b644daca25c059884cbc6f673dd91b5e4630
super1ha1 <khue0011@gmail.com>
2017-12-18 13:26:28 +0800

    UVA 10684 The jackpot https://uva.onlinejudge.org/external/106/10684.pdf - use Kadane algorithm to find maximum consecutive subsequence sum

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -1,13 +1,8 @@
 package problem_set;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-    private List<Integer> inputList;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -15,38 +10,30 @@ public class Main {
     }
 
     private void run(String[] args) throws Exception {
-                Scanner sc = new Scanner(System.in);
-//        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
+        Scanner sc = new Scanner(System.in);
+        //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
-        while (sc.hasNext()) {
-            inputList = new ArrayList<>();
-            while (true){
-                int value = sc.nextInt();
-                if(value == -999999) {
-                    break;
-                }
-                inputList.add(value);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
             }
-            if(inputList.size() == 0) {
-                continue;
-            }
-
-            BigInteger maxValue = BigInteger.valueOf(Long.MIN_VALUE);
-            for(int i = 0; i < inputList.size(); i++) {
-                BigInteger max = BigInteger.valueOf(inputList.get(i));
-                BigInteger value = BigInteger.valueOf(inputList.get(i));
-                for (int j = i + 1; j < inputList.size(); j++) {
-                    value = value.multiply(BigInteger.valueOf(inputList.get(j)));
-                    if(value.compareTo(max) > 0) {
-                        max = value;
-                    }
+            int maxSoFar = -1000000;
+            int maxEndHere = 0;
+            for (int i = 0; i < n; i++) {
+                maxEndHere += sc.nextInt();
+                if (maxEndHere > maxSoFar) {
+                    maxSoFar = maxEndHere;
                 }
-
-                if(max.compareTo(maxValue) > 0)  {
-                    maxValue = max;
+                if (maxEndHere < 0) {
+                    maxEndHere = 0;
                 }
             }
-            System.out.println(maxValue);
+            if (maxSoFar <= 0) {
+                System.out.println("Losing streak.");
+            } else {
+                System.out.println(String.format("The maximum winning streak is %d.", maxSoFar));
+            }
         }
     }
 }
