6623ec46e8df22f3aba3b3efc88ef757bda5ddb0
super1ha1 <khue0011@gmail.com>
2017-12-18 13:55:21 +0800

    UVA 10487 - Closest Sums iterative 2 loops to find closet sum

--- src/problem_set/Main.java
+++ src/problem_set/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
+    private int[] array;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -14,28 +15,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         //        Scanner sc = new Scanner(new File("C:\\toolbar_local\\workspace\\Testing\\codeforces\\in.txt"));
         //                 Scanner sc = new Scanner(new File("/Users/dackhue.nguyen/toolbar_local/workspace/codeforces/in.txt"));
+        int counter = 0;
         while (true) {
             int n = sc.nextInt();
             if (n == 0) {
                 break;
             }
-            int maxSoFar = -1000000;
-            int maxEndHere = 0;
+            counter++;
+            System.out.println(String.format("Case %d:", counter));
+            array = new int[n];
             for (int i = 0; i < n; i++) {
-                maxEndHere += sc.nextInt();
-                if (maxEndHere > maxSoFar) {
-                    maxSoFar = maxEndHere;
-                }
-                if (maxEndHere < 0) {
-                    maxEndHere = 0;
-                }
+                array[i] = sc.nextInt();
             }
-            if (maxSoFar <= 0) {
-                System.out.println("Losing streak.");
-            } else {
-                System.out.println(String.format("The maximum winning streak is %d.", maxSoFar));
+            int m = sc.nextInt();
+            for (int i = 0; i < m; i++) {
+                int query = sc.nextInt();
+                System.out.println(String.format("Closest sum to %d is %d.", query, nearest(query)));
             }
         }
+    }
+
+    private int nearest(int query) {
+        int n = array.length;
+        int bestSoFar = Integer.MAX_VALUE - 1000;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(array[i] + array[j] - query) < Math.abs(bestSoFar - query)) {
+                    bestSoFar = array[i] + array[j];
+                }
+            }
+        }
+        return bestSoFar;
     }
 }
 
